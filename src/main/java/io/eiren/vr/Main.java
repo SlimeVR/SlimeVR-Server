2b4ce4b920bd16e68f791732ed7cbc5bac54d33b
Eiren Rain <eirenliel@users.noreply.github.com>
2021-08-02 23:08:22 +0300

    Fix version number

--- src/main/java/io/eiren/vr/Main.java
+++ src/main/java/io/eiren/vr/Main.java
@@ -7,7 +7,7 @@ import io.eiren.util.logging.LogManager;
 
 public class Main {
 	
-	public static String VERSION = "0.0.14";
+	public static String VERSION = "0.0.13";
 	
 	public static VRServer vrServer;
 	
