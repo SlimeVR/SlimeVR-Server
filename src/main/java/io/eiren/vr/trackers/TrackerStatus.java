5600d95684471603b4d984d106790741cf382992
Eiren Rain <eirenliel@users.noreply.github.com>
2021-08-19 15:48:51 +0300

    Add OCCLUDED status to the tracker status for future usage

--- src/main/java/io/eiren/vr/trackers/TrackerStatus.java
+++ src/main/java/io/eiren/vr/trackers/TrackerStatus.java
@@ -6,9 +6,10 @@ public enum TrackerStatus {
 	OK(1, true),
 	BUSY(2, true),
 	ERROR(3, false),
+	OCCLUDED(4, false),
 	;
 	
-	private static final TrackerStatus byId[] = new TrackerStatus[4];
+	private static final TrackerStatus byId[] = new TrackerStatus[5];
 	
 	public final int id;
 	public final boolean sendData;
