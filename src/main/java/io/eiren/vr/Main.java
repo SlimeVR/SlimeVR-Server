4d3f04e227dedb99f918e374e2bc2b171d291ba9
Eiren Rain <eirenliel@users.noreply.github.com>
2021-09-12 12:11:20 +0300

    Bump version to 0.0.18 Test 3

--- src/main/java/io/eiren/vr/Main.java
+++ src/main/java/io/eiren/vr/Main.java
@@ -7,7 +7,7 @@ import io.eiren.util.logging.LogManager;
 
 public class Main {
 	
-	public static String VERSION = "0.0.18 Test 2";
+	public static String VERSION = "0.0.18 Test 3";
 	
 	public static VRServer vrServer;
 	
