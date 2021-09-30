591d7fbeea3e961479aaef5e3ec744dbc21b2e84
rastorguev.i.a <rastorguev.i.a@yandex.ru>
2021-09-30 21:32:04 +0300

    fixes

--- src/main/java/io/eiren/newGui/MainStageController.java
+++ src/main/java/io/eiren/newGui/MainStageController.java
@@ -4,7 +4,6 @@ import com.dustinredmond.fxtrayicon.FXTrayIcon;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import io.eiren.gui.SkeletonList;
 import io.eiren.gui.WiFiWindow;
 import io.eiren.util.StringUtils;
 import io.eiren.util.ann.ThreadSafe;
@@ -17,12 +16,11 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.Separator;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
@@ -31,62 +29,46 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import javax.swing.*;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import static io.eiren.util.Aligner.FROM_RIGHT;
-import static io.eiren.util.Aligner.align;
-
 public class MainStageController implements Initializable {
 
 	private Stage stage;
-	//required to trigger tray notifications
-	private FXTrayIcon icon;
-
-	private final List<TransformNode> nodes = new FastList<>();
 	private VRServer server = Main.vrServer;
+	private FXTrayIcon icon; //required to trigger tray notifications
+	private final List<TransformNode> nodes = new FastList<>();
 
 	Quaternion q = new Quaternion();
 	Vector3f v = new Vector3f();
 	float[] angles = new float[3];
 
 	@FXML
-	public MenuBar fxMenuBar;
+	private MenuBar fxMenuBar;
 	@FXML
-	public Button closeBTN;
-	@FXML
-	public Button trayBTN;
-	@FXML
-	public Button skeletonBTN;
-	@FXML
-	public Button bodyBTN;
-	@FXML
-	public AnchorPane bodyPane;
-	@FXML
-	public Button wifiBTN;
-
+	private AnchorPane bodyPane;
 
 	@FXML
-	public TextFlow jointTextFlow;
-	@FXML
-	public TextFlow xTextFlow;
-	@FXML
-	public TextFlow yTextFlow;
-	@FXML
-	public TextFlow zTextFlow;
-	@FXML
-	public TextFlow pitchTextFlow;
-	@FXML
-	public TextFlow yawTextFlow;
-	@FXML
-	public TextFlow rollTextFlow;
+	private Button reset;
 
+	@FXML
+	private TextFlow jointTextFlow;
+	@FXML
+	private TextFlow xTextFlow;
+	@FXML
+	private TextFlow yTextFlow;
+	@FXML
+	private TextFlow zTextFlow;
+	@FXML
+	private TextFlow pitchTextFlow;
+	@FXML
+	private TextFlow yawTextFlow;
+	@FXML
+	private TextFlow rollTextFlow;
 
-	public int i = 0;
-
-
+	private int i = 0;
+	private int n = 0;
 
 	private double xOffset = 0;
 	private double yOffset = 0;
@@ -100,69 +82,74 @@ public class MainStageController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+
+		fxMenuBar.prefWidthProperty().bind(stage.widthProperty());
 		fxMenuBar.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
 			xOffset = e.getSceneX();
 			yOffset = e.getSceneY();
 		});
-
 		fxMenuBar.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
 			stage.setX(event.getScreenX() - xOffset);
 			stage.setY(event.getScreenY() - yOffset);
 		});
 
-		fxMenuBar.prefWidthProperty().bind(stage.widthProperty());
+		skeletonDataInit();
+	}
 
-		closeBTN.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-			event.consume();
-			closeProgram();
-		});
+	@FXML
+	private void wifiBtnAction(ActionEvent event) {
+		new WiFiWindow();
+	}
 
-		trayBTN.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-			//stage.setIconified(true);
-			stage.hide();
-			//icon.showMessage("Slime VR", "Application minimized to tray");
-		});
+	@FXML
+	private void bodyBtnAction(ActionEvent event) {
+		bodyPane.setVisible(false);
+	}
 
-		skeletonBTN.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-			bodyPane.setVisible(true);
-		});
+	@FXML
+	private void skeletonBtnAction(ActionEvent event) {
+		bodyPane.setVisible(true);
+	}
 
-		bodyBTN.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-			bodyPane.setVisible(false);
-		});
+	@FXML
+	private void closeBtnAction(ActionEvent event) {
+		closeProgram();
+	}
 
-		wifiBTN.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-			new WiFiWindow();
-		});
+	@FXML
+	private void trayBtnAction(ActionEvent event) {
+		//stage.setIconified(true);
+		stage.hide();
+		icon.showMessage("Slime VR", "Application minimized to tray");
+	}
 
+	@FXML
+	private void reset(ActionEvent event) {
+		reset.setText(String.valueOf(3));
+		server.resetTrackers();
+		n = 2;
+		Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), timeLineEvent -> {
+			if (n > 0) reset.setText(String.valueOf(n));
+			if (n == 0) reset.setText("Reset");
+			n--;
+		}));
+		timeline.setCycleCount(3);
+		timeline.play();
+	}
 
+	@FXML
+	private void fastReset(ActionEvent event) {
+		server.resetTrackersYaw();
+	}
 
-
-		//final Separator separator = new Separator(Orientation.HORIZONTAL);
-		//separator.prefWidthProperty().bind(skeletonTextFlow.widthProperty());
-		//separator.setStyle("-fx-background-color: red;");
-
-		jointTextFlow.setLineSpacing(10.0f);
-		xTextFlow.setLineSpacing(10.0f);
-		yTextFlow.setLineSpacing(10.0f);
-		zTextFlow.setLineSpacing(10.0f);
-		pitchTextFlow.setLineSpacing(10.0f);
-		yawTextFlow.setLineSpacing(10.0f);
-		rollTextFlow.setLineSpacing(10.0f);
-
-
-		jointTextFlow.setTextAlignment(TextAlignment.CENTER);
-		xTextFlow.setTextAlignment(TextAlignment.CENTER);
-		yTextFlow.setTextAlignment(TextAlignment.CENTER);
-		zTextFlow.setTextAlignment(TextAlignment.CENTER);
-		pitchTextFlow.setTextAlignment(TextAlignment.CENTER);
-		yawTextFlow.setTextAlignment(TextAlignment.CENTER);
-		rollTextFlow.setTextAlignment(TextAlignment.CENTER);
-
-		//skeletonTextFlow.setPadding(new Insets(0, 50, 0, 0));
-
-
-
+	private void skeletonDataInit() {
+		customizeTextFlow(jointTextFlow);
+		customizeTextFlow(xTextFlow);
+		customizeTextFlow(yTextFlow);
+		customizeTextFlow(zTextFlow);
+		customizeTextFlow(pitchTextFlow);
+		customizeTextFlow(yawTextFlow);
+		customizeTextFlow(rollTextFlow);
 
 		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500), event -> {
 			i++;
@@ -174,47 +161,29 @@ public class MainStageController implements Initializable {
 			yawTextFlow.getChildren().clear();
 			rollTextFlow.getChildren().clear();
 
-			//skeletonTextFlow.getChildren().add(text1);
-			//skeletonTextFlow.getChildren().add(new Text(System.lineSeparator()));
-			//skeletonTextFlow.getChildren().add(text2);
-			//skeletonTextFlow.getChildren().add(new Text(System.lineSeparator()));
-			//skeletonTextFlow.getChildren().add(text3);
-
-			//skeletonTextFlow.getChildren().addAll(new Text("skeletondata "), new Text(String.valueOf(i)), new Text(" test"), new Text(System.lineSeparator()));
-
-			//skeletonTextFlow.getChildren().add(new Text("skeletondata "));
-			//skeletonTextFlow.getChildren().add(new Text(align(String.valueOf(i), ' ', 10, FROM_RIGHT)));
-			//skeletonTextFlow.getChildren().add(new Text(" test"));
-			//skeletonTextFlow.getChildren().add(new Text(System.lineSeparator()));
-
-			//skeletonTextFlow.getChildren().add(new Text("skeletondata " + i));
-			//skeletonTextFlow.getChildren().add(new Text(System.lineSeparator()));
-
-			//skeletonTextFlow.getChildren().add(new Text("skeletondata " + i));
-			//skeletonTextFlow.getChildren().add(new Text(System.lineSeparator()));
-
-
 			for (TransformNode n : nodes) {
 				update(n);
 			}
-
-
 		}));
 
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.play();
-
-
 	}
 
 	@ThreadSafe
-	public void skeletonUpdated(HumanSkeleton newSkeleton) {
+	private void skeletonUpdated(HumanSkeleton newSkeleton) {
 			newSkeleton.getRootNode().depthFirstTraversal((node) -> {
 				nodes.add(node);
 			});
 	}
 
-	public void update(TransformNode n) {
+	private void customizeTextFlow(TextFlow t) {
+		t.setTextAlignment(TextAlignment.CENTER);
+		t.setLineSpacing(10.0f);
+		//t.setPadding(new Insets(0, 50, 0, 0));
+	}
+
+	private void update(TransformNode n) {
 
 		Text name = new Text(n.getName());
 		Text x = new Text();
@@ -223,7 +192,6 @@ public class MainStageController implements Initializable {
 		Text a1 = new Text();
 		Text a2 = new Text();
 		Text a3 = new Text();
-		Text separator = new Text(System.lineSeparator());
 
 		n.worldTransform.getTranslation(v);
 		n.worldTransform.getRotation(q);
@@ -236,15 +204,9 @@ public class MainStageController implements Initializable {
 		a2.setText(StringUtils.prettyNumber(angles[1] * FastMath.RAD_TO_DEG, 0) + " ");
 		a3.setText(StringUtils.prettyNumber(angles[2] * FastMath.RAD_TO_DEG, 0));
 
-
-		jointTextFlow.setLineSpacing(10.0f);
-		xTextFlow.setLineSpacing(10.0f);
-		yTextFlow.setLineSpacing(10.0f);
-		zTextFlow.setLineSpacing(10.0f);
-		pitchTextFlow.setLineSpacing(10.0f);
-		yawTextFlow.setLineSpacing(10.0f);
-		rollTextFlow.setLineSpacing(10.0f);
-
+		//final Separator separator = new Separator(Orientation.HORIZONTAL);
+		//separator.prefWidthProperty().bind(skeletonTextFlow.widthProperty());
+		//separator.setStyle("-fx-background-color: red;");
 
 		jointTextFlow.getChildren().addAll(name, new Text(System.lineSeparator()));
 		xTextFlow.getChildren().addAll(x, new Text(System.lineSeparator()));
@@ -253,10 +215,9 @@ public class MainStageController implements Initializable {
 		pitchTextFlow.getChildren().addAll(a1, new Text(System.lineSeparator()));
 		yawTextFlow.getChildren().addAll(a2, new Text(System.lineSeparator()));
 		rollTextFlow.getChildren().addAll(a3, new Text(System.lineSeparator()));
-
 	}
 
-	public void closeProgram() {
+	private void closeProgram() {
 		if (ConfirmBox.display("Confirm Exit", "Are you sure you want to exit?")) {
 			// TODO add save settings
 			Platform.exit();
