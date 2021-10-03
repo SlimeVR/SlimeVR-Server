8ffa53b1a2a85c5a150a7196809526dc47a8d64e
Eiren Rain <eirenliel@users.noreply.github.com>
2021-10-03 13:41:33 +0300

    Move new gui in proper packages, small cleanup

--- src/main/java/dev/slimevr/gui/BodyProportion.java
+++ src/main/java/dev/slimevr/gui/BodyProportion.java
@@ -1,4 +1,4 @@
-package io.eiren.newGui.main;
+package dev.slimevr.gui;
 
 import io.eiren.util.StringUtils;
 import io.eiren.util.ann.ThreadSafe;
@@ -18,23 +18,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BodyProportion {
-
+	
 	private VRServer server;
-	private Map<String, Label> labels = new HashMap();
+	private Map<String, Label> labels = new HashMap<>();
 	private int counter = 0;
-
+	
 	public BodyProportion(VRServer server) {
 		this.server = server;
 	}
-
+	
 	public void bodyProportionInit(TextFlow name, TextFlow plus, TextFlow lable, TextFlow minus, TextFlow reset) {
-
+		
 		customizeTextFlow(name, 24.0f, 15);
 		customizeTextFlow(plus, 14.0f, 13);
 		customizeTextFlow(lable, 15.0f, 14);
 		customizeTextFlow(minus, 14.0f, 13);
 		customizeTextFlow(reset, 14.0f, 13);
-
+		
 		addBodyProp(name, plus, lable, minus, reset, "Chest", true);
 		addBodyPropTimedResetBtn(name, plus, lable, minus, reset, "Waist", true);
 		addBodyProp(name, plus, lable, minus, reset, "Hips width", true);
@@ -44,56 +44,60 @@ public class BodyProportion {
 		addBodyProp(name, plus, lable, minus, reset, "Head", true);
 		addBodyProp(name, plus, lable, minus, reset, "Neck", true);
 		addBodyProp(name, plus, lable, minus, reset, "Virtual waist", false);
-
+		
 	}
-
-
-	private void addBodyProp(TextFlow name, TextFlow plus, TextFlow lable,
-							 TextFlow minus, TextFlow reset, String nameStr, boolean lineSeparator) {
-		if (lineSeparator) name.getChildren().addAll(new Text(nameStr), new Text(System.lineSeparator()));
-		else name.getChildren().add(new Text(nameStr));
-
+	
+	private void addBodyProp(TextFlow name, TextFlow plus, TextFlow lable, TextFlow minus, TextFlow reset, String nameStr, boolean lineSeparator) {
+		if(lineSeparator)
+			name.getChildren().addAll(new Text(nameStr), new Text(System.lineSeparator()));
+		else
+			name.getChildren().add(new Text(nameStr));
+		
 		plus.getChildren().add(new ArithmOpBodyButton("+", nameStr, 0.01f));
-
-		if (lineSeparator) lable.getChildren().addAll(new BodyLabel(nameStr), new Text(System.lineSeparator()));
-		else lable.getChildren().add(new BodyLabel(nameStr));
-
+		
+		if(lineSeparator)
+			lable.getChildren().addAll(new BodyLabel(nameStr), new Text(System.lineSeparator()));
+		else
+			lable.getChildren().add(new BodyLabel(nameStr));
+		
 		minus.getChildren().add(new ArithmOpBodyButton("-", nameStr, -0.01f));
 		reset.getChildren().add(new ResetBodyButton(nameStr));
 	}
-
-	private void addBodyPropTimedResetBtn(TextFlow name, TextFlow plus, TextFlow lable,
-							 TextFlow minus, TextFlow reset, String nameStr, boolean lineSeparator) {
-		if (lineSeparator) name.getChildren().addAll(new Text(nameStr), new Text(System.lineSeparator()));
-		else name.getChildren().add(new Text(nameStr));
-
+	
+	private void addBodyPropTimedResetBtn(TextFlow name, TextFlow plus, TextFlow lable, TextFlow minus, TextFlow reset, String nameStr, boolean lineSeparator) {
+		if(lineSeparator)
+			name.getChildren().addAll(new Text(nameStr), new Text(System.lineSeparator()));
+		else
+			name.getChildren().add(new Text(nameStr));
+		
 		plus.getChildren().add(new ArithmOpBodyButton("+", nameStr, 0.01f));
-
-		if (lineSeparator) lable.getChildren().addAll(new BodyLabel(nameStr), new Text(System.lineSeparator()));
-		else lable.getChildren().add(new BodyLabel(nameStr));
-
+		
+		if(lineSeparator)
+			lable.getChildren().addAll(new BodyLabel(nameStr), new Text(System.lineSeparator()));
+		else
+			lable.getChildren().add(new BodyLabel(nameStr));
+		
 		minus.getChildren().add(new ArithmOpBodyButton("-", nameStr, -0.01f));
 		reset.getChildren().add(new TimedResetBodyButton(nameStr));
 	}
-
-
+	
 	private void customizeTextFlow(TextFlow t, float lineSpacing, int topPadding) {
 		t.setTextAlignment(TextAlignment.CENTER);
 		t.setLineSpacing(lineSpacing);
 		t.setPadding(new Insets(topPadding, 0, 0, 0));
 	}
-
+	
 	private class BodyLabel extends Label {
-
+		
 		public BodyLabel(String joint) {
 			super(StringUtils.prettyNumber(server.humanPoseProcessor.getSkeletonConfig(joint) * 100, 0));
 			setStyle("-fx-text-fill: #f4f4f4;");
 			labels.put(joint, this);
 		}
 	}
-
+	
 	private class ArithmOpBodyButton extends Button {
-
+		
 		public ArithmOpBodyButton(String text, String joint, float diff) {
 			super(text);
 			setPrefSize(31, 31);
@@ -101,29 +105,30 @@ public class BodyProportion {
 			addEventHandler(MouseEvent.MOUSE_CLICKED, e -> change(joint, diff));
 		}
 	}
-
+	
 	private class ResetBodyButton extends Button {
-
+		
 		public ResetBodyButton(String joint) {
 			super("Reset");
 			setPrefSize(55, 31);
-
+			
 			addEventHandler(MouseEvent.MOUSE_CLICKED, e -> reset(joint));
 		}
 	}
-
+	
 	private class TimedResetBodyButton extends Button {
-
+		
 		public TimedResetBodyButton(String joint) {
 			super("Reset");
 			setPrefSize(55, 31);
-
+			
 			addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
 				setText(String.valueOf(3));
 				counter = 2;
 				Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), timeLineEvent -> {
-					if (counter > 0) setText(String.valueOf(counter));
-					if (counter == 0) {
+					if(counter > 0)
+						setText(String.valueOf(counter));
+					if(counter == 0) {
 						reset(joint);
 						setText("Reset");
 					}
@@ -134,14 +139,14 @@ public class BodyProportion {
 			});
 		}
 	}
-
+	
 	private void change(String joint, float diff) {
 		float current = server.humanPoseProcessor.getSkeletonConfig(joint);
 		server.humanPoseProcessor.setSkeletonConfig(joint, current + diff);
 		server.saveConfig();
 		labels.get(joint).setText(StringUtils.prettyNumber((current + diff) * 100, 0));
 	}
-
+	
 	public void reset(String joint) {
 		server.humanPoseProcessor.resetSkeletonConfig(joint);
 		server.saveConfig();
@@ -155,12 +160,11 @@ public class BodyProportion {
 			});
 		}
 	}
-
+	
 	@ThreadSafe
 	public void refreshAll() {
 		labels.forEach((joint, label) -> {
 			label.setText(StringUtils.prettyNumber(server.humanPoseProcessor.getSkeletonConfig(joint) * 100, 0));
 		});
 	}
-
 }
--- src/main/java/dev/slimevr/gui/TrackersListNew.java
+++ src/main/java/dev/slimevr/gui/TrackersListNew.java
@@ -1,8 +1,7 @@
-package io.eiren.newGui.main;
+package dev.slimevr.gui;
 
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import io.eiren.gui.TrackersList;
 import io.eiren.util.ann.ThreadSafe;
 import io.eiren.util.collections.FastList;
 import io.eiren.vr.VRServer;
@@ -12,59 +11,46 @@ import java.util.Comparator;
 import java.util.List;
 
 public class TrackersListNew {
-
-	private VRServer server;
+	
 	private List<TrackerPanel> trackers;
-
+	
 	Quaternion q = new Quaternion();
 	Vector3f v = new Vector3f();
 	float[] angles = new float[3];
-
+	
 	public TrackersListNew(VRServer server) {
-		this.server = server;
 		this.trackers = new FastList<>();
-
+		
 		server.addNewTrackerConsumer(this::newTrackerAdded);
 	}
-
+	
 	private class TrackerPanel {
 		Tracker tracker;
-
+		
 		public TrackerPanel(Tracker tracker) {
 			this.tracker = tracker;
 		}
-
-
+		
 	}
-
+	
 	public void trackersListInit() {
 		trackers.sort(Comparator.comparingInt(tr -> getTrackerSort(tr.tracker)));
-
+		
 		Class<? extends Tracker> currentClass = null;
 		boolean first = true;
-
+		
 		for(int i = 0; i < trackers.size(); ++i) {
-
+			
 			TrackerPanel tr = trackers.get(i);
 			Tracker t = tr.tracker;
-
+			
 			if(t instanceof ReferenceAdjustedTracker)
 				t = ((ReferenceAdjustedTracker<?>) t).getTracker();
-
-
-
-
-
-
+			
 		}
-
-
-
-
-
-
+		
 	}
-
+	
 	private int getTrackerSort(Tracker t) {
 		if(t instanceof ReferenceAdjustedTracker)
 			t = ((ReferenceAdjustedTracker<?>) t).getTracker();
@@ -76,11 +62,11 @@ public class TrackersListNew {
 			return 200;
 		return 1000;
 	}
-
+	
 	@ThreadSafe
 	public void newTrackerAdded(Tracker t) {
 		trackers.add(new TrackerPanel(t));
 		//build();
 	}
-
+	
 }
