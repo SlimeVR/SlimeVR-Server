dd071f253849c12ae31f65fb915a4c0f14be9c5a
Eiren Rain <eirenliel@users.noreply.github.com>
2021-10-17 02:07:30 +0300

    Properly use tracker's IP if no MAC recieved

--- src/main/java/io/eiren/vr/trackers/TrackersUDPServer.java
+++ src/main/java/io/eiren/vr/trackers/TrackersUDPServer.java
@@ -98,6 +98,8 @@ public class TrackersUDPServer extends Thread {
 				if(data.remaining() > mac.length) {
 					data.get(mac);
 					macString = String.format("%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+					if(macString.equals("00:00:00:00:00:00"))
+						macString = null;
 				}
 			}
 			if(firmware.length() == 0) {
