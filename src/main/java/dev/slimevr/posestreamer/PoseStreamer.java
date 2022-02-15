8acb3b5f3fb291a5a3b69bcf25aa5da7491e8c10
ColdIce <coldice1605@gmail.com>
2022-02-15 00:50:52 -0600

    fix everything?
    Tiny steps to linux support
    oops oh well nows a good time to fix this messed up rebase
    git history is going to likely be a little strange

--- src/main/java/dev/slimevr/posestreamer/PoseStreamer.java
+++ src/main/java/dev/slimevr/posestreamer/PoseStreamer.java
@@ -2,111 +2,73 @@ package dev.slimevr.posestreamer;
 
 import java.io.IOException;
 
-import dev.slimevr.VRServer;
-import dev.slimevr.util.ann.VRServerThread;
 import dev.slimevr.vr.processor.skeleton.HumanSkeleton;
 import io.eiren.util.logging.LogManager;
 
 public class PoseStreamer {
-	
+
 	protected long frameRecordingInterval = 60L;
-	protected long nextFrameTimeMs = -1L;
-	
-	private HumanSkeleton skeleton;
-	private PoseDataStream poseFileStream;
-	
-	protected final VRServer server;
-	
-	public PoseStreamer(VRServer server) {
-		this.server = server;
-		
-		// Register callbacks/events
-		server.addSkeletonUpdatedCallback(this::onSkeletonUpdated);
-		server.addOnTick(this::onTick);
-	}
-	
-	@VRServerThread
-	public void onSkeletonUpdated(HumanSkeleton skeleton) {
+
+	protected HumanSkeleton skeleton;
+	protected PoseDataStream poseFileStream;
+
+	public PoseStreamer(HumanSkeleton skeleton) {
 		this.skeleton = skeleton;
 	}
-	
-	@VRServerThread
-	public void onTick() {
-		PoseDataStream poseFileStream = this.poseFileStream;
-		if(poseFileStream == null) {
+
+	public synchronized void captureFrame() {
+		// Make sure the stream is open before trying to write
+		if(poseFileStream.isClosed()) {
 			return;
 		}
-		
-		HumanSkeleton skeleton = this.skeleton;
-		if(skeleton == null) {
-			return;
-		}
-		
-		long curTime = System.currentTimeMillis();
-		if(curTime < nextFrameTimeMs) {
-			return;
-		}
-		
-		nextFrameTimeMs += frameRecordingInterval;
-		
-		// To prevent duplicate frames, make sure the frame time is always in the future
-		if(nextFrameTimeMs <= curTime) {
-			nextFrameTimeMs = curTime + frameRecordingInterval;
-		}
-		
-		// Make sure it's synchronized since this is the server thread interacting with
-		// an unknown outside thread controlling this class
-		synchronized(this) {
-			// Make sure the stream is open before trying to write
-			if(poseFileStream.isClosed()) {
-				return;
-			}
-			
-			try {
-				poseFileStream.writeFrame(skeleton);
-			} catch(Exception e) {
-				// Handle any exceptions without crashing the program
-				LogManager.log.severe("[PoseStreamer] Exception while saving frame", e);
-			}
+
+		try {
+			poseFileStream.writeFrame(skeleton);
+		} catch(Exception e) {
+			// Handle any exceptions without crashing the program
+			LogManager.log.severe("[PoseStreamer] Exception while saving frame", e);
 		}
 	}
-	
+
 	public synchronized void setFrameInterval(long intervalMs) {
 		if(intervalMs < 1) {
 			throw new IllegalArgumentException("intervalMs must at least have a value of 1");
 		}
-		
+
 		this.frameRecordingInterval = intervalMs;
 	}
-	
+
 	public synchronized long getFrameInterval() {
 		return frameRecordingInterval;
 	}
-	
+
+	public synchronized HumanSkeleton getSkeleton() {
+		return skeleton;
+	}
+
 	public synchronized void setOutput(PoseDataStream poseFileStream) throws IOException {
 		poseFileStream.writeHeader(skeleton, this);
 		this.poseFileStream = poseFileStream;
-		nextFrameTimeMs = -1L; // Reset the frame timing
 	}
-	
+
 	public synchronized void setOutput(PoseDataStream poseFileStream, long intervalMs) throws IOException {
 		setFrameInterval(intervalMs);
 		setOutput(poseFileStream);
 	}
-	
+
 	public synchronized PoseDataStream getOutput() {
 		return poseFileStream;
 	}
-	
+
 	public synchronized void closeOutput() throws IOException {
 		PoseDataStream poseFileStream = this.poseFileStream;
-		
+
 		if(poseFileStream != null) {
 			closeOutput(poseFileStream);
 			this.poseFileStream = null;
 		}
 	}
-	
+
 	public synchronized void closeOutput(PoseDataStream poseFileStream) throws IOException {
 		if(poseFileStream != null) {
 			poseFileStream.writeFooter(skeleton);
