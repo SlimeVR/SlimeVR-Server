c21caa76d55124e790931c946e592fa72c268e2a
Eiren Rain <eirenliel@users.noreply.github.com>
2022-03-14 20:02:10 +0200

    Bump version number to 0.1.6

--- src/main/java/dev/slimevr/Main.java
+++ src/main/java/dev/slimevr/Main.java
@@ -15,7 +15,7 @@ import io.eiren.util.logging.LogManager;
 
 public class Main {
 	
-	public static String VERSION = "0.1.5";
+	public static String VERSION = "0.1.6";
 	
 	public static VRServer vrServer;
 	
