166415826498aef41892134702740c95985de191
Butterscotch! <bscotchvanilla@gmail.com>
2022-03-25 01:47:12 -0400

    Update labels, add missing texture, and fix resource reference

--- src/main/java/dev/slimevr/gui/items/trackers/TrackerPanelCell.java
+++ src/main/java/dev/slimevr/gui/items/trackers/TrackerPanelCell.java
@@ -251,10 +251,13 @@ public class TrackerPanelCell extends AnchorPane {
 				StringUtils.prettyNumber(angles[1] * FastMath.RAD_TO_DEG, 0),
 				StringUtils.prettyNumber(angles[2] * FastMath.RAD_TO_DEG, 0)));
 		status.setText(t.getStatus().toString().toLowerCase());
+		// TODO: Use missing textures if they can't be found? This used to give a ton of errors per second, flooding the logs @ButterscotchV
 		if (status.getText().equals("ok")) {
-			connectIMG.setImage(new Image(getClass().getResource("/activeSlime.png").toString(), true));
+			URL imageUrl = getClass().getResource("/active_slime.png");
+			connectIMG.setImage(imageUrl != null ? new Image(imageUrl.toString(), true) : null);
 		} else {
-			connectIMG.setImage(new Image(getClass().getResource("/deactivedSlime.png").toString(), true));
+			URL imageUrl = getClass().getResource("/deactived_slime.png");
+			connectIMG.setImage(imageUrl != null ? new Image(imageUrl.toString(), true) : null);
 		}
 
 		if (realTracker instanceof TrackerWithTPS) {
