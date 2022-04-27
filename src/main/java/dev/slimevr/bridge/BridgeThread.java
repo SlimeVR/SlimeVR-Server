ba58f4df6acc589e31fecec29abcdb59afc4b89a
lucas lelievre <loucass003@gmail.com>
2022-04-27 22:25:28 +0200

    Reformat the formatter

--- src/main/java/dev/slimevr/bridge/BridgeThread.java
+++ src/main/java/dev/slimevr/bridge/BridgeThread.java
@@ -4,4 +4,5 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(value = RetentionPolicy.SOURCE)
-public @interface BridgeThread {}
+public @interface BridgeThread {
+}
