23d354b7e8a56371b2246a5b8f49bb9bbddb7ad0
lucas lelievre <loucass003@gmail.com>
2022-06-20 00:43:26 +0200

    Fix formatting

--- src/main/java/dev/slimevr/vr/trackers/TrackerWithFix.java
+++ src/main/java/dev/slimevr/vr/trackers/TrackerWithFix.java
@@ -2,10 +2,13 @@ package dev.slimevr.vr.trackers;
 
 import com.jme3.math.Quaternion;
 
+
 public interface TrackerWithFix {
 
 	Quaternion getYawFix();
+
 	Quaternion getGyroFix();
+
 	Quaternion getAttachmentFix();
 
 }
