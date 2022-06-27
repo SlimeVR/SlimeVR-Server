f2f74263f62b42f44b119c83e62603c41fbda7d6
Butterscotch! <bscotchvanilla@gmail.com>
2022-06-26 20:53:30 -0400

    Add comment explaining height error

--- src/main/java/dev/slimevr/autobone/errors/HeightError.java
+++ src/main/java/dev/slimevr/autobone/errors/HeightError.java
@@ -5,6 +5,7 @@ import com.jme3.math.FastMath;
 import dev.slimevr.autobone.AutoBoneTrainingStep;
 
 
+// The difference from the current height to the target height
 public class HeightError implements IAutoBoneError {
 	@Override
 	public float getStepError(AutoBoneTrainingStep trainingStep) throws AutoBoneException {
