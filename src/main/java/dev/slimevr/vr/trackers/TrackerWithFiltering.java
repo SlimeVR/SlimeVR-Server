dd0c4a534d130e0f39638e6d13c4ef9a5d03fde5
Louka <marioluigivideo@gmail.com>
2022-08-25 21:18:54 -0400

    fix formatting

--- src/main/java/dev/slimevr/vr/trackers/TrackerWithFiltering.java
+++ src/main/java/dev/slimevr/vr/trackers/TrackerWithFiltering.java
@@ -3,6 +3,7 @@ package dev.slimevr.vr.trackers;
 
 import dev.slimevr.filtering.TrackerFilters;
 
+
 public interface TrackerWithFiltering {
 
 	void setFiltering(TrackerFilters type, float amount, int buffer);
