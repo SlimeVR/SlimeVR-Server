188704ec4c6546df83b2fe31b8e39fd168e42861
Louka <marioluigivideo@gmail.com>
2022-08-29 01:36:10 -0400

    WIP math

--- src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.java
+++ src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.java
@@ -6,13 +6,12 @@ import com.jme3.math.Quaternion;
 public class QuaternionMovingAverage {
 
 	private final float factor;
-	private final int buffer;
 	private CircularArrayList<Quaternion> quatBuffer;
 	private CircularArrayList<Quaternion> rotBuffer;
 	private Quaternion averageRotation;
 	private final Quaternion filteredQuaternion;
-	private static final float SMOOTH_DIVIDER = 1.25f;
-	private static final float PREDICT_DIVIDER = 1.25f;
+	private static final float SMOOTH_MULTIPLIER = 0.95f;
+	private static final float PREDICT_MULTIPLIER = 2.0f;
 
 	public QuaternionMovingAverage(
 		TrackerFilters type,
@@ -20,15 +19,14 @@ public class QuaternionMovingAverage {
 		int buffer,
 		Quaternion initialRotation
 	) {
-		// Dividing to control range.
-		if (type == TrackerFilters.SMOOTHING) {
-			this.factor = 1 - (amount / SMOOTH_DIVIDER);
-		} else { // if prediction
-			this.factor = 1 + (amount / PREDICT_DIVIDER);
-		}
+		buffer = Math.max(buffer, 1);
+		int latency = buffer * 2;
 
-		// Sanity check to prevent crash.
-		this.buffer = Math.max(buffer, 1);
+		if (type == TrackerFilters.SMOOTHING) {
+			this.factor = (1 - (amount / SMOOTH_MULTIPLIER)) / latency;
+		} else { // if prediction
+			this.factor = (amount / PREDICT_MULTIPLIER / latency) + 1;
+		}
 
 		quatBuffer = new CircularArrayList<>(buffer);
 		rotBuffer = new CircularArrayList<>(buffer);
@@ -36,25 +34,24 @@ public class QuaternionMovingAverage {
 		filteredQuaternion = new Quaternion(initialRotation);
 		quatBuffer.add(initialRotation);
 	}
+	// TODO Buffer? Latency? Protocol...
 
 	// 1000hz
 	synchronized public void update() {
 		if (quatBuffer.size() > 0) {
 			filteredQuaternion
-				.slerp(filteredQuaternion, quatBuffer.get(quatBuffer.size() - 1), factor);
+				.slerpLocal(quatBuffer.get(quatBuffer.size() - 1), factor);
 		}
-		// TODO
 	}
 
 	synchronized public void addQuaternion(Quaternion q) {
-		if (quatBuffer.size() == buffer) {
+		if (quatBuffer.size() == quatBuffer.capacity()) {
 			quatBuffer.remove(0);
 		}
 		quatBuffer.add(q);
-		// TODO
 	}
 
 	public Quaternion getFilteredQuaternion() {
-		return filteredQuaternion;
+		return filteredQuaternion.clone();
 	}
 }
