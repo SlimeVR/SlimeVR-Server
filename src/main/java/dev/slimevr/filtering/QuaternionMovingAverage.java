8c8abbf80d017725f30eae50362588337c9a7b12
Louka <marioluigivideo@gmail.com>
2022-09-06 22:27:42 -0400

    basic prediction

--- src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.java
+++ src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.java
@@ -6,59 +6,78 @@ import com.jme3.math.Quaternion;
 
 public class QuaternionMovingAverage {
 
-	private final float smoothFactor, predictFactor;
-	private final TrackerFilters type;
-	private final CircularArrayList<Quaternion> quatBuffer;
+	private final float smoothFactor;
+	private final float predictFactor;
+	private final boolean smooths;
+	private final boolean predicts;
 	private final CircularArrayList<Quaternion> rotBuffer;
+	private final Quaternion quatBuf = new Quaternion();
+	private final Quaternion targetQuat = new Quaternion();
+	private final Quaternion lastQuaternion;
 	private final Quaternion filteredQuaternion;
-	private static final float SMOOTH_QUADRATIC_DIVIDER = 18f;
-	private static final float SMOOTH_QUADRATIC_MIN = 0.013f;
+	private static final float SMOOTH_QUADRATIC_DIVIDER = 16f;
+	private static final float SMOOTH_QUADRATIC_MIN = 0.015f;
+	private static final float PREDICT_QUADRATIC_DIVIDER = 4f;
+	private static final float PREDICT_QUADRATIC_MIN = 0.06f;
+	private static final int PREDICT_BUFFER = 6;
+
 
 	public QuaternionMovingAverage(
 		TrackerFilters type,
 		float amount,
 		Quaternion initialRotation
 	) {
-		this.type = type;
+		smooths = type == TrackerFilters.SMOOTHING;
+		predicts = type == TrackerFilters.PREDICTION;
 
 		amount = Math.max(amount, 0);
-		smoothFactor = (FastMath.pow(1 - amount, 2) / SMOOTH_EXPONENTIAL_DIVIDER)
-			+ SMOOTH_EXPONENTIAL_MIN;
-		predictFactor = 1 + amount;
+		smoothFactor = FastMath.pow(1 - amount, 2) / SMOOTH_QUADRATIC_DIVIDER
+			+ SMOOTH_QUADRATIC_MIN;
+		predictFactor = FastMath.pow(amount, 2) / PREDICT_QUADRATIC_DIVIDER
+			+ PREDICT_QUADRATIC_MIN;
 
-		int buffer = 3;
-
-		quatBuffer = new CircularArrayList<>(buffer);
-		rotBuffer = new CircularArrayList<>(buffer);
+		if (predicts) {
+			rotBuffer = new CircularArrayList<>(PREDICT_BUFFER);
+		} else {
+			rotBuffer = null;
+		}
 
 		filteredQuaternion = new Quaternion(initialRotation);
-		quatBuffer.add(initialRotation);
+		lastQuaternion = new Quaternion(initialRotation);
 	}
 
 	// 1000hz
 	synchronized public void update() {
-		if (type == TrackerFilters.SMOOTHING) {
-			if (quatBuffer.size() > 0) {
-				filteredQuaternion
-					.slerpLocal(
-						quatBuffer.get(quatBuffer.size() - 1),
-						smoothFactor
-					);
-			}
-		} else if (type == TrackerFilters.PREDICTION) {
-			if (quatBuffer.size() > 1) {
-				filteredQuaternion
-					.slerpLocal(quatBuffer.get(quatBuffer.size() - 1), predictFactor);
+		targetQuat.set(lastQuaternion);
+
+		if (predicts) {
+			if (rotBuffer.size() > 0) {
+				quatBuf.set(targetQuat);
+				rotBuffer.forEach(quatBuf::multLocal);
+				targetQuat.slerpLocal(quatBuf, predictFactor);
 			}
 		}
-		// TODO: prediction
+
+		if (smooths) {
+			filteredQuaternion.slerpLocal(targetQuat, smoothFactor);
+		} else {
+			filteredQuaternion.set(targetQuat);
+		}
 	}
 
+	int i;
+
 	synchronized public void addQuaternion(Quaternion q) {
-		if (quatBuffer.size() == quatBuffer.capacity()) {
-			quatBuffer.remove(0);
+		if (rotBuffer != null) {
+			if (rotBuffer.size() == rotBuffer.capacity()) {
+				rotBuffer.remove(0);
+			}
+			quatBuf.set(lastQuaternion);
+			quatBuf.inverseLocal();
+			rotBuffer.add(quatBuf.mult(q));
 		}
-		quatBuffer.add(q);
+
+		lastQuaternion.set(q);
 	}
 
 	public Quaternion getFilteredQuaternion() {
