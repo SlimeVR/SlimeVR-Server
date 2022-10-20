3829dc1d421519f154997fcc30dce721c2ec0fc2
Butterscotch! <bscotchvanilla@gmail.com>
2022-10-20 03:53:28 -0400

    Tune error factors

--- src/main/java/dev/slimevr/config/AutoBoneConfig.java
+++ src/main/java/dev/slimevr/config/AutoBoneConfig.java
@@ -14,7 +14,7 @@ public class AutoBoneConfig {
 	public float adjustRateMultiplier = 0.995f;
 
 	public float slideErrorFactor = 0.0f;
-	public float offsetSlideErrorFactor = 1.0f;
+	public float offsetSlideErrorFactor = 2.0f;
 	public float footHeightOffsetErrorFactor = 0.0f;
 	public float bodyProportionErrorFactor = 0.825f;
 	public float heightErrorFactor = 0.0f;
