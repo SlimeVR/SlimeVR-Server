933327bbdbf7c1c81c15df15a336a7974237d6aa
Louka <marioluigivideo@gmail.com>
2022-12-12 22:52:11 -0500

    fix formatting lol

--- server/src/main/java/dev/slimevr/config/FiltersConfig.java
+++ server/src/main/java/dev/slimevr/config/FiltersConfig.java
@@ -8,55 +8,55 @@ import dev.slimevr.vr.trackers.TrackerWithFiltering;
 
 public class FiltersConfig {
 
-    // Type of filtering applied (none, smoothing or prediction)
-    private String type = "prediction";
+	// Type of filtering applied (none, smoothing or prediction)
+	private String type = "prediction";
 
-    // Amount/Intensity of the specified filtering (0 to 1)
-    private float amount = 0.2f;
+	// Amount/Intensity of the specified filtering (0 to 1)
+	private float amount = 0.2f;
 
-    public FiltersConfig() {
-    }
+	public FiltersConfig() {
+	}
 
-    public void updateTrackersFilters() {
-        for (Tracker t : Main.vrServer.getAllTrackers()) {
-            Tracker tracker = t.get();
-            if (tracker instanceof TrackerWithFiltering) {
-                ((TrackerWithFiltering) tracker)
-                        .setFiltering(
-                                enumGetType(),
-                                getAmount()
-                        );
-            }
-        }
-    }
+	public void updateTrackersFilters() {
+		for (Tracker t : Main.vrServer.getAllTrackers()) {
+			Tracker tracker = t.get();
+			if (tracker instanceof TrackerWithFiltering) {
+				((TrackerWithFiltering) tracker)
+					.setFiltering(
+						enumGetType(),
+						getAmount()
+					);
+			}
+		}
+	}
 
-    public TrackerFilters enumGetType() {
-        return TrackerFilters.getByConfigkey(type);
-    }
+	public TrackerFilters enumGetType() {
+		return TrackerFilters.getByConfigkey(type);
+	}
 
-    public void enumSetType(TrackerFilters type) {
-        this.type = type.configKey;
-    }
+	public void enumSetType(TrackerFilters type) {
+		this.type = type.configKey;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public float getAmount() {
-        // Check for out of bound amount
-        // Note: allow up to 2 for power users or something
-        if (amount > 2f) {
-            setAmount(0.2f);
-        }
+	public float getAmount() {
+		// Check for out of bound amount
+		// Note: allow up to 2 for power users or something
+		if (amount > 2f) {
+			setAmount(0.2f);
+		}
 
-        return amount;
-    }
+		return amount;
+	}
 
-    public void setAmount(float amount) {
-        this.amount = amount;
-    }
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
 }
