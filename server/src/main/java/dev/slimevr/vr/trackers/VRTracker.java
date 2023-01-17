eeaff4266bb97f1b157542b93ed6d076d462f31d
Louka <marioluigivideo@gmail.com>
2023-01-16 19:10:39 -0500

    Make it so Vive trackers can reset

--- server/src/main/java/dev/slimevr/vr/trackers/VRTracker.java
+++ server/src/main/java/dev/slimevr/vr/trackers/VRTracker.java
@@ -1,5 +1,6 @@
 package dev.slimevr.vr.trackers;
 
+import com.jme3.math.Quaternion;
 import dev.slimevr.vr.Device;
 import io.eiren.util.BufferedTimer;
 
@@ -7,6 +8,9 @@ import io.eiren.util.BufferedTimer;
 public class VRTracker extends ComputedTracker {
 
 	protected BufferedTimer timer = new BufferedTimer(1f);
+	public final Quaternion mountFix = new Quaternion();
+	public final Quaternion attachmentFix = new Quaternion();
+	public final Quaternion yawFix = new Quaternion();
 
 	public VRTracker(
 		int id,
@@ -21,6 +25,54 @@ public class VRTracker extends ComputedTracker {
 
 	public VRTracker(int id, String name, boolean hasRotation, boolean hasPosition) {
 		super(id, name, name, hasRotation, hasPosition, null);
+	}
+
+	@Override
+	public boolean getRotation(Quaternion store) {
+		store.set(rotation);
+		// Don't adjust if tracker is HMD/Head (use raw rotation)
+		if (super.getBodyPosition() != TrackerPosition.HMD)
+			adjustInternal(store);
+		return true;
+	}
+
+	protected void adjustInternal(Quaternion store) {
+		store.multLocal(attachmentFix);
+		store.multLocal(mountFix);
+		yawFix.mult(store, store);
+	}
+
+	@Override
+	public void resetFull(Quaternion reference) {
+		fixAttachment(rotation, reference);
+		fixYaw(rotation, reference);
+	}
+
+	@Override
+	public void resetYaw(Quaternion reference) {
+		fixYaw(rotation, reference);
+	}
+
+	private void fixAttachment(Quaternion sensorRotation, Quaternion reference) {
+		reference = reference.clone();
+		mountFix.fromAngles(0, reference.getYaw(), 0);
+
+		sensorRotation = sensorRotation.clone();
+		attachmentFix.set(sensorRotation.inverse());
+	}
+
+	private void fixYaw(Quaternion sensorRotation, Quaternion reference) {
+		// Use only yaw HMD rotation
+		reference = reference.clone();
+		reference.fromAngles(0, reference.getYaw(), 0);
+
+		sensorRotation = sensorRotation.clone();
+		sensorRotation.multLocal(attachmentFix);
+		sensorRotation.multLocal(mountFix);
+
+		sensorRotation.fromAngles(0, sensorRotation.getYaw(), 0);
+
+		yawFix.set(sensorRotation.inverse().mult(reference));
 	}
 
 	@Override
