58b1fbba1fae8b9bf728f601f1967edd2c45656a
lucas lelievre <loucass003@gmail.com>
2023-02-12 18:54:40 +0100

    Moving wifi provisioning and new device detection to the server (#542)

--- server/src/main/java/dev/slimevr/VRServer.java
+++ server/src/main/java/dev/slimevr/VRServer.java
@@ -13,6 +13,7 @@ import dev.slimevr.platform.linux.UnixSocketBridge;
 import dev.slimevr.platform.windows.WindowsNamedPipeBridge;
 import dev.slimevr.poserecorder.BVHRecorder;
 import dev.slimevr.protocol.ProtocolAPI;
+import dev.slimevr.serial.ProvisioningHandler;
 import dev.slimevr.serial.SerialHandler;
 import dev.slimevr.tracking.DeviceManager;
 import dev.slimevr.tracking.processor.HumanPoseManager;
@@ -63,6 +64,7 @@ public class VRServer extends Thread {
 	private final ConfigManager configManager;
 	private final Timer timer = new Timer();
 	private final NanoTimer fpsTimer = new NanoTimer();
+	private final ProvisioningHandler provisioningHandler;
 
 	/**
 	 * This function is used by VRWorkout, do not remove!
@@ -80,6 +82,9 @@ public class VRServer extends Thread {
 		deviceManager = new DeviceManager(this);
 
 		serialHandler = new SerialHandler();
+
+		provisioningHandler = new ProvisioningHandler(this);
+
 		autoBoneHandler = new AutoBoneHandler(this);
 		protocolAPI = new ProtocolAPI(this);
 
@@ -430,6 +435,10 @@ public class VRServer extends Thread {
 
 	public NanoTimer getFpsTimer() {
 		return fpsTimer;
+	}
+
+	public ProvisioningHandler getProvisioningHandler() {
+		return provisioningHandler;
 	}
 
 	public void clearTrackersDriftCompensation() {
