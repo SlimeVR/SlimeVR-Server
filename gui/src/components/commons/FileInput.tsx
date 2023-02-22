43a7e73d86f361355475321d16cbb426a26a6cac
ImUrX <urielfontan2002@gmail.com>
2023-02-22 18:13:14 -0300

    file icon

--- gui/src/components/commons/FileInput.tsx
+++ gui/src/components/commons/FileInput.tsx
@@ -2,6 +2,7 @@ import { Localized } from '@fluent/react';
 import classNames from 'classnames';
 import { forwardRef, useMemo, useState } from 'react';
 import { Control, Controller, UseControllerProps } from 'react-hook-form';
+import { FileIcon } from './icon/FileIcon';
 import { Typography } from './Typography';
 
 interface InputProps {
@@ -78,22 +79,7 @@ export const FileInputInside = forwardRef<
       onDragLeave={() => setDragging(false)}
     >
       <span className="flex items-center space-x-2 pointer-events-none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className={classNames(
-            'w-6 h-6 transition-transform',
-            isDragging && 'scale-150'
-          )}
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
-            clipRule="evenodd"
-          />
-          <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
-        </svg>
+        <FileIcon isDragging={isDragging} />
         <div>
           <Localized
             id={label}
