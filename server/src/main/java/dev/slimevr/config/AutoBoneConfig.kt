1ae96f2ef45924db5e896a6243d4a28efec674b8
Butterscotch! <bscotchvanilla@gmail.com>
2023-02-27 22:17:12 -0500

    Fix formatting

--- server/src/main/java/dev/slimevr/config/AutoBoneConfig.kt
+++ server/src/main/java/dev/slimevr/config/AutoBoneConfig.kt
@@ -1,25 +1,25 @@
 package dev.slimevr.config
 
 class AutoBoneConfig {
-    var cursorIncrement = 2
-    var minDataDistance = 1
-    var maxDataDistance = 1
-    var numEpochs = 100
-    var printEveryNumEpochs = 25
-    var initialAdjustRate = 10f
-    var adjustRateMultiplier = 0.995f
-    var slideErrorFactor = 0.0f
-    var offsetSlideErrorFactor = 2.0f
-    var footHeightOffsetErrorFactor = 0.0f
-    var bodyProportionErrorFactor = 0.825f
-    var heightErrorFactor = 0.0f
-    var positionErrorFactor = 0.0f
-    var positionOffsetErrorFactor = 0.0f
-    var calcInitError = false
-    var targetHeight = -1f
-    var randomizeFrameOrder = true
-    var scaleEachStep = true
-    var sampleCount = 1000
-    var sampleRateMs: Long = 20
-    var saveRecordings = false
+	var cursorIncrement = 2
+	var minDataDistance = 1
+	var maxDataDistance = 1
+	var numEpochs = 100
+	var printEveryNumEpochs = 25
+	var initialAdjustRate = 10f
+	var adjustRateMultiplier = 0.995f
+	var slideErrorFactor = 0.0f
+	var offsetSlideErrorFactor = 2.0f
+	var footHeightOffsetErrorFactor = 0.0f
+	var bodyProportionErrorFactor = 0.825f
+	var heightErrorFactor = 0.0f
+	var positionErrorFactor = 0.0f
+	var positionOffsetErrorFactor = 0.0f
+	var calcInitError = false
+	var targetHeight = -1f
+	var randomizeFrameOrder = true
+	var scaleEachStep = true
+	var sampleCount = 1000
+	var sampleRateMs: Long = 20
+	var saveRecordings = false
 }
