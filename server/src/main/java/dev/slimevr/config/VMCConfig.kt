ad538fe6cbe000eb63f95f880c43cbbb6c59e111
marcozzxx810 <marcowingwing@gmail.com>
2023-03-02 18:53:30 +0800

    refactor: kotlin override java

--- server/src/main/java/dev/slimevr/config/VMCConfig.kt
+++ server/src/main/java/dev/slimevr/config/VMCConfig.kt
@@ -1,15 +1,29 @@
 package dev.slimevr.config
 
 class VMCConfig : // Address of the VRM to be used
-	OSCConfig {
+	OSCConfig() {
 	// Anchor the tracking at the hip?
 
-	constructor() : super() {
-		super.setPortIn(39540)
-		super.setPortOut(39539)
-	}
+	private var portIn: Int = 39540
+	private var portOut: Int = 39539
 
 	var anchorHip = true
 
 	var vrmJson: String? = null
+
+	override fun getPortIn(): Int {
+		return portIn
+	}
+
+	override fun setPortIn(portIn: Int) {
+		this.portIn = portIn
+	}
+
+	override fun getPortOut(): Int {
+		return portOut
+	}
+
+	override fun setPortOut(portOut: Int) {
+		this.portOut = portOut
+	}
 }
