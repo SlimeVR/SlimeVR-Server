07600f013324f5f58d00a068fa1d2aea75043612
Butterscotch! <bscotchvanilla@gmail.com>
2023-03-31 18:39:59 -0400

    Set version number and add icons

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -143,10 +143,10 @@ android {
 		targetSdk = 33
 
 		// Defines the version number of your app.
-		versionCode = 1
+		versionCode = 2
 
 		// Defines a user-friendly version name for your app.
-		versionName = "1.0"
+		versionName = "0.7.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
