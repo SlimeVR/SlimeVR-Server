6b7c47d36cfc53a1c576c0b58b4187bf1befb1b1
Butterscotch! <bscotchvanilla@gmail.com>
2023-04-03 00:21:31 -0400

    Add GUI webview & remove titlebar

--- server/src/main/java/dev/slimevr/MainActivity.kt
+++ server/src/main/java/dev/slimevr/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.slimevr
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import io.eiren.util.logging.LogManager
 import io.ktor.server.engine.embeddedServer
@@ -11,10 +12,29 @@ import java.io.File
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 
+
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		// Host the web GUI server
+		embeddedServer(Netty, port = 8080) {
+			routing {
+				static("/") {
+					staticBasePackage = "web-gui"
+					resources()
+					defaultResource("index.html")
+				}
+			}
+		}.start(wait = false)
+
+		// Load the web GUI web page
+		val guiWebView = findViewById<WebView>(R.id.guiWebView)
+		guiWebView.settings.javaScriptEnabled = true
+		guiWebView.settings.domStorageEnabled = true
+		guiWebView.settings.setSupportZoom(true)
+		guiWebView.loadUrl("http://localhost:8080/")
 
 		thread(start = true, name = "Main VRServer Thread") {
 			try {
@@ -24,17 +44,6 @@ class MainActivity : AppCompatActivity() {
 			}
 			LogManager.info("Running version $VERSION")
 			try {
-				// Run the web GUI server
-				embeddedServer(Netty, port = 8080) {
-					routing {
-						static("/") {
-							staticBasePackage = "web-gui"
-							resources()
-							defaultResource("index.html")
-						}
-					}
-				}.start(wait = false)
-
 				vrServer = VRServer(File(filesDir, "vrconfig.yml").absolutePath)
 				vrServer.start()
 				Keybinding(vrServer)
