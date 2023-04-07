c90869ec434266b29e5dc41bcc52ef5d25feb954
ImUrX <urielfontan2002@gmail.com>
2023-04-07 19:47:57 -0300

    remove useless line

--- gui/src/components/commons/BaseModal.tsx
+++ gui/src/components/commons/BaseModal.tsx
@@ -24,7 +24,6 @@ export function BaseModal({
       className={
         props.className ||
         classNames(
-          props.className as string,
           'items-center focus:ring-transparent focus:ring-offset-transparent',
           'focus:outline-transparent outline-none bg-background-60 p-6 rounded-lg',
           'text-background-10'
