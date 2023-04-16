2ecab36f2c5e498c642b66b007de3763f35354e3
Louka <marioluigivideo@gmail.com>
2023-04-16 19:10:27 -0400

    Update VRCOSCSettings.tsx

--- gui/src/components/settings/pages/VRCOSCSettings.tsx
+++ gui/src/components/settings/pages/VRCOSCSettings.tsx
@@ -255,9 +255,7 @@ export function VRCOSCSettings() {
               outlined
               control={control}
               name="vrchat.trackers.waist"
-              label={l10n.getString(
-                'settings-osc-vrchat-network-trackers-hip'
-              )}
+              label={l10n.getString('settings-osc-vrchat-network-trackers-hip')}
             />
             <CheckBox
               variant="toggle"
