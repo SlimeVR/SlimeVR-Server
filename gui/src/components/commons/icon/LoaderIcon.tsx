4aff3acebdcde90048057020cf40b6eac2a14408
ImUrX <urielfontan2002@gmail.com>
2023-04-30 13:36:16 -0300

    remove pixelation

--- gui/src/components/commons/icon/LoaderIcon.tsx
+++ gui/src/components/commons/icon/LoaderIcon.tsx
@@ -17,28 +17,28 @@ export function LoaderIcon({
         src="/images/jumping-slime.gif"
         alt="Slime jumping"
         width="85"
-        className="crisp-edges"
+        // className="crisp-edges"
       ></img>
       <img
         hidden={slimeState !== SlimeState.HAPPY}
         src="/images/happy-slime.gif"
         alt="Happy slime"
         width="85"
-        className="crisp-edges"
+        // className="crisp-edges"
       ></img>
       <img
         hidden={slimeState !== SlimeState.SAD}
         src="/images/sad-slime.gif"
         alt="Sad slime"
         width="85"
-        className="crisp-edges"
+        // className="crisp-edges"
       ></img>
       <img
         hidden={slimeState !== SlimeState.CURIOUS}
         src="/images/curious-slime.gif"
         alt="Slime looking for something"
         width="85"
-        className="crisp-edges"
+        // className="crisp-edges"
       ></img>
     </>
   );
