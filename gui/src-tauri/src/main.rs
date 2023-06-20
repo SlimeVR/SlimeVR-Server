366a2558878f92aae84e983fed2a8f67ce387824
Uriel <urielfontan2002@gmail.com>
2023-06-20 04:14:33 -0700

    Finally fix small window (#731)

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,26 +1,42 @@
 #![cfg_attr(all(not(debug_assertions), windows), windows_subsystem = "windows")]
-use std::env;
 use std::panic;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 
 use clap::Parser;
+use state::WindowState;
 use tauri::api::process::{Command, CommandChild};
 use tauri::Manager;
 use tauri::RunEvent;
-
-#[cfg(windows)]
 use tauri::WindowEvent;
 
 use crate::util::{
 	get_launch_path, show_error, valid_java_paths, Cli, JAVA_BIN, MINIMUM_JAVA_VERSION,
 };
 
+mod state;
 mod util;
+
+#[tauri::command]
+fn update_window_state(
+	window: tauri::Window,
+	state: tauri::State<Mutex<WindowState>>,
+) -> Result<(), String> {
+	let mut lock = state.lock().unwrap();
+	lock.update_state(&window, false)
+		.map_err(|e| format!("{:?}", e))?;
+	if window.is_maximized().map_err(|e| e.to_string())? {
+		window.unmaximize().map_err(|e| e.to_string())?;
+		lock.update_state(&window, true)
+			.map_err(|e| format!("{:?}", e))?;
+	}
+	Ok(())
+}
 
 fn main() {
 	// Make an error dialog box when panicking
@@ -108,8 +124,32 @@ fn main() {
 
 	let exit_flag_terminated = exit_flag.clone();
 	let build_result = tauri::Builder::default()
-		.plugin(tauri_plugin_window_state::Builder::default().build())
+		.invoke_handler(tauri::generate_handler![update_window_state])
 		.setup(move |app| {
+			let window_state =
+				WindowState::open_state(app.path_resolver().app_config_dir().unwrap())
+					.unwrap_or_default();
+
+			let window = tauri::WindowBuilder::new(
+				app,
+				"local",
+				tauri::WindowUrl::App("index.html".into()),
+			)
+			.title("SlimeVR")
+			.inner_size(1289.0, 709.0)
+			.min_inner_size(393.0, 667.0)
+			.resizable(true)
+			.visible(true)
+			.decorations(false)
+			.fullscreen(false)
+			.disable_file_drop_handler()
+			.build()?;
+			if window_state.is_old() {
+				window_state.update_window(&window, false)?;
+			}
+
+			app.manage(Mutex::new(window_state));
+
 			if let Some(mut recv) = stdout_recv {
 				let app_handle = app.app_handle();
 				tauri::async_runtime::spawn(async move {
@@ -139,6 +179,12 @@ fn main() {
 			Ok(())
 		})
 		.on_window_event(|e| match e.event() {
+			WindowEvent::CloseRequested { .. } => {
+				let window_state = e.window().state::<Mutex<WindowState>>();
+				if let Err(e) = update_window_state(e.window().clone(), window_state) {
+					log::error!("failed to update window state {}", e)
+				}
+			}
 			// See https://github.com/tauri-apps/tauri/issues/4012#issuecomment-1449499149
 			#[cfg(windows)]
 			WindowEvent::Resized(_) => std::thread::sleep(std::time::Duration::from_nanos(1)),
@@ -147,8 +193,18 @@ fn main() {
 		.build(tauri::generate_context!());
 	match build_result {
 		Ok(app) => {
-			app.run(move |_app_handle, event| match event {
+			app.run(move |app_handle, event| match event {
 				RunEvent::ExitRequested { .. } => {
+					let window_state = app_handle.state::<Mutex<WindowState>>();
+					let lock = window_state.lock().unwrap();
+					let config_dir =
+						app_handle.path_resolver().app_config_dir().unwrap();
+					let window_state_res = lock.save_state(config_dir);
+					match window_state_res {
+						Ok(()) => log::info!("saved window state"),
+						Err(e) => log::error!("failed to save window state: {}", e),
+					}
+
 					let Some(ref mut child) = backend else { return };
 					let write_result = child.write(b"exit\n");
 					match write_result {
