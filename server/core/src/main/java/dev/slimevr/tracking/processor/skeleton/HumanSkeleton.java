c9b45fd57f6f8b8e8f9f1926e760dff16f9f5758
Louka <marioluigivideo@gmail.com>
2023-07-16 20:40:05 -0400

    Refresh headShift when setting trackers

--- server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.java
+++ server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.java
@@ -505,6 +505,9 @@ public class HumanSkeleton {
 		// Rebuilds the arm skeleton nodes attachments
 		assembleSkeletonArms(true);
 
+		// Refresh headShift
+		humanPoseManager.computeNodeOffset(BoneType.HEAD);
+
 		// Refresh node offsets for arms
 		computeDependentArmOffsets();
 
