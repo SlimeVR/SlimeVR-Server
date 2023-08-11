bfb14af4b49ad6c930de2419cd27fa934c4163d6
ImUrX <urielfontan2002@gmail.com>
2023-08-11 10:02:35 -0300

    offset skeleton if no HMD

--- gui/src/components/widgets/SkeletonVisualizerWidget.tsx
+++ gui/src/components/widgets/SkeletonVisualizerWidget.tsx
@@ -1,14 +1,15 @@
-import { Canvas, Object3DNode, extend } from '@react-three/fiber';
+import { Canvas, Object3DNode, extend, useThree } from '@react-three/fiber';
 import { useAppContext } from '@/hooks/app';
-import { Bone, PerspectiveCamera } from 'three';
+import { Bone } from 'three';
 import { useMemo, useEffect, useRef } from 'react';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, OrthographicCamera } from '@react-three/drei';
 import {
   BoneKind,
   createChildren,
   BasedSkeletonHelper,
 } from '../../utils/skeletonHelper';
 import * as THREE from 'three';
+import { BodyPart, BoneT } from 'solarxr-protocol';
 
 extend({ BasedSkeletonHelper });
 
@@ -22,6 +23,38 @@ declare module '@react-three/fiber' {
 }
 
 const groundColor = '#4444aa';
+const frustumSize = 10;
+const factor = 2;
+
+function OrthographicCameraWrapper() {
+  const { size } = useThree();
+  const aspect = useMemo(() => size.width / size.height, [size]);
+
+  return (
+    <OrthographicCamera
+      makeDefault
+      zoom={200}
+      top={frustumSize / factor}
+      bottom={frustumSize / -factor}
+      left={(0.5 * frustumSize * aspect) / -factor}
+      right={(0.5 * frustumSize * aspect) / factor}
+      near={0.1}
+      far={1000}
+      position={[25, 75, 50]}
+    />
+  );
+}
+
+const yParts = [
+  BodyPart.NECK,
+  BodyPart.UPPER_CHEST,
+  BodyPart.CHEST,
+  BodyPart.WAIST,
+  BodyPart.HIP,
+  BodyPart.LEFT_UPPER_LEG,
+  BodyPart.LEFT_LOWER_LEG,
+  BodyPart.LEFT_FOOT,
+];
 
 export function SkeletonVisualizerWidget() {
   const { bones: _bones } = useAppContext();
@@ -43,20 +76,24 @@ export function SkeletonVisualizerWidget() {
     );
   }, [bones]);
 
+  const heightOffset = useMemo(() => {
+    const hmd = bones.get(BodyPart.HEAD);
+    if (hmd?.headPositionG?.y === undefined || hmd.headPositionG?.y > 0) {
+      return 0;
+    }
+    const yLength = yParts.map((x) => bones.get(x));
+    if (yLength.some((x) => x === undefined)) return 0;
+    return (yLength as BoneT[]).reduce((prev, cur) => prev + cur.boneLength, 0);
+  }, [bones]);
+
   if (!skeleton.current) return <></>;
   return (
     <div className="bg-background-70 flex flex-col p-3 rounded-lg gap-2">
       <Canvas
         className="container"
         style={{ height: 400, background: 'transparent' }}
-        onCreated={({camera}) => {
-          (camera as PerspectiveCamera).fov = 60;
-          camera.near = 0.01
-          camera.position.set(3, 5, 3)
-          camera.zoom = 4
-        }}
       >
-        <mesh position={[0, -3, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[10, 10, 50, 50]} />
           <meshBasicMaterial
             wireframe
@@ -66,14 +103,15 @@ export function SkeletonVisualizerWidget() {
             side={THREE.DoubleSide}
           />
         </mesh>
-        <group scale={2}>
+        <group position={[0, heightOffset, 0]}>
           <basedSkeletonHelper
             frustumCulled={false}
             args={[skeleton.current[0]]}
           ></basedSkeletonHelper>
         </group>
         <primitive object={skeleton.current[0]} />
-        <OrbitControls target={[0, -1.5, 0]} />
+        <OrbitControls target={[0, -1, 0]} />
+        <OrthographicCameraWrapper />
       </Canvas>
     </div>
   );
