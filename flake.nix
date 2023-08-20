ed023793c4d046f439ea80a666a4f3fa0ab29e82
Uriel <urielfontan2002@gmail.com>
2023-08-20 01:43:26 -0300

    use devenv for nix develop

--- flake.nix
+++ flake.nix
@@ -1,87 +1,109 @@
 {
-  description = "Server app for SlimeVR ecosystem";
+  description = "Affordable full-body tracking for VR!";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
 
-  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        nativeBuildInputs = with pkgs; [
-          curl
-          gcc
-          openssl
-          pkgconfig
-          which
-          zlib
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
-          freetype
-          expat
-        ];
-        buildInputs = with pkgs; [
-          appimagekit
-          atk
-          cairo
-          dbus
-          dbus.lib
-          dprint
-          gdk-pixbuf
-          glib.out
-          gobject-introspection
-          gtk3
-          harfbuzz
-          libffi
-          libsoup_3
-          openssl.out
-          pango
-          pkg-config
-          treefmt
-          webkitgtk_4_1
-          zlib
-          gst_all_1.gstreamer
-          gst_all_1.gst-plugins-base
-          gst_all_1.gst-plugins-good
-          gst_all_1.gst-plugins-bad
-          librsvg
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        lib,
+        ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
 
-          # Some nice things to have
-          exa
-          fd
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        # packages.default = pkgs.hello;
 
-          jdk17 # JDK17
-          nodejs
-          gradle
-        ];
-      in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs =
-            nativeBuildInputs
-            ++ [
+        devenv.shells.default = {
+          name = "slimevr";
+
+          imports = [
+            # This is just like the imports in devenv.nix.
+            # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
+            # ./devenv-foo.nix
+          ];
+
+          # https://devenv.sh/reference/options/
+          packages =
+            [
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+              appimagekit
+              atk
+              cairo
+              dbus
+              dbus.lib
+              dprint
+              gdk-pixbuf
+              glib.out
+              gobject-introspection
+              gtk3
+              harfbuzz
+              libffi
+              libsoup_3
+              openssl.out
+              pango
+              pkg-config
+              treefmt
+              webkitgtk_4_1
+              zlib
+              gst_all_1.gstreamer
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-good
+              gst_all_1.gst-plugins-bad
+              librsvg
+              freetype
+              expat
+            ])
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.Security
             ];
-          buildInputs =
-            buildInputs
-            ++ [
-              rustTarget
-            ];
 
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
+          languages.java = {
+            enable = true;
+            gradle.enable = true;
+            jdk.package = pkgs.jdk17;
+          };
+          languages.kotlin.enable = true;
+
+          languages.javascript = {
+            enable = true;
+            corepack.enable = true;
+          };
+
+          languages.rust.enable = true;
+
+          enterShell = with pkgs; ''
           '';
         };
-      }
-    );
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
 }
