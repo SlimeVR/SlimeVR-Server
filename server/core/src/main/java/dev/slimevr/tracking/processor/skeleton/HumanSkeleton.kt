dba02efb422657d6a3e82c75c2077307c1b8a829
Collin Kees <Collin@kees.net>
2023-09-19 16:49:13 -0700

    commit starting code

--- server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.kt
@@ -146,6 +146,7 @@ class HumanSkeleton(
 	var tapDetectionManager = TapDetectionManager(this)
 	var viveEmulation = ViveEmulation(this)
 	var localizer = Localizer(this)
+	var ikSolver = IKSolver(headBone)
 
 	// Constructors
 	init {
@@ -303,6 +304,9 @@ class HumanSkeleton(
 
 		// Update tap detection's trackers
 		tapDetectionManager.updateConfig(trackers)
+
+		// Rebuild Ik Solver
+		ikSolver.buildChains(trackers)
 	}
 
 	/**
@@ -361,6 +365,7 @@ class HumanSkeleton(
 
 		updateTransforms()
 		updateBones()
+		ikSolver.solve()
 		updateComputedTrackers()
 
 		// Don't run post-processing if the tracking is paused
