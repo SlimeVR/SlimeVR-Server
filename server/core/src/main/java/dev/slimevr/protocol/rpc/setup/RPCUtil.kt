3fd427c02514c93fe01242ee6162c35ead17e181
Uriel <urielfontan2002@gmail.com>
2023-09-20 03:18:09 +0000

    Fix RPCUtil not checking for the interface before looking for it

--- server/core/src/main/java/dev/slimevr/protocol/rpc/setup/RPCUtil.kt
+++ server/core/src/main/java/dev/slimevr/protocol/rpc/setup/RPCUtil.kt
@@ -6,7 +6,7 @@ object RPCUtil {
 	@JvmStatic
 	fun getLocalIp(): String =
 		NetworkInterface.getNetworkInterfaces().asSequence().first { netInt ->
-			netInt.isUp && !netInt.isLoopback && !netInt.isVirtual && netInt.interfaceAddresses.any { it.broadcast != null }
+			netInt.isUp && !netInt.isLoopback && !netInt.isVirtual && netInt.interfaceAddresses.any { it.address.isSiteLocalAddress && it.broadcast != null }
 		}.interfaceAddresses.first {
 			it.address.isSiteLocalAddress && it.broadcast != null
 		}.address.hostAddress
