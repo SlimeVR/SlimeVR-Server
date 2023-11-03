d37466cf9f39db6ca5bfc9b9389d9b7923de113a
Uriel <imurx@proton.me>
2023-11-03 14:29:19 -0300

    Skeleton preview (#817)

--- gui/src/components/tracker/TrackerSettings.tsx
+++ gui/src/components/tracker/TrackerSettings.tsx
@@ -399,7 +399,7 @@ export function TrackerSettingsPage() {
               type="text"
               name="trackerName"
               control={control}
-              autocomplete="on"
+              autocomplete="off"
               rules={undefined}
               label="Tracker name"
             ></Input>
