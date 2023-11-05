875f6c4779e01e5f4ac1267ec2e9b588615de993
Uriel <urielfontan2002@gmail.com>
2023-11-05 04:34:42 -0300

    solve lint issues

--- gui/src/hooks/countdown.ts
+++ gui/src/hooks/countdown.ts
@@ -11,7 +11,7 @@ export function useCountdown({
 }) {
   const [isCounting, setIsCounting] = useState(false);
   const [timer, setDisplayTimer] = useState(0);
-  const countdownTimer = useRef<NodeJS.Timeout>();
+  const countdownTimer = useRef<number>();
   const counter = useRef(0);
 
   const startCountdown = () => {
