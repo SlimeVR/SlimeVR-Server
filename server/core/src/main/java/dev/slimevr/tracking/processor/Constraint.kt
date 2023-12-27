94a904ee912922efbe59297841a128fe7d35b1ee
Collin Kees <Collin@kees.net>
2023-12-27 15:41:30 -0800

    add apply Constraint Inverse

--- server/core/src/main/java/dev/slimevr/tracking/processor/Constraint.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/Constraint.kt
@@ -7,5 +7,18 @@ import io.github.axisangles.ktmath.Vector3
  * Represents the rotational limits of a Bone relative to its parent
  */
 abstract class Constraint {
+	/**
+	 * Apply constraints to the direction vector
+	 */
 	abstract fun applyConstraint(direction: Vector3, parent: Bone?): Quaternion
+
+	/**
+	 * Apply constraints to the direction vector such that when
+	 * inverted the vector would be within the constraints.
+	 * This is used for constraining direction vectors on the backwards pass
+	 * of the FABRIK solver.
+	 */
+	fun applyConstraintInverse(direction: Vector3, parent: Bone?) : Vector3 {
+		return -applyConstraint(-direction, parent).sandwich(Vector3.NEG_Y)
+	}
 }
