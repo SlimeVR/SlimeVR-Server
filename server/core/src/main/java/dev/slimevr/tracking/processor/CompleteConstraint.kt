d793bbf124c5fb75190acd7a31b92e310fa86212
Collin Kees <Collin@kees.net>
2023-12-28 14:59:37 -0800

    modify applyConstraint + add CompleteConstraint

--- server/core/src/main/java/dev/slimevr/tracking/processor/CompleteConstraint.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/CompleteConstraint.kt
@@ -3,8 +3,11 @@ package dev.slimevr.tracking.processor
 import io.github.axisangles.ktmath.Quaternion
 import io.github.axisangles.ktmath.Vector3
 
-class HingeConstraint(val min: Float, val max: Float) : Constraint()  {
+/**
+ * A constraint type that allows no modifications to the rotation
+ */
+class CompleteConstraint : Constraint() {
 	override fun applyConstraint(direction: Vector3, thisBone: Bone): Quaternion {
-		return Quaternion.fromTo(Vector3.NEG_Y, direction).unit()
+		return thisBone.getGlobalRotation()
 	}
 }
