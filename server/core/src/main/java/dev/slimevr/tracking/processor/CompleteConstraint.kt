9bdf7ee2b667f81210c77012be69687eef7acab4
Collin Kees <Collin@kees.net>
2024-01-01 16:07:26 -0800

    account for bone.rotationOffset + change input to constrainRotation

--- server/core/src/main/java/dev/slimevr/tracking/processor/CompleteConstraint.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/CompleteConstraint.kt
@@ -7,7 +7,7 @@ import io.github.axisangles.ktmath.Vector3
  * A constraint type that allows no modifications to the rotation
  */
 class CompleteConstraint : Constraint() {
-	override fun constraintRotation(direction: Vector3, thisBone: Bone): Quaternion {
+	override fun constraintRotation(rotation: Quaternion, thisBone: Bone): Quaternion {
 		return thisBone.getGlobalRotation()
 	}
 }
