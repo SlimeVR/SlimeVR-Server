82f1995c1fb8e67b8776f4d472c1394930318384
Uriel <imurx@proton.me>
2024-02-09 19:26:19 -0300

    finish autobone video

--- gui/src/components/onboarding/pages/body-proportions/autobone-steps/Requirements.tsx
+++ gui/src/components/onboarding/pages/body-proportions/autobone-steps/Requirements.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/commons/Button';
 import { Typography } from '@/components/commons/Typography';
+import { PlayCircleIcon } from '@/components/commons/icon/PlayIcon';
 import { useLocalization } from '@fluent/react';
+import { useRef, useState } from 'react';
 
 export function RequirementsStep({
   nextStep,
@@ -12,6 +14,19 @@ export function RequirementsStep({
   variant: 'onboarding' | 'alone';
 }) {
   const { l10n } = useLocalization();
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [paused, setPaused] = useState(true);
+
+  function toggleVideo() {
+    if (!videoRef.current) return;
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+      videoRef.current.fastSeek(0);
+    }
+    setPaused(videoRef.current.paused);
+  }
 
   return (
     <>
@@ -37,24 +52,29 @@ export function RequirementsStep({
                   ))}
               </>
             </ul>
-            <div className="relative h-fit">
+            <button
+              className="relative appearance-none h-fit"
+              onClick={toggleVideo}
+            >
+              <div
+                className="absolute w-[100px] h-[100px] top-0 bottom-0 left-0 right-0 m-auto fill-background-20"
+                hidden={!paused}
+              >
+                <PlayCircleIcon width={100}></PlayCircleIcon>
+              </div>
+
               <video
+                preload=""
+                ref={videoRef}
                 src="/images/autobone.webm"
                 className="min-w-[12rem]"
                 muted
                 loop
-                onClick={(ev) => {
-                  if (!(ev.target instanceof HTMLVideoElement)) return;
-                  if (ev.target.paused) {
-                    ev.target.play();
-                  } else {
-                    ev.target.pause();
-                    ev.target.fastSeek(0);
-                  }
-                }}
+                playsInline
+                controls={false}
+                poster="/images/autobone-poster.webp"
               ></video>
-              <div className="absolute w-5 top-0 bottom-0 left-0 right-0 m-auto h-5 z-10"></div>
-            </div>
+            </button>
           </div>
         </div>
 
