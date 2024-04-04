67e29410de229a4d91709fe9857e1ea5744ee054
Butterscotch! <bscotchvanilla@gmail.com>
2024-04-04 19:58:21 -0400

    Fix AutoBone HMD height

--- server/core/src/main/java/dev/slimevr/autobone/AutoBone.kt
+++ server/core/src/main/java/dev/slimevr/autobone/AutoBone.kt
@@ -4,6 +4,7 @@ import dev.slimevr.SLIMEVR_IDENTIFIER
 import dev.slimevr.VRServer
 import dev.slimevr.autobone.errors.*
 import dev.slimevr.config.AutoBoneConfig
+import dev.slimevr.config.SkeletonConfig
 import dev.slimevr.poseframeformat.PoseFrameIO
 import dev.slimevr.poseframeformat.PoseFrames
 import dev.slimevr.tracking.processor.HumanPoseManager
@@ -22,7 +23,7 @@ import java.util.function.Consumer
 import java.util.function.Function
 import kotlin.math.*
 
-class AutoBone(server: VRServer) {
+class AutoBone(private val server: VRServer) {
 	// This is filled by loadConfigValues()
 	val offsets = EnumMap<SkeletonConfigOffsets, Float>(
 		SkeletonConfigOffsets::class.java,
@@ -48,8 +49,6 @@ class AutoBone(server: VRServer) {
 	// The total height of the normalized adjusted offsets
 	var adjustedHeightNormalized: Float = 1f
 
-	private val server: VRServer
-
 	// #region Error functions
 	var slideError = SlideError()
 	var offsetSlideError = OffsetSlideError()
@@ -62,11 +61,10 @@ class AutoBone(server: VRServer) {
 
 	private val rand = Random()
 
-	val globalConfig: AutoBoneConfig
+	val globalConfig: AutoBoneConfig = server.configManager.vrConfig.autoBone
+	val globalSkeletonConfig: SkeletonConfig = server.configManager.vrConfig.skeleton
 
 	init {
-		globalConfig = server.configManager.vrConfig.autoBone
-		this.server = server
 		loadConfigValues()
 	}
 
@@ -193,14 +191,15 @@ class AutoBone(server: VRServer) {
 	fun processFrames(
 		frames: PoseFrames,
 		config: AutoBoneConfig = globalConfig,
+		skeletonConfig: SkeletonConfig = globalSkeletonConfig,
 		epochCallback: Consumer<Epoch>? = null,
 	): AutoBoneResults {
 		// Load current values for adjustable configs
 		loadConfigValues()
 
 		// Set the target heights either from config or calculate them
-		val targetHmdHeight = if (config.targetHmdHeight > 0f) {
-			config.targetHmdHeight
+		val targetHmdHeight = if (skeletonConfig.hmdHeight > 0f) {
+			skeletonConfig.hmdHeight
 		} else {
 			calcTargetHmdHeight(frames, config)
 		}
