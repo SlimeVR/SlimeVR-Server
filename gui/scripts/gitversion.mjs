1cf61cb6b434bbf770df59e8dc48c5bd01f7f82c
Uriel <imurx@proton.me>
2024-04-15 15:12:55 -0300

    return valid semver always

--- gui/scripts/gitversion.mjs
+++ gui/scripts/gitversion.mjs
@@ -3,21 +3,22 @@ import { promisify } from 'node:util';
 import { exec as execCallback } from 'node:child_process';
 const exec = promisify(execCallback);
 
-const [commitHash, versionTag, gitClean] = await Promise.all([
+const [commitHash, versionTag, lastTag, gitClean] = await Promise.all([
   exec('git rev-parse --verify --short HEAD').then((res) =>
-    res.stdout.toString().trim()
+    res.stdout.trim().substring(1)
   ),
   exec('git --no-pager tag --sort -taggerdate --points-at HEAD').then((res) =>
-    res.stdout.toString().split('\n')[0].trim()
+    res.stdout.split('\n')[0].trim()
+  ),
+  exec('git describe --tags --abbrev=0').then((res) =>
+    res.stdout.trim().substring(1)
   ),
   // If not empty then it's not clean
-  exec('git status --porcelain').then((res) =>
-    res.stdout.toString() ? false : true
-  ),
+  exec('git status --porcelain').then((res) => (res.stdout ? false : true)),
 ]);
 
 console.log(
   JSON.stringify({
-    version: `${versionTag || commitHash}${gitClean ? '' : '-dirty'}`,
+    version: `${versionTag || `${lastTag}-${commitHash}`}${gitClean ? '' : '-dirty'}`,
   })
 );
