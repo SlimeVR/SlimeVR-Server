00ffec83bea7efc061ceb93cd42a91c1c96b2c2a
Uriel <imurx@proton.me>
2024-04-15 15:23:12 -0300

    fix not having any previous tag

--- gui/scripts/gitversion.mjs
+++ gui/scripts/gitversion.mjs
@@ -3,15 +3,12 @@ import { promisify } from 'node:util';
 import { exec as execCallback } from 'node:child_process';
 const exec = promisify(execCallback);
 
-const [commitHash, versionTag, lastTag, gitClean] = await Promise.all([
+const [commitHash, versionTag, gitClean] = await Promise.all([
   exec('git rev-parse --verify --short HEAD').then((res) =>
     res.stdout.trim().substring(1)
   ),
   exec('git --no-pager tag --sort -taggerdate --points-at HEAD').then((res) =>
     res.stdout.split('\n')[0].trim()
-  ),
-  exec('git describe --tags --abbrev=0').then((res) =>
-    res.stdout.trim().substring(1)
   ),
   // If not empty then it's not clean
   exec('git status --porcelain').then((res) => (res.stdout ? false : true)),
@@ -19,6 +16,6 @@ const [commitHash, versionTag, lastTag, gitClean] = await Promise.all([
 
 console.log(
   JSON.stringify({
-    version: `${versionTag || `${lastTag}-${commitHash}`}${gitClean ? '' : '-dirty'}`,
+    version: `${versionTag || `0.0.0-${commitHash}`}${gitClean ? '' : '-dirty'}`,
   })
 );
