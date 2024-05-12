121c43480112efe24ce8a9003c873c33e957d376
VocalFan <45863583+VocalFan@users.noreply.github.com>
2024-05-12 19:37:23 -0400

    One and done!

--- server/core/resources/firewall.bat
+++ server/core/resources/firewall.bat
@@ -1,17 +1,33 @@
 @echo off
+:: Firewall modification doesn't work w/o admin, slimes will be sad.
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    echo Running with administrative privileges! - Needed for firewall modification!
+) else (
+    echo Requesting administrative privileges - Needed for firewall modification! 
+    :: Temp script to request admin... Works and doesn't leave a mess.
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+)
 echo Installing firewall rules...
 
 rem Discovery default port
-netsh advfirewall firewall add rule name="SlimeVR UDP 35903 incoming" dir=in action=allow protocol=UDP localport=35903
-netsh advfirewall firewall add rule name="SlimeVR UDP 35903 outgoing" dir=out action=allow protocol=UDP localport=35903
+netsh advfirewall firewall add rule name="SlimeVR UDP 35903 incoming" dir=in action=allow protocol=UDP localport=35903 enable=yes
+netsh advfirewall firewall add rule name="SlimeVR UDP 35903 outgoing" dir=out action=allow protocol=UDP localport=35903 enable=yes
 
 rem Rotational data default port
-netsh advfirewall firewall add rule name="SlimeVR UDP 6969 incoming" dir=in action=allow protocol=UDP localport=6969
-netsh advfirewall firewall add rule name="SlimeVR UDP 6969 outgoing" dir=out action=allow protocol=UDP localport=6969
+netsh advfirewall firewall add rule name="SlimeVR UDP 6969 incoming" dir=in action=allow protocol=UDP localport=6969 enable=yes
+netsh advfirewall firewall add rule name="SlimeVR UDP 6969 outgoing" dir=out action=allow protocol=UDP localport=6969 enable=yes
 
 rem WebSocket server default port
-netsh advfirewall firewall add rule name="SlimeVR TCP 21110 incoming" dir=in action=allow protocol=TCP localport=21110
-netsh advfirewall firewall add rule name="SlimeVR TCP 21110 outgoing" dir=out action=allow protocol=TCP localport=21110
+netsh advfirewall firewall add rule name="SlimeVR TCP 21110 incoming" dir=in action=allow protocol=TCP localport=21110 enable=yes
+netsh advfirewall firewall add rule name="SlimeVR TCP 21110 outgoing" dir=out action=allow protocol=TCP localport=21110 enable=yes
+
+rem OpenJDK Platform Binary access
+netsh advfirewall firewall add rule name="SlimeVR OpenJDK Platform incoming" dir=in action=allow program="%CD%\jre\bin\java.exe" enable=yes
+netsh advfirewall firewall add rule name="SlimeVR OpenJDK Platform outgoing" dir=out action=allow program="%CD%\jre\bin\java.exe" enable=yes
 
 echo Done!
 pause
--- server/core/resources/firewall_uninstall.bat
+++ server/core/resources/firewall_uninstall.bat
@@ -1,7 +1,19 @@
 @echo off
+:: Firewall modification doesn't work w/o admin, slimes will be sad.
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    echo Running with administrative privileges! - Needed for firewall modification!
+) else (
+    echo Requesting administrative privileges - Needed for firewall modification! 
+    :: Temp script to request admin... Works and doesn't leave a mess.
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+)
 echo Uninstalling firewall rules...
 
-rem Discovery defauly port
+rem Discovery default port
 netsh advfirewall firewall delete rule name="SlimeVR UDP 35903 incoming"
 netsh advfirewall firewall delete rule name="SlimeVR UDP 35903 outgoing"
 
@@ -12,6 +24,10 @@ netsh advfirewall firewall delete rule name="SlimeVR UDP 6969 outgoing"
 rem WebSocket server default port
 netsh advfirewall firewall delete rule name="SlimeVR TCP 21110 incoming"
 netsh advfirewall firewall delete rule name="SlimeVR TCP 21110 outgoing"
+
+rem OpenJDK Platform Binary access
+netsh advfirewall firewall delete rule name="SlimeVR OpenJDK Platform incoming"
+netsh advfirewall firewall delete rule name="SlimeVR OpenJDK Platform outgoing"
 
 echo Done!
 pause
