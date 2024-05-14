461767b26b9c4d3d30bfc42442daacf022ff7957
Electromuis <anton.grootes11@gmail.com>
2024-05-14 13:22:04 +0200

    Added wifi dropdown to the wizzard

--- gui/src/components/onboarding/pages/WifiCreds.tsx
+++ gui/src/components/onboarding/pages/WifiCreds.tsx
@@ -1,8 +1,10 @@
+import { useMemo } from 'react';
 import { Localized, useLocalization } from '@fluent/react';
 import { useOnboarding } from '@/hooks/onboarding';
 import { useWifiForm } from '@/hooks/wifi-form';
 import { Button } from '@/components/commons/Button';
 import { Input } from '@/components/commons/Input';
+import { Dropdown } from '@/components/commons/Dropdown';
 import { Typography } from '@/components/commons/Typography';
 import classNames from 'classnames';
 import { useTrackers } from '@/hooks/tracker';
@@ -11,13 +13,25 @@ import { useBnoExists } from '@/hooks/imu-logic';
 export function WifiCredsPage() {
   const { l10n } = useLocalization();
   const { applyProgress, state } = useOnboarding();
-  const { control, handleSubmit, submitWifiCreds, formState } = useWifiForm();
+  const { control, handleSubmit, submitWifiCreds, formState, wifiNetworks, watch } = useWifiForm();
   const { useConnectedIMUTrackers } = useTrackers();
   const connectedIMUTrackers = useConnectedIMUTrackers();
 
   applyProgress(0.2);
 
   const bnoExists = useBnoExists(connectedIMUTrackers);
+
+  const wifiNetworksDropdownItems = useMemo(() => {
+    const networksMapped = wifiNetworks.map((network) => ({
+      label: network.ssid,
+      value: network.ssid,
+    }));
+
+    return [
+      ...networksMapped,
+      { label: 'Other', value: 'other' },
+    ];
+  }, [wifiNetworks]);
 
   return (
     <form
@@ -57,7 +71,29 @@ export function WifiCredsPage() {
               state.alonePage && 'bg-background-60'
             )}
           >
+
+          {wifiNetworks.length > 0 && <>
+            <Typography bold>
+              {l10n.getString('onboarding-wifi_creds-ssidSelect-label')}
+            </Typography>
             <Localized
+              id="onboarding-wifi_creds-ssidSelect"
+              attrs={{ placeholder: true }}
+            >
+              <Dropdown
+                control={control}
+                rules={{ required: true }}
+                name="ssidSelect"
+                variant="primary"
+                placeholder="SSID"
+                direction="down"
+                display="block"
+                items={wifiNetworksDropdownItems}
+              />
+            </Localized>
+            </>}
+
+            {(wifiNetworks.length === 0 || watch('ssidSelect') == 'other') && <Localized
               id="onboarding-wifi_creds-ssid"
               attrs={{ placeholder: true, label: true }}
             >
@@ -70,7 +106,8 @@ export function WifiCredsPage() {
                 placeholder="ssid"
                 variant="secondary"
               />
-            </Localized>
+            </Localized>}
+
             <Localized
               id="onboarding-wifi_creds-password"
               attrs={{ placeholder: true, label: true }}
