ba8bace994d7d342f3ac3c3b658e1421bd71016b
VocalFan <45863583+VocalFan@users.noreply.github.com>
2024-05-14 19:54:06 -0400

    We make things even BETTER, HOLY SHIT

--- server/core/resources/firewall.bat
+++ server/core/resources/firewall.bat
@@ -11,23 +11,35 @@ if %errorlevel% == 0 (
     "%temp%\getadmin.vbs"
     exit /B
 )
+
 echo Installing firewall rules...
 
-rem Discovery default port
-netsh advfirewall firewall add rule name="SlimeVR UDP 35903 incoming" dir=in action=allow protocol=UDP localport=35903 enable=yes
-netsh advfirewall firewall add rule name="SlimeVR UDP 35903 outgoing" dir=out action=allow protocol=UDP localport=35903 enable=yes
-
-rem Rotational data default port
-netsh advfirewall firewall add rule name="SlimeVR UDP 6969 incoming" dir=in action=allow protocol=UDP localport=6969 enable=yes
-netsh advfirewall firewall add rule name="SlimeVR UDP 6969 outgoing" dir=out action=allow protocol=UDP localport=6969 enable=yes
-
-rem WebSocket server default port
-netsh advfirewall firewall add rule name="SlimeVR TCP 21110 incoming" dir=in action=allow protocol=TCP localport=21110 enable=yes
-netsh advfirewall firewall add rule name="SlimeVR TCP 21110 outgoing" dir=out action=allow protocol=TCP localport=21110 enable=yes
-
-rem OpenJDK Platform Binary access
-netsh advfirewall firewall add rule name="SlimeVR OpenJDK Platform incoming" dir=in action=allow program="%CD%\jre\bin\java.exe" enable=yes
-netsh advfirewall firewall add rule name="SlimeVR OpenJDK Platform outgoing" dir=out action=allow program="%CD%\jre\bin\java.exe" enable=yes
+call :AddRule "SlimeVR UDP 35903 incoming" "dir=in action=allow protocol=UDP localport=35903 enable=yes"
+call :AddRule "SlimeVR UDP 35903 outgoing" "dir=out action=allow protocol=UDP localport=35903 enable=yes"
+call :AddRule "SlimeVR UDP 6969 incoming" "dir=in action=allow protocol=UDP localport=6969 enable=yes"
+call :AddRule "SlimeVR UDP 6969 outgoing" "dir=out action=allow protocol=UDP localport=6969 enable=yes"
+call :AddRule "SlimeVR TCP 21110 incoming" "dir=in action=allow protocol=TCP localport=21110 enable=yes"
+call :AddRule "SlimeVR TCP 21110 outgoing" "dir=out action=allow protocol=TCP localport=21110 enable=yes"
+call :AddRule "SlimeVR OpenJDK Platform incoming" "dir=in action=allow program=%~dp0jre\bin\java.exe enable=yes"
+call :AddRule "SlimeVR OpenJDK Platform outgoing" "dir=out action=allow program=%~dp0jre\bin\java.exe enable=yes"
 
 echo Done!
 pause
+exit /B
+
+:: Check and don't make duplicate rules :3
+:AddRule
+setlocal
+set "ruleName=%~1"
+set "ruleParams=%~2"
+
+netsh advfirewall firewall show rule name="%ruleName%" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Adding rule: %ruleName%
+    netsh advfirewall firewall add rule name="%ruleName%" %ruleParams%
+) else (
+    echo Rule already exists: %ruleName%
+)
+
+endlocal
+exit /B
--- server/core/resources/firewall_uninstall.bat
+++ server/core/resources/firewall_uninstall.bat
@@ -11,23 +11,34 @@ if %errorlevel% == 0 (
     "%temp%\getadmin.vbs"
     exit /B
 )
+
 echo Uninstalling firewall rules...
 
-rem Discovery default port
-netsh advfirewall firewall delete rule name="SlimeVR UDP 35903 incoming"
-netsh advfirewall firewall delete rule name="SlimeVR UDP 35903 outgoing"
-
-rem Rotational data default port
-netsh advfirewall firewall delete rule name="SlimeVR UDP 6969 incoming"
-netsh advfirewall firewall delete rule name="SlimeVR UDP 6969 outgoing"
-
-rem WebSocket server default port
-netsh advfirewall firewall delete rule name="SlimeVR TCP 21110 incoming"
-netsh advfirewall firewall delete rule name="SlimeVR TCP 21110 outgoing"
-
-rem OpenJDK Platform Binary access
-netsh advfirewall firewall delete rule name="SlimeVR OpenJDK Platform incoming"
-netsh advfirewall firewall delete rule name="SlimeVR OpenJDK Platform outgoing"
+call :DeleteRule "SlimeVR UDP 35903 incoming"
+call :DeleteRule "SlimeVR UDP 35903 outgoing"
+call :DeleteRule "SlimeVR UDP 6969 incoming"
+call :DeleteRule "SlimeVR UDP 6969 outgoing"
+call :DeleteRule "SlimeVR TCP 21110 incoming"
+call :DeleteRule "SlimeVR OpenJDK Platform outgoing"
+call :DeleteRule "SlimeVR TCP 21110 outgoing"
+call :DeleteRule "SlimeVR OpenJDK Platform incoming"
 
 echo Done!
 pause
+exit /B
+
+:: Delete rules if they exist :3
+:DeleteRule
+setlocal
+set "ruleName=%~1"
+
+netsh advfirewall firewall show rule name="%ruleName%" >nul 2>&1
+if %errorlevel% == 0 (
+    echo Deleting rule: %ruleName%
+    netsh advfirewall firewall delete rule name="%ruleName%"
+) else (
+    echo Rule does not exist: %ruleName%
+)
+
+endlocal
+exit /B
