c82b2a93e52ed2b250738ce7b92a2f36ca1e7259
VocalFan <45863583+VocalFan@users.noreply.github.com>
2024-05-14 20:29:14 -0400

    Clean up my goofy ah formatting

--- server/core/resources/firewall_uninstall.bat
+++ server/core/resources/firewall_uninstall.bat
@@ -14,14 +14,18 @@ if %errorlevel% == 0 (
 
 echo Uninstalling firewall rules...
 
+rem Discovery default port
 call :DeleteRule "SlimeVR UDP 35903 incoming"
 call :DeleteRule "SlimeVR UDP 35903 outgoing"
+rem Rotational data default port
 call :DeleteRule "SlimeVR UDP 6969 incoming"
 call :DeleteRule "SlimeVR UDP 6969 outgoing"
+rem WebSocket server default port
 call :DeleteRule "SlimeVR TCP 21110 incoming"
-call :DeleteRule "SlimeVR OpenJDK Platform outgoing"
 call :DeleteRule "SlimeVR TCP 21110 outgoing"
+rem OpenJDK Platform Binary access
 call :DeleteRule "SlimeVR OpenJDK Platform incoming"
+call :DeleteRule "SlimeVR OpenJDK Platform outgoing"
 
 echo Done!
 pause
