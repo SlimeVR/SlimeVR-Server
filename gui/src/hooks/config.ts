d9da5544bbdc784ef5aca46ebe4123b02763d6e3
Ondrej Hruska <11602729+ZycaR@users.noreply.github.com>
2024-06-17 08:45:06 -0700

    GUI - Tracker assignment options (#1009)
    Co-authored-by: Erimel <marioluigivideo@gmail.com>
    Co-authored-by: loucass003 <loucass003@gmail.com>

--- gui/src/hooks/config.ts
+++ gui/src/hooks/config.ts
@@ -13,6 +13,14 @@ export interface WindowConfig {
   y: number;
 }
 
+export enum AssignMode {
+  LowerBody = 'lower-body',
+  Core = 'core',
+  EnhancedCore = 'enhanced-core',
+  FullBody = 'full-body',
+  All = 'all',
+}
+
 export interface Config {
   debug: boolean;
   lang: string;
@@ -25,10 +33,10 @@ export interface Config {
   theme: string;
   textSize: number;
   fonts: string[];
-  advancedAssign: boolean;
   useTray: boolean | null;
   doneManualMounting: boolean;
   mirrorView: boolean;
+  assignMode: AssignMode;
   discordPresence: boolean;
 }
 
@@ -51,10 +59,10 @@ export const defaultConfig: Omit<Config, 'devSettings'> = {
   theme: 'slime',
   textSize: 12,
   fonts: ['poppins'],
-  advancedAssign: false,
   useTray: null,
   doneManualMounting: false,
   mirrorView: true,
+  assignMode: AssignMode.Core,
   discordPresence: false,
 };
 
