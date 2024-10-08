6b3aca84226bed269c9736b37cdb8a2581c397e8
loucass003 <loucass003@gmail.com>
2024-10-08 19:10:01 +0200

    Revert back to api

--- server/core/build.gradle.kts
+++ server/core/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 	implementation("com.mayakapps.kache:kache:2.0.0-rc02")
 
-	implementation("com.github.loucass003:EspflashKotlin:v0.10.0")
+	api("com.github.loucass003:EspflashKotlin:v0.10.0")
 
 	// Allow the use of reflection
 	implementation(kotlin("reflect"))
