066760bf7987eefdb98e0e6afa490f663c5262a6
Uriel <imurx@proton.me>
2024-10-10 14:58:13 +0200

    Update all gradle dependencies we have (#1173)

--- server/desktop/build.gradle.kts
+++ server/desktop/build.gradle.kts
@@ -5,12 +5,13 @@
  * For more details take a look at the Java Libraries chapter in the Gradle
  * User Manual available at https://docs.gradle.org/6.3/userguide/java_library_plugin.html
  */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm")
 	application
-	id("com.github.johnrengelman.shadow")
+	id("com.gradleup.shadow")
 	id("com.github.gmazzo.buildconfig")
 	id("org.ajoberstar.grgit")
 }
@@ -26,8 +27,10 @@ java {
 	}
 }
 tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "17"
-	kotlinOptions.freeCompilerArgs += "-Xvalue-classes"
+	compilerOptions {
+		jvmTarget.set(JvmTarget.JVM_17)
+		freeCompilerArgs.set(listOf("-Xvalue-classes"))
+	}
 }
 
 // Set compiler to use UTF-8
@@ -54,8 +57,8 @@ dependencies {
 	implementation(project(":server:core"))
 	implementation(project(":solarxr-protocol"))
 
-	implementation("commons-cli:commons-cli:1.5.0")
-	implementation("org.apache.commons:commons-lang3:3.12.0")
+	implementation("commons-cli:commons-cli:1.8.0")
+	implementation("org.apache.commons:commons-lang3:3.15.0")
 	implementation("com.google.protobuf:protobuf-java:3.21.12")
 	implementation("net.java.dev.jna:jna:5.+")
 	implementation("net.java.dev.jna:jna-platform:5.+")
