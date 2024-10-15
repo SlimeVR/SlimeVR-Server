7f6fdcaa390d09d1308eaac9dc1bf62255ebcea9
Collin Kees <Collin@kees.net>
2024-10-15 14:33:49 -0700

    Adjust hand constraints and fix constraint corrections interaction with ik solver

--- server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/HumanSkeleton.kt
@@ -63,8 +63,8 @@ class HumanSkeleton(
 	val rightUpperArmBone = Bone(BoneType.RIGHT_UPPER_ARM, Constraint(ConstraintType.TWIST_SWING, 120f, 180f))
 	val leftLowerArmBone = Bone(BoneType.LEFT_LOWER_ARM, Constraint(ConstraintType.LOOSE_HINGE, 0f, -180f, 40f))
 	val rightLowerArmBone = Bone(BoneType.RIGHT_LOWER_ARM, Constraint(ConstraintType.LOOSE_HINGE, 0f, -180f, 40f))
-	val leftHandBone = Bone(BoneType.LEFT_HAND, Constraint(ConstraintType.TWIST_SWING, 90f, 90f))
-	val rightHandBone = Bone(BoneType.RIGHT_HAND, Constraint(ConstraintType.TWIST_SWING, 90f, 90f))
+	val leftHandBone = Bone(BoneType.LEFT_HAND, Constraint(ConstraintType.TWIST_SWING, 120f, 120f))
+	val rightHandBone = Bone(BoneType.RIGHT_HAND, Constraint(ConstraintType.TWIST_SWING, 120f, 120f))
 
 	// Finger bones
 	val leftThumbMetacarpalBone = Bone(BoneType.LEFT_THUMB_METACARPAL)
@@ -548,10 +548,10 @@ class HumanSkeleton(
 			val parentTracker = getTrackerForBone(bone.parent?.boneType)
 			if ((angle > 0.01f) && (
 					tracker?.filteringHandler?.getFilteringImpact()
-						?: 0f
+						?: 1f
 					) < 0.01f && (
 					parentTracker?.filteringHandler?.getFilteringImpact()
-						?: 0f
+						?: 1f
 					) < 0.01f
 			) {
 				tracker?.resetsHandler?.updateDynamicFix(deltaRot)
