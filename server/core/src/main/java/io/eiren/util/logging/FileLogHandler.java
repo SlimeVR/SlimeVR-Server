5244d8a93a968b2109c744f8dbafe9b0e1a7493f
Butterscotch! <bscotchvanilla@gmail.com>
2024-12-14 02:13:55 -0500

    Document and handle exceptions better

--- server/core/src/main/java/io/eiren/util/logging/FileLogHandler.java
+++ server/core/src/main/java/io/eiren/util/logging/FileLogHandler.java
@@ -52,34 +52,28 @@ public class FileLogHandler extends StreamHandler {
 	private int fileCount = 0;
 
 	public FileLogHandler(
-		Path path,
-		String logTag,
-		DateTimeFormatter dateFormat,
+		@NotNull Path path,
+		@NotNull String logTag,
+		@NotNull DateTimeFormatter dateFormat,
 		int limit,
 		int count
 	) {
+		assert (count > 0);
+
 		this.path = path;
 		this.logTag = logTag;
+
 		this.dateFormat = dateFormat;
 		this.dateTime = LocalDateTime.now();
 		this.date = dateTime.format(dateFormat);
+
 		this.limit = limit;
 		this.maxCount = count;
 
+		// Find old logs to manage
 		logFiles = findLogs(path);
-
+		// Create new log and delete over the count
 		newFile();
-	}
-
-	private synchronized void deleteFile(File file) {
-		if (!file.delete()) {
-			file.deleteOnExit();
-			reportError(
-				"Failed to delete file, deleting on exit.",
-				null,
-				ErrorManager.GENERIC_FAILURE
-			);
-		}
 	}
 
 	private DatedLogFile parseFileName(File file) {
@@ -102,14 +96,20 @@ public class FileLogHandler extends StreamHandler {
 			return null;
 		}
 
-		// Move past the tag, then between the two '_'
-		String dateTimeStr = name.substring(logTag.length() + 1, dateEnd);
-		LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, dateFormat);
+		try {
+			// Move past the tag, then between the two '_'
+			String dateTimeStr = name.substring(logTag.length() + 1, dateEnd);
+			LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, dateFormat);
 
-		// Move past the date '_' and behind the suffix
-		int logNum = Integer.parseInt(name, dateEnd + 1, name.length() - logSuffix.length(), 10);
+			// Move past the date '_' and behind the suffix
+			int logNum = Integer
+				.parseInt(name, dateEnd + 1, name.length() - logSuffix.length(), 10);
 
-		return new DatedLogFile(file, dateTime, logNum);
+			return new DatedLogFile(file, dateTime, logNum);
+		} catch (Exception e) {
+			// Unable to parse log file, probably not valid
+			return null;
+		}
 	}
 
 	private ArrayList<DatedLogFile> findLogs(Path path) {
@@ -119,6 +119,7 @@ public class FileLogHandler extends StreamHandler {
 		if (files == null)
 			return logFiles;
 
+		// Find all parseable log files
 		for (File log : files) {
 			DatedLogFile parsedFile = parseFileName(log);
 			if (parsedFile != null) {
@@ -129,7 +130,18 @@ public class FileLogHandler extends StreamHandler {
 		return logFiles;
 	}
 
-	private synchronized void deleteEarliestFile() {
+	private void deleteFile(File file) {
+		if (!file.delete()) {
+			file.deleteOnExit();
+			reportError(
+				"Failed to delete file, deleting on exit.",
+				null,
+				ErrorManager.GENERIC_FAILURE
+			);
+		}
+	}
+
+	private void deleteEarliestFile(ArrayList<DatedLogFile> logFiles) {
 		DatedLogFile earliest = null;
 
 		for (DatedLogFile log : logFiles) {
@@ -147,18 +159,21 @@ public class FileLogHandler extends StreamHandler {
 	private synchronized void newFile() {
 		// Delete files over the count
 		while (logFiles.size() >= maxCount) {
-			deleteEarliestFile();
+			deleteEarliestFile(logFiles);
 		}
 
 		try {
 			Path logPath = path.resolve(logTag + "_" + date + "_" + fileCount + logSuffix);
 			File newFile = logPath.toFile();
 
+			// Use DataOutputStream to count bytes written
 			curStream = new DataOutputStream(
 				new BufferedOutputStream(new FileOutputStream(newFile))
 			);
+			// Closes the last stream automatically
 			setOutputStream(curStream);
 
+			// Add log to the tracking list to be deleted if needed
 			logFiles.add(new DatedLogFile(newFile, dateTime, fileCount));
 			fileCount += 1;
 		} catch (FileNotFoundException e) {
@@ -175,6 +190,7 @@ public class FileLogHandler extends StreamHandler {
 		super.publish(record);
 		flush();
 
+		// If written above the log limit, make a new file
 		if (limit > 0 && curStream.size() >= limit) {
 			newFile();
 		}
