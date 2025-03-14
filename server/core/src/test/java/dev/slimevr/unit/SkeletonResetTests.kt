48083d6549e5aabfa6ac9f071c01077d187c55c6
Butterscotch! <bscotchvanilla@gmail.com>
2025-03-14 04:12:02 -0400

    Make TestTrackerSet

--- server/core/src/test/java/dev/slimevr/unit/SkeletonResetTests.kt
+++ server/core/src/test/java/dev/slimevr/unit/SkeletonResetTests.kt
@@ -1,15 +1,10 @@
 package dev.slimevr.unit
 
 import com.jme3.math.FastMath
-import dev.slimevr.VRServer.Companion.getNextLocalTrackerId
 import dev.slimevr.tracking.processor.HumanPoseManager
-import dev.slimevr.tracking.trackers.Tracker
 import dev.slimevr.tracking.trackers.TrackerPosition
-import dev.slimevr.tracking.trackers.TrackerStatus
-import dev.slimevr.tracking.trackers.udp.IMUType
 import dev.slimevr.unit.TrackerTestUtils.assertAnglesApproxEqual
 import dev.slimevr.unit.TrackerTestUtils.quatApproxEqual
-import io.eiren.util.collections.FastList
 import io.github.axisangles.ktmath.EulerAngles
 import io.github.axisangles.ktmath.EulerOrder
 import io.github.axisangles.ktmath.Quaternion
@@ -23,31 +18,17 @@ class SkeletonResetTests {
 	@Test
 	fun testSkeletonReset() {
 		val rand = Random(42)
-
-		val hmd = mkTrack(TrackerPosition.HEAD, true, true, false)
-		val chest = mkTrack(TrackerPosition.CHEST)
-		val hip = mkTrack(TrackerPosition.HIP)
-
-		val upperLeft = mkTrack(TrackerPosition.LEFT_UPPER_LEG)
-		val lowerLeft = mkTrack(TrackerPosition.LEFT_LOWER_LEG)
-
-		val upperRight = mkTrack(TrackerPosition.RIGHT_UPPER_LEG)
-		val lowerRight = mkTrack(TrackerPosition.RIGHT_LOWER_LEG)
-
-		// Collect all our trackers
-		val tracks = arrayOf(chest, hip, upperLeft, lowerLeft, upperRight, lowerRight)
-		val tracksWithHmd = tracks.plus(hmd)
-		val trackerList = FastList(tracksWithHmd)
+		val trackers = TestTrackerSet()
 
 		// Initialize skeleton and everything
-		val hpm = HumanPoseManager(trackerList)
+		val hpm = HumanPoseManager(trackers.allL)
 
 		val headRot1 = EulerAngles(EulerOrder.YZX, 0f, FastMath.HALF_PI, FastMath.QUARTER_PI).toQuaternion()
 		val expectRot1 = EulerAngles(EulerOrder.YZX, 0f, FastMath.HALF_PI, 0f).toQuaternion()
 
 		// Randomize tracker orientations, these should be zeroed and matched to the
 		// headset yaw by full reset
-		for (tracker in tracks) {
+		for (tracker in trackers.set) {
 			val init = EulerAngles(
 				EulerOrder.YZX,
 				rand.nextFloat() * FastMath.TWO_PI,
@@ -56,10 +37,10 @@ class SkeletonResetTests {
 			).toQuaternion()
 			tracker.setRotation(init)
 		}
-		hmd.setRotation(headRot1)
+		trackers.head.setRotation(headRot1)
 		hpm.resetTrackersFull(resetSource)
 
-		for (tracker in tracks) {
+		for (tracker in trackers.set) {
 			val actual = tracker.getRotation()
 			assert(quatApproxEqual(expectRot1, actual)) {
 				"\"${tracker.name}\" did not reset to the reference rotation. Expected <$expectRot1>, actual <$actual>."
@@ -68,7 +49,7 @@ class SkeletonResetTests {
 
 		// Randomize full tracker orientations, these should match the headset yaw but
 		// retain orientation otherwise
-		for (tracker in tracks) {
+		for (tracker in trackers.set) {
 			val init = EulerAngles(
 				EulerOrder.YZX,
 				rand.nextFloat() * FastMath.TWO_PI,
@@ -77,10 +58,10 @@ class SkeletonResetTests {
 			).toQuaternion()
 			tracker.setRotation(init)
 		}
-		hmd.setRotation(Quaternion.IDENTITY)
+		trackers.head.setRotation(Quaternion.IDENTITY)
 		hpm.resetTrackersYaw(resetSource)
 
-		for (tracker in tracks) {
+		for (tracker in trackers.set) {
 			val yaw = TrackerTestUtils.yaw(tracker.getRotation())
 			assertAnglesApproxEqual(0f, yaw, "\"${tracker.name}\" did not reset to the reference rotation.")
 		}
@@ -88,32 +69,19 @@ class SkeletonResetTests {
 
 	@Test
 	fun testSkeletonMount() {
-		val hmd = mkTrack(TrackerPosition.HEAD, true, true, false)
-		val chest = mkTrack(TrackerPosition.CHEST)
-		val hip = mkTrack(TrackerPosition.HIP)
-
-		val upperLeft = mkTrack(TrackerPosition.LEFT_UPPER_LEG)
-		val lowerLeft = mkTrack(TrackerPosition.LEFT_LOWER_LEG)
-
-		val upperRight = mkTrack(TrackerPosition.RIGHT_UPPER_LEG)
-		val lowerRight = mkTrack(TrackerPosition.RIGHT_LOWER_LEG)
-
-		// Collect all our trackers
-		val tracks = arrayOf(chest, hip, upperLeft, lowerLeft, upperRight, lowerRight)
-		val tracksWithHmd = tracks.plus(hmd)
-		val trackerList = FastList(tracksWithHmd)
+		val trackers = TestTrackerSet()
 
 		// Initialize skeleton and everything
-		val hpm = HumanPoseManager(trackerList)
+		val hpm = HumanPoseManager(trackers.allL)
 
 		// Just a bunch of random mounting orientations
 		val expected = arrayOf(
-			Pair(chest, Quaternion.SLIMEVR.FRONT),
-			Pair(hip, Quaternion.SLIMEVR.RIGHT),
-			Pair(upperLeft, Quaternion.SLIMEVR.BACK),
-			Pair(lowerLeft, Quaternion.SLIMEVR.LEFT),
-			Pair(upperRight, Quaternion.SLIMEVR.FRONT),
-			Pair(lowerRight, Quaternion.SLIMEVR.RIGHT),
+			Pair(trackers.chest, Quaternion.SLIMEVR.FRONT),
+			Pair(trackers.hip, Quaternion.SLIMEVR.RIGHT),
+			Pair(trackers.leftThigh, Quaternion.SLIMEVR.BACK),
+			Pair(trackers.leftCalf, Quaternion.SLIMEVR.LEFT),
+			Pair(trackers.rightThigh, Quaternion.SLIMEVR.FRONT),
+			Pair(trackers.rightCalf, Quaternion.SLIMEVR.RIGHT),
 		)
 		// Rotate the tracker to fit the expected mounting orientation
 		for ((tracker, mountRot) in expected) {
@@ -153,27 +121,6 @@ class SkeletonResetTests {
 				"\"${tracker.name}\" did not reset to the reference rotation. Expected <0.0>, actual <${actualMounting.z}>."
 			}
 		}
-	}
-
-	fun mkTrack(pos: TrackerPosition, hmd: Boolean = false, computed: Boolean = false, reset: Boolean = true): Tracker {
-		val name = "test ${pos.designation}"
-		val tracker = Tracker(
-			null,
-			getNextLocalTrackerId(),
-			name,
-			name,
-			pos,
-			hasPosition = hmd,
-			hasRotation = true,
-			isComputed = computed,
-			imuType = IMUType.UNKNOWN,
-			needsReset = true,
-			needsMounting = reset,
-			isHmd = hmd,
-			trackRotDirection = false,
-		)
-		tracker.status = TrackerStatus.OK
-		return tracker
 	}
 
 	fun mkTrackMount(rot: Quaternion): Quaternion = rot * (TrackerTestUtils.frontRot / rot)
