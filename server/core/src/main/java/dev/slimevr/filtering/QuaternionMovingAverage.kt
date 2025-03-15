7b38407ca1afba9715ff46a09d38164d871e8efd
Butterscotch! <bscotchvanilla@gmail.com>
2025-03-15 05:18:52 -0400

    Remove frame count from tracker smoothing

--- server/core/src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.kt
+++ server/core/src/main/java/dev/slimevr/filtering/QuaternionMovingAverage.kt
@@ -29,8 +29,7 @@ class QuaternionMovingAverage(
 	private var latestQuaternion = IDENTITY
 	private var smoothingQuaternion = IDENTITY
 	private val fpsTimer = if (VRServer.instanceInitialized) VRServer.instance.fpsTimer else NanoTimer()
-	private var frameCounter = 0
-	private var lastAmt = 0f
+	private var timeSinceUpdate = 0f
 
 	init {
 		// amount should range from 0 to 1.
@@ -71,22 +70,12 @@ class QuaternionMovingAverage(
 				filteredQuaternion = filteredQuaternion.interpR(quatBuf, amt)
 			}
 		} else if (type == TrackerFilters.SMOOTHING) {
-			// Increase every update for linear interpolation
-			frameCounter++
+			// Make it framerate-independent
+			timeSinceUpdate += fpsTimer.timePerFrame
 
 			// Calculate the slerp factor based off the smoothFactor and smoothingCounter
-			var amt = smoothFactor * frameCounter
-
-			// Make it framerate-independent
-			amt *= fpsTimer.timePerFrame
-
-			// Be at least last amount to not rollback
-			amt = amt.coerceAtLeast(lastAmt)
-
 			// limit to 1 to not overshoot
-			amt = amt.coerceAtMost(1f)
-
-			lastAmt = amt
+			val amt = (smoothFactor * timeSinceUpdate).coerceAtMost(1f)
 
 			// Smooth towards the target rotation by the slerp factor
 			filteredQuaternion = smoothingQuaternion.interpR(latestQuaternion, amt)
@@ -105,8 +94,7 @@ class QuaternionMovingAverage(
 			// Gets and stores the rotation between the last 2 quaternions
 			rotBuffer?.add(latestQuaternion.inv().times(q))
 		} else if (type == TrackerFilters.SMOOTHING) {
-			frameCounter = 0
-			lastAmt = 0f
+			timeSinceUpdate = 0f
 			smoothingQuaternion = filteredQuaternion
 		} else {
 			// No filtering; just keep track of rotations (for going over 180 degrees)
