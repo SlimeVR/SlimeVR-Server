c3f33c24fb0d2d5145d888bcdfbda4088e48eae2
Erimel <marioluigivideo@gmail.com>
2025-03-25 19:30:45 -0400

    Verbose shoulderTrackingDisabled condition

--- server/core/src/main/java/dev/slimevr/games/vrchat/VRCConfigHandler.kt
+++ server/core/src/main/java/dev/slimevr/games/vrchat/VRCConfigHandler.kt
@@ -1,6 +1,9 @@
 package dev.slimevr.games.vrchat
 
 import dev.slimevr.VRServer
+import dev.slimevr.tracking.processor.config.SkeletonConfigToggles
+import dev.slimevr.tracking.trackers.TrackerPosition
+import dev.slimevr.tracking.trackers.TrackerUtils
 import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.math.*
 
@@ -9,7 +12,8 @@ enum class VRCTrackerModel(val value: Int, val id: Int) {
 	SPHERE(0, solarxr_protocol.rpc.VRCTrackerModel.SPHERE),
 	SYSTEM(1, solarxr_protocol.rpc.VRCTrackerModel.SYSTEM),
 	BOX(2, solarxr_protocol.rpc.VRCTrackerModel.BOX),
-	AXIS(3, solarxr_protocol.rpc.VRCTrackerModel.AXIS);
+	AXIS(3, solarxr_protocol.rpc.VRCTrackerModel.AXIS),
+	;
 
 	companion object {
 		private val byValue = VRCTrackerModel.entries.associateBy { it.value }
@@ -22,8 +26,8 @@ enum class VRCSpineModel(val value: Int, val id: Int) {
 	UNKNOWN(-1, solarxr_protocol.rpc.VRCSpineMode.UNKNOWN),
 	LOCK_HIP(0, solarxr_protocol.rpc.VRCSpineMode.LOCK_HIP),
 	LOCK_HEAD(1, solarxr_protocol.rpc.VRCSpineMode.LOCK_HEAD),
-	LOCK_BOTH(2, solarxr_protocol.rpc.VRCSpineMode.LOCK_BOTH);
-
+	LOCK_BOTH(2, solarxr_protocol.rpc.VRCSpineMode.LOCK_BOTH),
+	;
 
 	companion object {
 		private val byValue = VRCSpineModel.entries.associateBy { it.value }
@@ -64,10 +68,10 @@ data class VRCConfigValidity(
 
 abstract class VRCConfigHandler {
 	abstract val isSupported: Boolean
-	abstract fun initHandler(onChange: (config: VRCConfigValues) -> Unit);
+	abstract fun initHandler(onChange: (config: VRCConfigValues) -> Unit)
 }
 
-class VRCConfigHandlerStub: VRCConfigHandler() {
+class VRCConfigHandlerStub : VRCConfigHandler() {
 	override val isSupported: Boolean
 		get() = false
 
@@ -81,7 +85,7 @@ interface VRCConfigListener {
 class VRChatConfigManager(val server: VRServer, private val handler: VRCConfigHandler) {
 
 	private val listeners: MutableList<VRCConfigListener> = CopyOnWriteArrayList()
-	var currentValues: VRCConfigValues? = null;
+	var currentValues: VRCConfigValues? = null
 
 	val isSupported: Boolean
 		get() = handler.isSupported
@@ -90,15 +94,31 @@ class VRChatConfigManager(val server: VRServer, private val handler: VRCConfigHa
 		handler.initHandler(::onChange)
 	}
 
+	/**
+	 * shoulderTrackingDisabled should be true if:
+	 * The user isn't tracking their whole arms from their controllers:
+	 * forceArmsFromHMD is enabled || the user doesn't have hand trackers with position || the user doesn't have lower arms trackers || the user doesn't have upper arm trackers
+	 * And the user isn't tracking their arms from their HMD or doesn't have both shoulders:
+	 * (forceArmsFromHMD is disabled && user has hand trackers with position) || user is missing a shoulder tracker
+	 */
 	fun recommendedValues(): VRCConfigRecommendedValues {
+		val forceArmsFromHMD = server.humanPoseManager.getToggle(SkeletonConfigToggles.FORCE_ARMS_FROM_HMD)
+
+		val hasLeftHandWithPosition = TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.LEFT_HAND)?.hasPosition ?: false
+		val hasRightHandWithPosition = TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.RIGHT_HAND)?.hasPosition ?: false
+
+		val isMissingAnArmTracker = TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.LEFT_LOWER_ARM) == null ||
+			TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.RIGHT_LOWER_ARM) == null ||
+			TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.LEFT_UPPER_ARM) == null ||
+			TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.RIGHT_UPPER_ARM) == null
+		val isMissingAShoulderTracker = TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.LEFT_SHOULDER) == null ||
+			TrackerUtils.getTrackerForSkeleton(server.allTrackers, TrackerPosition.RIGHT_SHOULDER) == null
+
 		return VRCConfigRecommendedValues(
 			legacyMode = false,
-			shoulderTrackingDisabled = !server.allTrackers
-				.any { //FIXME: do better condition for shoulder tracking disabled recommendation
-					if (it.isComputed) return@any false;
-					val position = it.trackerPosition ?: return@any false
-					position.isLeftArmTracker() || position.isRightArmTracker()
-				},
+			shoulderTrackingDisabled =
+			((forceArmsFromHMD || !hasLeftHandWithPosition || !hasRightHandWithPosition) || isMissingAnArmTracker) && // Not tracking shoulders from hands
+				((!forceArmsFromHMD && hasLeftHandWithPosition && hasRightHandWithPosition) || isMissingAShoulderTracker), // Not tracking shoulders from HMD
 			userHeight = server.humanPoseManager.userHeightFromConfig / 0.936,
 			calibrationRange = 0.2,
 			trackerModel = VRCTrackerModel.AXIS,
@@ -108,32 +128,29 @@ class VRChatConfigManager(val server: VRServer, private val handler: VRCConfigHa
 	}
 
 	fun addListener(listener: VRCConfigListener) {
-		listeners.add(listener);
+		listeners.add(listener)
 	}
 
 	fun removeListener(listener: VRCConfigListener) {
 		listeners.removeIf { l -> l === listener }
 	}
 
-	fun checkValidity(values: VRCConfigValues, recommended: VRCConfigRecommendedValues): VRCConfigValidity {
-		return VRCConfigValidity(
-			legacyModeOk = values.legacyMode == recommended.legacyMode,
-			shoulderTrackingOk = values.shoulderTrackingDisabled == recommended.shoulderTrackingDisabled,
-			spineModeOk = recommended.spineMode.contains(values.spineMode),
-			tackerModelOk = values.trackerModel == recommended.trackerModel,
-			calibrationOk = abs(values.calibrationRange - recommended.calibrationRange) < 0.1,
-			userHeightOk = abs(server.humanPoseManager.userHeightFromConfig / 0.936 - values.userHeight) < 0.1,
-			calibrationVisualsOk = values.calibrationVisuals == recommended.calibrationVisuals
-		)
-	}
+	fun checkValidity(values: VRCConfigValues, recommended: VRCConfigRecommendedValues): VRCConfigValidity = VRCConfigValidity(
+		legacyModeOk = values.legacyMode == recommended.legacyMode,
+		shoulderTrackingOk = values.shoulderTrackingDisabled == recommended.shoulderTrackingDisabled,
+		spineModeOk = recommended.spineMode.contains(values.spineMode),
+		tackerModelOk = values.trackerModel == recommended.trackerModel,
+		calibrationOk = abs(values.calibrationRange - recommended.calibrationRange) < 0.1,
+		userHeightOk = abs(server.humanPoseManager.userHeightFromConfig / 0.936 - values.userHeight) < 0.1,
+		calibrationVisualsOk = values.calibrationVisuals == recommended.calibrationVisuals,
+	)
 
 	fun onChange(values: VRCConfigValues) {
-		val recommended = recommendedValues();
+		val recommended = recommendedValues()
 		val validity = checkValidity(values, recommended)
-		currentValues = values;
+		currentValues = values
 		listeners.forEach {
 			it.onChange(validity, values, recommended)
 		}
 	}
-
 }
