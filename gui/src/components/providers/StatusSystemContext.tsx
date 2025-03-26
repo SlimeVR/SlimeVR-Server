3996a60bf578a90a54c308ff117d5cda70aebe46
loucass003 <loucass003@gmail.com>
2025-03-26 21:26:44 +0100

    Remove test comment

--- gui/src/components/providers/StatusSystemContext.tsx
+++ gui/src/components/providers/StatusSystemContext.tsx
@@ -4,8 +4,6 @@ import { StatusSystemC, useProvideStatusContext } from '@/hooks/status-system';
 export function StatusProvider({ children }: { children: ReactNode }) {
   const context = useProvideStatusContext();
 
-  // throw new Error('BOOOOMMMMMM');
-
   return (
     <StatusSystemC.Provider value={context}>{children}</StatusSystemC.Provider>
   );
