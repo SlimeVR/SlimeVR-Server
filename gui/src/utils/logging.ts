a894c4e1916f822a75df224b63e536429948fc5f
Uriel <imurx@proton.me>
2025-04-12 18:37:14 -0400

    Fix uncatched errors from not having Tauri

--- gui/src/utils/logging.ts
+++ gui/src/utils/logging.ts
@@ -1,16 +1,16 @@
-import { invoke } from '@tauri-apps/api/core';
+import { invoke, isTauri } from '@tauri-apps/api/core';
 
 export function log(...msgs: any[]) {
   console.log(...msgs);
-  invoke('logging', { msg: msgs.join() });
+  if (isTauri()) invoke('logging', { msg: msgs.join() });
 }
 
 export function error(...msgs: any[]) {
   console.error(...msgs);
-  invoke('erroring', { msg: msgs.join() });
+  if (isTauri()) invoke('erroring', { msg: msgs.join() });
 }
 
 export function warn(...msgs: any[]) {
   console.warn(...msgs);
-  invoke('warning', { msg: msgs.join() });
+  if (isTauri()) invoke('warning', { msg: msgs.join() });
 }
