00c768919d1dd5c71348760cda98fb0485cb8d9d
loucass003 <loucass003@gmail.com>
2025-05-14 14:50:54 +0200

    Lint

--- gui/src/hooks/vrc-config.ts
+++ gui/src/hooks/vrc-config.ts
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { useLayoutEffect, useMemo, useState } from 'react';
 import { useWebsocketAPI } from './websocket-api';
 import {
   RpcMessage,
