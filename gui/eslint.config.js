290b9869b800b3d4f7ba26841e3ba91e5a6ad8e3
Uriel <imurx@proton.me>
2025-06-15 04:11:38 -0400

    Update eslint packages

--- gui/eslint.config.js
+++ gui/eslint.config.js
@@ -1,28 +1,75 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import importAlias from '@dword-design/eslint-plugin-import-alias';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import { rules as prettierConfigRules } from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-const compat = new FlatCompat();
+const jsConfig = [
+  // ESLint Recommended Rules
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic Plugin
+  plugins.stylistic,
+  // Import X Plugin
+  plugins.importX,
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+];
+
+const reactConfig = [
+  // React Plugin
+  plugins.react,
+  // React Hooks Plugin
+  plugins.reactHooks,
+  // React JSX A11y Plugin
+  plugins.reactA11y,
+  // Airbnb React Recommended Config
+  ...configs.react.recommended,
+];
+
+const typescriptConfig = [
+  // TypeScript ESLint Plugin
+  plugins.typescriptEslint,
+  // Airbnb Base TypeScript Config
+  ...configs.base.typescript,
+  // Airbnb React TypeScript Config
+  ...configs.react.typescript,
+];
+
+const prettierConfig = [
+  // Prettier Plugin
+  {
+    name: 'prettier/plugin/config',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  // Prettier Config
+  {
+    name: 'prettier/config',
+    rules: {
+      ...prettierConfigRules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
 
 export const gui = [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...compat.extends('plugin:@dword-design/import-alias/recommended'),
-  ...compat.plugins('eslint-plugin-react-hooks'),
-  // Add import-alias rule inside compat because plugin doesn't like flat configs
-  ...compat.config({
-    rules: {
-      '@dword-design/import-alias/prefer-alias': [
-        'error',
-        {
-          alias: {
-            '@': './src/',
-          },
-        },
-      ],
-    },
-  }),
+  // Javascript Config
+  ...jsConfig,
+  // React Config
+  ...reactConfig,
+  // TypeScript Config
+  ...typescriptConfig,
+  // Prettier Config
+  ...prettierConfig,
+  importAlias.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 'latest',
@@ -39,10 +86,50 @@ export const gui = [
       },
     },
     files: ['src/**/*.{js,jsx,ts,tsx,json}'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
     rules: {
+      // fixes for airbnb
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-plusplus': 'off',
+      'consistent-return': 'off',
+      'no-bitwise': 'off',
+      eqeqeq: ['error', 'smart'],
+      'no-restricted-syntax': 'off',
+      'no-param-reassign': 'off',
+      'no-return-assign': ['error', 'except-parens'],
+      'default-case': 'off',
+      'no-continue': 'off',
+      'react/forbid-prop-types': 'off',
+      'no-script-url': 'off',
+      // stuff that should be enabled again later
+      eqeqeq: 'off',
+      'no-nested-ternary': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      'import-x/prefer-default-export': 'off',
+      'import-x/no-cycle': 'off',
+      'import-x/no-rename-default': 'off',
+      'jsx-a11y/heading-has-content': 'off',
+      'jsx-a11y/alt-text': 'off',
+      'jsx-a11y/media-has-caption': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/label-has-associated-control': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+      'jsx-a11y/no-noninteractive-element-to-interactive-role': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      'react/require-default-props': 'off',
+      'react/no-unstable-nested-components': 'off',
+      'react/no-unused-prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      // end
+      '@typescript-eslint/switch-exhaustiveness-check': [
+        'error',
+        {
+          considerDefaultExhaustiveForUnions: true,
+        },
+      ],
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'spaced-comment': 'error',
@@ -60,14 +147,14 @@ export const gui = [
           ignoreRestSiblings: true,
         },
       ],
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {},
-      },
-      react: {
-        version: 'detect',
-      },
+      '@dword-design/import-alias/prefer-alias': [
+        'error',
+        {
+          alias: {
+            '@': './src/',
+          },
+        },
+      ],
     },
   },
   // Global ignore
