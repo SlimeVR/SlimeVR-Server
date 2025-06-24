5f09676373da7ac7c76f1a5ba02d46a89d473755
0forks <114709761+0forks@users.noreply.github.com>
2025-06-25 01:02:41 +0300

    Use JNA for network profile detection

--- server/desktop/src/main/java/dev/slimevr/desktop/NetworkProfileChecker.kt
+++ server/desktop/src/main/java/dev/slimevr/desktop/NetworkProfileChecker.kt
@@ -1,63 +1,273 @@
 package dev.slimevr.desktop
 
+import com.sun.jna.Pointer
+import com.sun.jna.platform.win32.COM.COMUtils
+import com.sun.jna.platform.win32.COM.Dispatch
+import com.sun.jna.platform.win32.Guid.CLSID
+import com.sun.jna.platform.win32.Guid.IID
+import com.sun.jna.platform.win32.Ole32
+import com.sun.jna.platform.win32.WTypes
+import com.sun.jna.platform.win32.WinDef.BOOLByReference
+import com.sun.jna.platform.win32.WinNT.HRESULT
+import com.sun.jna.ptr.IntByReference
+import com.sun.jna.ptr.PointerByReference
 import dev.slimevr.VRServer
 import io.eiren.util.OperatingSystem
 import solarxr_protocol.rpc.StatusData
 import solarxr_protocol.rpc.StatusDataUnion
 import solarxr_protocol.rpc.StatusPublicNetworkT
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.*
 import kotlin.concurrent.scheduleAtFixedRate
 
-enum class NetworkProfile {
-	PRIVATE,
-	PUBLIC,
+data class NetworkInfo(
+	val name: String?,
+	val description: String?,
+	val category: NetworkCategory?,
+	val connectivity: Set<ConnectivityFlags>?,
+	val connected: Boolean?,
+)
+
+enum class NetworkCategory(val value: Int) {
+	PUBLIC(0),
+	PRIVATE(1),
+	DOMAIN_AUTHENTICATED(2),
+	;
+
+	companion object {
+		fun fromInt(value: Int) = values().find { it.value == value }
+	}
 }
 
-fun checkNetworkProfile(): NetworkProfile? {
+enum class ConnectivityFlags(val value: Int) {
+	DISCONNECTED(0),
+	IPV4_NOTRAFFIC(0x1),
+	IPV6_NOTRAFFIC(0x2),
+	IPV4_SUBNET(0x10),
+	IPV4_LOCALNETWORK(0x20),
+	IPV4_INTERNET(0x40),
+	IPV6_SUBNET(0x100),
+	IPV6_LOCALNETWORK(0x200),
+	IPV6_INTERNET(0x400),
+	;
+
+	companion object {
+		fun fromInt(value: Int): Set<ConnectivityFlags> =
+			if (value == 0) {
+				setOf(DISCONNECTED)
+			} else {
+				values().filter { it != DISCONNECTED && (value and it.value) != 0 }.toSet()
+			}
+	}
+}
+
+@Suppress("ktlint:standard:backing-property-naming", "ktlint:standard:function-naming")
+class INetworkConnection(instance: Pointer?) : Dispatch(instance) {
+	companion object VTable {
+		// IUnknown +3
+		// IDispatch +4
+		// IEnumNetworkConnections
+		const val GetNetwork = 7
+		const val IsConnectedToInternet = 8
+		const val IsConnected = 9
+		const val GetConnectivity = 10
+		const val GetConnectionId = 11
+		const val GetAdapterId = 12
+		const val GetDomainType = 13
+	}
+
+	fun GetNetwork(): INetwork? {
+		val pNetwork = PointerByReference()
+		val hr = _invokeNativeInt(VTable.GetNetwork, arrayOf(pointer, pNetwork))
+		COMUtils.checkRC(HRESULT(hr))
+		return INetwork(pNetwork.value)
+	}
+}
+
+@Suppress("ktlint:standard:backing-property-naming", "ktlint:standard:function-naming")
+class IEnumNetworkConnections(instance: Pointer?) : Dispatch(instance) {
+	companion object VTable {
+		// IUnknown +3
+		// IDispatch +4
+		// IEnumNetworkConnections
+		const val _NewEnum = 7
+		const val Next = 8
+		const val Skip = 9
+		const val Reset = 10
+		const val Clone = 11
+	}
+
+	fun Next(): INetworkConnection? {
+		val ppNetworkConnections = PointerByReference()
+		val nFetched = IntByReference()
+		val hr = _invokeNativeInt(VTable.Next, arrayOf(pointer, 1, ppNetworkConnections, nFetched))
+		COMUtils.checkRC(HRESULT(hr))
+		if (nFetched.value.equals(0)) return null
+		return INetworkConnection(ppNetworkConnections.value)
+	}
+}
+
+@Suppress("ktlint:standard:backing-property-naming", "ktlint:standard:function-naming")
+class INetworkListManager(instance: Pointer?) : Dispatch(instance) {
+	companion object VTable {
+		// IUnknown +3
+		// IDispatch +4
+		// INetworkListManager
+		const val GetNetworks = 7
+		const val GetNetwork = 8
+		const val GetNetworkConnections = 9
+		const val GetNetworkConnection = 10
+		const val IsConnectedToInternet = 11
+		const val IsConnected = 12
+		const val GetConnectivity = 13
+	}
+
+	fun GetNetworkConnections(): IEnumNetworkConnections? {
+		val pEnumNetworkConnections = PointerByReference()
+		val hr = _invokeNativeInt(VTable.GetNetworkConnections, arrayOf(pointer, pEnumNetworkConnections))
+		COMUtils.checkRC(HRESULT(hr))
+		return IEnumNetworkConnections(pEnumNetworkConnections.value)
+	}
+}
+
+@Suppress("ktlint:standard:backing-property-naming", "ktlint:standard:function-naming")
+class INetwork(instance: Pointer?) : Dispatch(instance) {
+	companion object VTable {
+		// IUnknown +3
+		// IDispatch +4
+		// INetworkListManager
+		const val GetName = 7
+		const val SetName = 8
+		const val GetDescription = 9
+		const val SetDescription = 10
+		const val GetNetworkId = 11
+		const val GetDomainType = 12
+		const val GetNetworkConnections = 13
+		const val GetTimeCreatedAndConnected = 14
+		const val IsConnectedToInternet = 15
+		const val IsConnected = 16
+		const val GetConnectivity = 17
+		const val GetCategory = 18
+		const val SetCategory = 19
+	}
+
+	fun GetName(): String? {
+		val name = PointerByReference()
+		val hr = _invokeNativeInt(VTable.GetName, arrayOf(pointer, name))
+		COMUtils.checkRC(HRESULT(hr))
+		return WTypes.BSTR(name.value).value
+	}
+
+	fun GetDescription(): String? {
+		val desc = PointerByReference()
+		val hr = _invokeNativeInt(VTable.GetDescription, arrayOf(pointer, desc))
+		COMUtils.checkRC(HRESULT(hr))
+		return WTypes.BSTR(desc.value).value
+	}
+
+	fun IsConnected(): Boolean {
+		val bool = BOOLByReference()
+		val hr = _invokeNativeInt(VTable.IsConnected, arrayOf(pointer, bool))
+		COMUtils.checkRC(HRESULT(hr))
+		return bool.value.booleanValue()
+	}
+
+	fun GetConnectivity(): Set<ConnectivityFlags> {
+		val connectivity = IntByReference()
+		val hr = _invokeNativeInt(VTable.GetConnectivity, arrayOf(pointer, connectivity))
+		COMUtils.checkRC(HRESULT(hr))
+		return ConnectivityFlags.fromInt(connectivity.value)
+	}
+
+	fun GetCategory(): NetworkCategory? {
+		val category = IntByReference()
+		val hr = _invokeNativeInt(VTable.GetCategory, arrayOf(pointer, category))
+		COMUtils.checkRC(HRESULT(hr))
+		return NetworkCategory.fromInt(category.value)
+	}
+}
+
+class COMNetworkManager {
+	lateinit var instance: INetworkListManager
+	private var shouldUninitialize = false
+
+	fun init() {
+		shouldUninitialize = COMUtils.SUCCEEDED(Ole32.INSTANCE.CoInitialize(null))
+
+		val CLSID_NetworkListManager = CLSID("dcb00c01-570f-4a9b-8d69-199fdba5723b")
+		val IID_INetworkListManager = IID("dcb00000-570f-4a9b-8d69-199fdba5723b")
+
+		val ptr = PointerByReference()
+		val hr = Ole32.INSTANCE.CoCreateInstance(
+			CLSID_NetworkListManager,
+			null,
+			WTypes.CLSCTX_ALL,
+			IID_INetworkListManager,
+			ptr,
+		)
+		COMUtils.checkRC(hr)
+
+		instance = INetworkListManager(ptr.value)
+	}
+
+	fun shutdown() {
+		if (::instance.isInitialized) {
+			instance.Release()
+		}
+		if (shouldUninitialize) {
+			Ole32.INSTANCE.CoUninitialize()
+		}
+	}
+}
+
+fun enumerateNetworks(): List<NetworkInfo>? {
 	if (OperatingSystem.currentPlatform != OperatingSystem.WINDOWS) {
 		return null
 	}
+	val netmgr = COMNetworkManager()
+	val result = mutableListOf<NetworkInfo>()
 	try {
-		// Full command as a single string
-		val command = "powershell.exe -Command \"(Get-NetConnectionProfile).NetworkCategory\""
+		netmgr.init()
+		val conns = netmgr.instance.GetNetworkConnections()
 
-		// Use ProcessBuilder with the full command passed through the shell
-		val processBuilder = ProcessBuilder(command.split(" "))
-		processBuilder.redirectErrorStream(true)
-		val process = processBuilder.start()
-
-		// Capture the output
-		val output = BufferedReader(InputStreamReader(process.inputStream)).useLines { lines ->
-			lines.joinToString("\n")
-		}
-		val exitCode = process.waitFor()
-		if (exitCode != 0) {
-			return null
-		}
-		return when (output.trim()) {
-			"Private" -> NetworkProfile.PRIVATE
-			"Public" -> NetworkProfile.PUBLIC
-			else -> null
-		}
-	} catch (e: Exception) {
-		return null
+		generateSequence { conns?.Next() }
+			.forEach { conn ->
+				val network = conn.GetNetwork()
+				result.add(
+					NetworkInfo(
+						network?.GetName(),
+						network?.GetDescription(),
+						network?.GetCategory(),
+						network?.GetConnectivity(),
+						network?.IsConnected(),
+					),
+				)
+			}
+		return result
+	} catch (err: Exception) {
+		println(err.stackTraceToString())
+	} finally {
+		netmgr.shutdown()
 	}
+	return null
 }
 
 class NetworkProfileChecker(private val server: VRServer) {
 	private val updateTickTimer = Timer("NetworkProfileCheck")
 	private var lastPublicNetworkStatus: UInt = 0u
-	private var currentNetworkProfile: NetworkProfile? = null
+	private var numPublicNetworks = 0
 
 	init {
 		if (OperatingSystem.currentPlatform == OperatingSystem.WINDOWS) {
 			this.updateTickTimer.scheduleAtFixedRate(0, 3000) {
-				val profile = checkNetworkProfile()
-				if (profile != currentNetworkProfile) {
-					currentNetworkProfile = profile
-					if (lastPublicNetworkStatus == 0u && profile == NetworkProfile.PUBLIC) {
+				val networks = enumerateNetworks()
+				val currentNumPublicNetworks =
+					networks?.filter { net ->
+						net.connected == true && net.category == NetworkCategory.PUBLIC
+					}?.count() ?: 0
+
+				if (numPublicNetworks != currentNumPublicNetworks) {
+					numPublicNetworks = currentNumPublicNetworks
+					if (lastPublicNetworkStatus == 0u && numPublicNetworks > 0) {
 						lastPublicNetworkStatus = server.statusSystem.addStatus(
 							StatusDataUnion().apply {
 								type = StatusData.StatusPublicNetwork
@@ -65,7 +275,7 @@ class NetworkProfileChecker(private val server: VRServer) {
 							},
 							false,
 						)
-					} else if (lastPublicNetworkStatus != 0u && profile != NetworkProfile.PUBLIC) {
+					} else if (lastPublicNetworkStatus != 0u && numPublicNetworks == 0) {
 						server.statusSystem.removeStatus(lastPublicNetworkStatus)
 						lastPublicNetworkStatus = 0u
 					}
