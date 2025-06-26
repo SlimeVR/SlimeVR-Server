20c2847fc01dd99d522bdefe680fe65fb6dff4b4
Butterscotch! <bscotchvanilla@gmail.com>
2025-06-25 21:09:57 -0400

    Directly use Lambda syntax

--- server/core/src/main/java/dev/slimevr/posestreamer/ServerPoseStreamer.kt
+++ server/core/src/main/java/dev/slimevr/posestreamer/ServerPoseStreamer.kt
@@ -3,14 +3,17 @@ package dev.slimevr.posestreamer
 import dev.slimevr.VRServer
 import dev.slimevr.tracking.processor.skeleton.HumanSkeleton
 import dev.slimevr.util.ann.VRServerThread
-import java.util.function.Consumer
 
 class ServerPoseStreamer(val server: VRServer) : TickPoseStreamer(server.humanPoseManager.skeleton) {
 
 	init {
 		// Register callbacks/events
-		server.addSkeletonUpdatedCallback(Consumer { skeleton: HumanSkeleton? -> this.onSkeletonUpdated(skeleton) })
-		server.addOnTick(Runnable { this.tick() })
+		server.addSkeletonUpdatedCallback { skeleton: HumanSkeleton? ->
+			this.onSkeletonUpdated(
+				skeleton,
+			)
+		}
+		server.addOnTick { this.tick() }
 	}
 
 	@VRServerThread
