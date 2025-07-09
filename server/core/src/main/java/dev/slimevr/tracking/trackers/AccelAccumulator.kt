2b5761ef8df8a83247fd1ba76b839a8ce7f435aa
Butterscotch! <bscotchvanilla@gmail.com>
2025-07-09 16:13:44 +0200

    Acceleration based mounting (step mounting)

---
+++ server/core/src/main/java/dev/slimevr/tracking/trackers/AccelAccumulator.kt
@@ -0,0 +1,24 @@
+package dev.slimevr.tracking.trackers
+
+import com.jme3.system.NanoTimer
+import io.github.axisangles.ktmath.Vector3
+
+class AccelAccumulator {
+	var acceleration = Vector3.NULL
+		private set
+	var velocity = Vector3.NULL
+		private set
+	var offset = Vector3.NULL
+		private set
+
+	val timer = NanoTimer()
+
+	fun dataTick(acceleration: Vector3, time: Float? = null) {
+		timer.update()
+		val deltaTime = time ?: timer.timePerFrame
+
+		this.acceleration = acceleration
+		offset += (velocity * deltaTime) + ((acceleration * deltaTime * deltaTime) / 2f)
+		velocity += acceleration * deltaTime
+	}
+}
