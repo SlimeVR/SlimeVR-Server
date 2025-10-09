329d559a0c3c92f96a0b57e3583356bac6ebfa40
Sebastina <sebane1@thavea.com>
2025-10-08 22:37:14 -0500

    Properly reference toe trackers.

--- server/core/src/main/java/dev/slimevr/tracking/processor/stayaligned/poses/RelaxedPose.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/stayaligned/poses/RelaxedPose.kt
@@ -11,13 +11,16 @@ class RelaxedPose(
 	val upperLeg: Angle,
 	val lowerLeg: Angle,
 	val foot: Angle,
+	val toe1: Angle,
+	val toe2: Angle,
+	val toe3: Angle,
 ) {
 	override fun toString(): String =
 		"upperLeg=$upperLeg lowerLeg=$lowerLeg foot=$foot"
 
 	companion object {
 
-		val ZERO = RelaxedPose(Angle.ZERO, Angle.ZERO, Angle.ZERO)
+		val ZERO = RelaxedPose(Angle.ZERO, Angle.ZERO, Angle.ZERO, Angle.ZERO, toe2 = Angle.ZERO, toe3 = Angle.ZERO)
 
 		/**
 		 * Gets the relaxed angles for a particular pose. May provide defaults if the
@@ -35,6 +38,9 @@ class RelaxedPose(
 							Angle.ofDeg(poseConfig.upperLegAngleInDeg),
 							Angle.ofDeg(poseConfig.lowerLegAngleInDeg),
 							Angle.ofDeg(poseConfig.footAngleInDeg),
+							Angle.ofDeg(poseConfig.toe1AngleInDeg),
+							Angle.ofDeg(poseConfig.toe2AngleInDeg),
+							Angle.ofDeg(poseConfig.toe3AngleInDeg),
 						)
 					} else {
 						null
@@ -48,6 +54,9 @@ class RelaxedPose(
 							Angle.ofDeg(poseConfig.upperLegAngleInDeg),
 							Angle.ofDeg(poseConfig.lowerLegAngleInDeg),
 							Angle.ofDeg(poseConfig.footAngleInDeg),
+							Angle.ofDeg(poseConfig.toe1AngleInDeg),
+							Angle.ofDeg(poseConfig.toe2AngleInDeg),
+							Angle.ofDeg(poseConfig.toe3AngleInDeg),
 						)
 					} else {
 						null
@@ -63,6 +72,9 @@ class RelaxedPose(
 							Angle.ofDeg(poseConfig.upperLegAngleInDeg),
 							Angle.ofDeg(poseConfig.lowerLegAngleInDeg),
 							Angle.ofDeg(poseConfig.footAngleInDeg),
+							Angle.ofDeg(poseConfig.toe1AngleInDeg),
+							Angle.ofDeg(poseConfig.toe2AngleInDeg),
+							Angle.ofDeg(poseConfig.toe3AngleInDeg),
 						)
 					} else {
 						null
@@ -105,7 +117,26 @@ class RelaxedPose(
 				}
 			}
 
-			return RelaxedPose(upperLegAngle, lowerLegAngle, footAngle)
+			var toe1Angle = Angle.ZERO
+			humanSkeleton.leftToe1Tracker?.let { left ->
+				humanSkeleton.rightToe1Tracker?.let { right ->
+					toe1Angle = halfAngleBetween(left, right)
+				}
+			}
+			var toe2Angle = Angle.ZERO
+			humanSkeleton.leftToe2Tracker?.let { left ->
+				humanSkeleton.rightToe2Tracker?.let { right ->
+					toe2Angle = halfAngleBetween(left, right)
+				}
+			}
+			var toe3Angle = Angle.ZERO
+			humanSkeleton.leftToe3Tracker?.let { left ->
+				humanSkeleton.rightToe3Tracker?.let { right ->
+					toe3Angle = halfAngleBetween(left, right)
+				}
+			}
+
+			return RelaxedPose(upperLegAngle, lowerLegAngle, footAngle,toe1Angle, toe2Angle, toe3Angle)
 		}
 	}
 }
