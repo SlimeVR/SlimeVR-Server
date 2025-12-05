a236d75c46acd285654cabff99b6e9631cdc014b
loucass003 <loucass003@gmail.com>
2025-12-05 07:46:06 +0100

    Lint

--- gui/src/components/onboarding/pages/ResetTutorial.tsx
+++ gui/src/components/onboarding/pages/ResetTutorial.tsx
@@ -15,13 +15,11 @@ import {
 import { BodyDisplay } from '@/components/commons/BodyDisplay';
 import { useWebsocketAPI } from '@/hooks/websocket-api';
 import classNames from 'classnames';
-import { useBreakpoint } from '@/hooks/breakpoint';
 import { log } from '@/utils/logging';
 import { useAtomValue } from 'jotai';
 import { assignedTrackersAtom } from '@/store/app-store';
 
 export function ResetTutorialPage() {
-  const { isMobile } = useBreakpoint('mobile');
   const { l10n } = useLocalization();
   const { applyProgress } = useOnboarding();
   const { useRPCPacket, sendRPCPacket } = useWebsocketAPI();
