276e73e72404745aaa610c3e26db9964b2c46984
Butterscotch! <bscotchvanilla@gmail.com>
2025-12-08 23:24:20 -0500

    Remove Ktor dependency

--- server/android/src/main/java/dev/slimevr/android/Main.kt
+++ server/android/src/main/java/dev/slimevr/android/Main.kt
@@ -12,49 +12,14 @@ import dev.slimevr.android.tracking.trackers.hid.AndroidHIDManager
 import dev.slimevr.config.ConfigManager
 import dev.slimevr.tracking.trackers.Tracker
 import io.eiren.util.logging.LogManager
-import io.ktor.http.CacheControl
-import io.ktor.http.CacheControl.Visibility
-import io.ktor.server.application.install
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.http.content.CachingOptions
-import io.ktor.server.http.content.staticResources
-import io.ktor.server.netty.Netty
-import io.ktor.server.netty.NettyApplicationEngine
-import io.ktor.server.plugins.cachingheaders.CachingHeaders
-import io.ktor.server.routing.routing
-import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.time.ZonedDateTime
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
-
-lateinit var webServer: NettyApplicationEngine
-	private set
-
-val webServerInitialized: Boolean
-	get() = ::webServer.isInitialized
-
-var webServerPort = 0
 
 lateinit var vrServer: VRServer
 	private set
 val vrServerInitialized: Boolean
 	get() = ::vrServer.isInitialized
-
-fun startWebServer() {
-	// Host the web GUI server
-	webServer = embeddedServer(Netty, port = 0) {
-		routing {
-			install(CachingHeaders) {
-				options { _, _ ->
-					CachingOptions(CacheControl.NoStore(Visibility.Public), ZonedDateTime.now())
-				}
-			}
-			staticResources("/", "web-gui", "index.html")
-		}
-	}.start(wait = false)
-	webServerPort = runBlocking { webServer.resolvedConnectors().first().port }
-}
 
 fun startVRServer(activity: AppCompatActivity) {
 	thread(start = true, name = "Main VRServer Thread") {
