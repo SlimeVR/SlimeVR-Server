6a86ad7fe9ab19c4d010010239cdf0e3d3627bff
Sebastina <sebane1@thavea.com>
2025-12-13 01:43:31 -0600

    Remove offset change for toes. Does not resolve issue.

--- server/core/src/main/java/dev/slimevr/tracking/trackers/TrackerResetsHandler.kt
+++ server/core/src/main/java/dev/slimevr/tracking/trackers/TrackerResetsHandler.kt
@@ -272,7 +272,7 @@ class TrackerResetsHandler(val tracker: Tracker) {
 			if (tracker.isComputed) {
 				fixGyroscope(rotation)
 			} else {
-				if (tracker.trackerPosition.isFoot() || tracker.trackerPosition.isToe()) {
+				if (tracker.trackerPosition.isFoot()) {
 					// Feet and Toes are rotated by 90 deg pitch, this means we're relying on IMU rotation
 					//  to be set correctly here.
 					fixGyroscope(rotation * tposeDownFix * QuarterPitch)
