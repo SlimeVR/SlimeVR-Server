4368b62587629b018a6a9780a934f6f3f311f8f3
Hannah Lynn Lindrob <hannahlindrob@outlook.com>
2026-01-20 17:08:01 +0100

    Update keybind component

--- gui/src/components/commons/Keybind.tsx
+++ gui/src/components/commons/Keybind.tsx
@@ -1,116 +1,148 @@
 import { useState, useRef, forwardRef, useEffect } from 'react';
-import { Controller, Control } from 'react-hook-form';
+import { Controller, Control, ResetFieldConfig, UseFormResetField } from 'react-hook-form';
+import { useMemo } from 'react';
 import { useWebsocketAPI } from '@/hooks/websocket-api';
+import { Typography } from './Typography';
 import { Button } from './Button';
 import { ClearIcon } from './icon/ClearIcon';
+import { RecordIcon } from './icon/RecordIcon';
 
 
-
-interface keybindInternalProps {
-  label: string;
-  value: string[];
-  onChange: (value: string[]) => void;
-}
-
-interface KeybindProps {
-  label: string;
-  name: string;
-  delay: number;
-  control: Control<any>
-}
-
-/*
 export const KeybindInputInternal = forwardRef<
   HTMLInputElement,
   {
-    disabled?: boolean;
-    label?: string;
-    name: string;
+    label: string;
+    keys: string[];
+    delay: number;
+    resetField: UseFormResetField<any>;
+    onKeysChange: (v: string[]) => void;
+    onDelayChange: (v: number) => void;
   } & Partial<React.HTMLProps<HTMLInputElement>>
-  >(function AppKeybindInput(
+  >(function AppKeybindInputInternal(
     {
       label,
-      onChange,
-      name,
+      keys,
+      delay,
+      resetField,
+      onKeysChange,
+      onDelayChange,
     },
     ref
   ) {
+    //TODO: This is probably bad code but it fixes a bug where after loading you can add more than 4 keys
+    const keyCountRef = useRef(keys.length);
+    const [isRecordingKeybind, setIsRecordingKeybind] = useState(false)
 
-  })
-  */
+    const classNames = "max-h-[32px] placeholder:text-background-10 placeholder:italic bg-background-60 border-background-60 w-full focus:ring-transparent focus:ring-offset-transparent min-h-[42px] z-10 focus:outline-transparent rounded-md focus:border-accent-background-40 text-standard text-background-10 relative transition-colors"
+    console.log(`Hello from ${label}`)
+    console.log(keys)
+    console.log(delay)
+    return (
+        <label className="flex flex-col gap-1">
+          {label}
+
+          <div className="relative w-full flex gap-3 items-center rounded-lg cursor-pointer ">
+            <div className='flex flex-grow h-full hover:ring-4 rounded-lg bg-background-80 '>
+              <input
+                className="opacity-0 absolute cursor-pointer h-full"
+                ref={ref}
+                onKeyDown={(event) => {
+                  if (keyCountRef.current < 4) {
+
+                    const newKeys = [...keys, event.key];
+                    keyCountRef.current++;
+                    onKeysChange(newKeys);
+                  }
+                }}
+              />
+
+              <div className="flex flex-grow h-full gap-1 min-h-[42px] items-center">
+                {keys.map((key) => (
+                  <div key={key} className="bg-red-500 p-2 rounded-md text-center max-h-[42px]">
+                    {key}
+                  </div>
+                ))}
+              </div>
+
+              <Button
+                variant="primary"
+                  onClick={() => {
+                  setIsRecordingKeybind(!isRecordingKeybind);
+                }}
+              >
+                {isRecordingKeybind ? "Recording Keybind" : "Record Keybind"}
+              </Button>
+            </div>
+
+            <label className="flex flex-col gap-1">
+              Delay
+              <input
+                type="number"
+                className={classNames}
+                value={delay}
+                onChange={(e) => onDelayChange(Number(e.target.value))}
+              />
+            </label>
+          
+
+            <Button
+              onClick={() => {
+                //TODO: Reset field to default
+              }}
+              variant="primary"
+            >
+              Reset<ClearIcon size={12} />
+            </Button>
+
+            <Button
+              onClick={() => {
+                keyCountRef.current = 0;
+                onKeysChange([]);
+              }}
+              variant="primary"
+            >
+              Clear<ClearIcon size={12} />
+            </Button>
+          </div>
+      </label>
+    );
+  });
+  
 
 
 export function KeybindInput({
     label,
     control,
-    name,
-    delay
-}: KeybindProps) {
-  const keyCountRef = useRef(0);
-  const ref = useRef<HTMLInputElement>(null);
-  const { sendRPCPacket, useRPCPacket } = useWebsocketAPI();
-
-  const handleKeyDown = (event: any, value: string[], onChange: any) => {
-    if (keyCountRef.current < 3) {
-      const newKeys = [...value, event.key];
-      keyCountRef.current++;
-    }
-  };
-
-  const setFocus = () => {
-    if (ref.current) {
-      ref.current.focus();
-    }
-  };
-
-  const emptyKeybind = (onchange: any) => {
-    keyCountRef.current = 0;
-    onchange([]);
-  };
-
+    resetField,
+    bindingName,
+    delayName
+}: {
+  label: string;
+  control: Control<any>;
+  resetField: UseFormResetField<any>;
+  bindingName: string;
+  delayName: string;
+}) {
   return (
     <Controller
       control={control}
-      name={name}
-      render={({ field: { onChange, value = [], ref} }) => (
-      <label className="flex flex-col gap-1">
-          {label}
-        <div className="relative w-full flex gap-2 items-center" onClick={setFocus}>
-          <input
-            className="opacity-0 absolute"
-            onKeyDown={(event) => {
-              if (keyCountRef.current < 3) {
-                const newKeys = [...value, event.key];
-                keyCountRef.current++;
-                onChange(newKeys);
-              }
-            }}
-            ref={ref}
-          />
-          <div className="flex flex-grow h-full gap-2" onClick={() => setFocus()}>
-            {value.map((key: String) => <div className='bg-red-500 p-2 rounded-md'>{key}</div>)}
-          </div>
-          <div>current value {value}</div>
-          <div>
-            <label>
-              Delay:
-              <input
-                className=''
-              />  
-            </label>
-          </div>
-          <Button
-            onClick={() => {
-              keyCountRef.current = 0
-              onChange([]);
-            }}
-            variant='primary'
-          >
-              <ClearIcon size={12}/>
-          </Button>
-
-        </div>
-      </label>
+      name={bindingName}
+      render={({ field: { value: keys = [], onChange: onKeysChange, ref} }) => (
+        <Controller
+          control={control}
+          name={delayName}
+          render={({ field: { value: delay, onChange: onDelayChange } }) => (
+            <KeybindInputInternal
+              label={label}
+              keys={keys}
+              delay={delay}
+              resetField={resetField}
+              onKeysChange={onKeysChange}
+              onDelayChange={onDelayChange}
+              ref={ref}
+            />
+          )}
+        />
       )}
     />
   );
