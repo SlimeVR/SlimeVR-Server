SlimeVR/SlimeVR-Server

commit 8f57ef2de409c240be88a97cce8c13d8522fe454

Migrate core/dev.slimevr.protocol to Kotlin (#1688)
Co-authored-by: Butterscotch! <bscotchvanilla@gmail.com>

server/core/src/main/java/dev/slimevr/protocol/ProtocolAPI.kt
@@ -1,0 +1,48 @@
+package dev.slimevr.protocol
+
+import dev.slimevr.VRServer
+import dev.slimevr.protocol.datafeed.DataFeedHandler
+import dev.slimevr.protocol.pubsub.PubSubHandler
+import dev.slimevr.protocol.rpc.RPCHandler
+import solarxr_protocol.MessageBundle
+import java.nio.ByteBuffer
+
+class ProtocolAPI(val server: VRServer) {
+	val rpcHandler: RPCHandler = RPCHandler(this)
+	val dataFeedHandler: DataFeedHandler = DataFeedHandler(this)
+	val pubSubHandler: PubSubHandler = PubSubHandler(this)
+
+	val apiServers: MutableList<ProtocolAPIServer> = ArrayList()
+
+	fun onMessage(conn: GenericConnection, message: ByteBuffer) {
+		val messageBundle = MessageBundle.getRootAsMessageBundle(message)
+
+		try {
+			for (index in 0..<messageBundle.dataFeedMsgsLength()) {
+				val header = messageBundle.dataFeedMsgsVector().get(index)
+				this.dataFeedHandler.onMessage(conn, header)
+			}
+
+			for (index in 0..<messageBundle.rpcMsgsLength()) {
+				val header = messageBundle.rpcMsgsVector().get(index)
+				this.rpcHandler.onMessage(conn, header)
+			}
+
+			for (index in 0..<messageBundle.pubSubMsgsLength()) {
+				val header = messageBundle.pubSubMsgsVector().get(index)
+				this.pubSubHandler.onMessage(conn, header)
+			}
+		} catch (e: AssertionError) {
+			// Catch flatbuffer errors
+			e.printStackTrace()
+		}
+	}
+
+	fun registerAPIServer(server: ProtocolAPIServer) {
+		this.apiServers.add(server)
+	}
+
+	fun removeAPIServer(server: ProtocolAPIServer) {
+		this.apiServers.remove(server)
+	}
+}

server/core/src/main/java/dev/slimevr/protocol/ConnectionContext.java
@@ -1,49 +1,0 @@
-package dev.slimevr.protocol;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class ConnectionContext {
-
-	private final List<DataFeed> dataFeedList = new ArrayList<>();
-
-	private final List<Integer> subscribedTopics = new ArrayList<>();
-
-	private boolean useSerial = false;
-
-	private boolean useProvisioning = false;
-	private boolean useAutoBone = false;
-
-	public List<DataFeed> getDataFeedList() {
-		return dataFeedList;
-	}
-
-	public List<Integer> getSubscribedTopics() {
-		return subscribedTopics;
-	}
-
-	public boolean useSerial() {
-		return useSerial;
-	}
-
-	public void setUseSerial(boolean useSerial) {
-		this.useSerial = useSerial;
-	}
-
-	public boolean useAutoBone() {
-		return useAutoBone;
-	}
-
-	public void setUseAutoBone(boolean useAutoBone) {
-		this.useAutoBone = useAutoBone;
-	}
-
-	public boolean useProvisioning() {
-		return useProvisioning;
-	}
-
-	public void setUseProvisioning(boolean useProvisioning) {
-		this.useProvisioning = useProvisioning;
-	}
-}

server/core/src/main/java/dev/slimevr/protocol/ConnectionContext.kt
@@ -1,0 +1,10 @@
+package dev.slimevr.protocol
+
+class ConnectionContext {
+	val dataFeedList: MutableList<DataFeed> = mutableListOf()
+	val subscribedTopics: MutableList<Int> = mutableListOf()
+
+	var useSerial: Boolean = false
+	var useProvisioning: Boolean = false
+	var useAutoBone: Boolean = false
+}

server/core/src/main/java/dev/slimevr/protocol/DataFeed.java
@@ -1,26 +1,0 @@
-package dev.slimevr.protocol;
-
-import solarxr_protocol.data_feed.DataFeedConfigT;
-
-
-public class DataFeed {
-	private DataFeedConfigT config;
-	private Long timeLastSent;
-
-	public DataFeed(DataFeedConfigT config) {
-		this.config = config;
-		this.timeLastSent = System.currentTimeMillis();
-	}
-
-	public DataFeedConfigT getConfig() {
-		return config;
-	}
-
-	public Long getTimeLastSent() {
-		return timeLastSent;
-	}
-
-	public void setTimeLastSent(Long timeLastSent) {
-		this.timeLastSent = timeLastSent;
-	}
-}

server/core/src/main/java/dev/slimevr/protocol/DataFeed.kt
@@ -1,0 +1,7 @@
+package dev.slimevr.protocol
+
+import solarxr_protocol.data_feed.DataFeedConfigT
+
+class DataFeed(val config: DataFeedConfigT) {
+	var timeLastSent: Long = System.currentTimeMillis()
+}

server/core/src/main/java/dev/slimevr/protocol/GenericConnection.java
@@ -1,14 +1,0 @@
-package dev.slimevr.protocol;
-
-import java.nio.ByteBuffer;
-import java.util.UUID;
-
-
-public interface GenericConnection {
-
-	UUID getConnectionId();
-
-	ConnectionContext getContext();
-
-	void send(ByteBuffer bytes);
-}

server/core/src/main/java/dev/slimevr/protocol/GenericConnection.kt
@@ -1,0 +1,12 @@
+package dev.slimevr.protocol
+
+import java.nio.ByteBuffer
+import java.util.UUID
+
+interface GenericConnection {
+	val connectionId: UUID
+
+	val context: ConnectionContext
+
+	fun send(bytes: ByteBuffer)
+}

server/core/src/main/java/dev/slimevr/protocol/ProtocolAPI.java
@@ -1,69 +1,0 @@
-package dev.slimevr.protocol;
-
-import dev.slimevr.VRServer;
-import dev.slimevr.protocol.datafeed.DataFeedHandler;
-import dev.slimevr.protocol.pubsub.PubSubHandler;
-import dev.slimevr.protocol.rpc.RPCHandler;
-import solarxr_protocol.MessageBundle;
-import solarxr_protocol.data_feed.DataFeedMessageHeader;
-import solarxr_protocol.pub_sub.PubSubHeader;
-import solarxr_protocol.rpc.RpcMessageHeader;
-
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class ProtocolAPI {
-
-	public final VRServer server;
-	public final RPCHandler rpcHandler;
-	public final DataFeedHandler dataFeedHandler;
-	public final PubSubHandler pubSubHandler;
-
-	private final List<ProtocolAPIServer> servers = new ArrayList<>();
-
-	public ProtocolAPI(VRServer server) {
-		this.server = server;
-		this.rpcHandler = new RPCHandler(this);
-		this.dataFeedHandler = new DataFeedHandler(this);
-		this.pubSubHandler = new PubSubHandler(this);
-	}
-
-	public void onMessage(GenericConnection conn, ByteBuffer message) {
-		MessageBundle messageBundle = MessageBundle.getRootAsMessageBundle(message);
-
-		try {
-			for (int index = 0; index < messageBundle.dataFeedMsgsLength(); index++) {
-				DataFeedMessageHeader header = messageBundle.dataFeedMsgsVector().get(index);
-				this.dataFeedHandler.onMessage(conn, header);
-			}
-
-			for (int index = 0; index < messageBundle.rpcMsgsLength(); index++) {
-				RpcMessageHeader header = messageBundle.rpcMsgsVector().get(index);
-				this.rpcHandler.onMessage(conn, header);
-			}
-
-			for (int index = 0; index < messageBundle.pubSubMsgsLength(); index++) {
-				PubSubHeader header = messageBundle.pubSubMsgsVector().get(index);
-				this.pubSubHandler.onMessage(conn, header);
-			}
-		} catch (AssertionError e) {
-			// Catch flatbuffer errors
-			e.printStackTrace();
-		}
-	}
-
-	public List<ProtocolAPIServer> getAPIServers() {
-		return servers;
-	}
-
-	public void registerAPIServer(ProtocolAPIServer server) {
-		this.servers.add(server);
-	}
-
-	public void removeAPIServer(ProtocolAPIServer server) {
-		this.servers.remove(server);
-	}
-
-}

commit 081451724d84f671d8932d3cfc49ab2753ef97ba

ProtocolAPI: Initialise RPCHandler after other members
The RPCHandler was being initialised before apiServers, so when
RPCVRChatHandler tried to access apiServers on init it would crash with
a NullPointerException.

Fixes: 8f57ef2de409c240be88a97cce8c13d8522fe454

server/core/src/main/java/dev/slimevr/protocol/ProtocolAPI.kt
@@ -8,11 +8,10 @@ import solarxr_protocol.MessageBundle
 import java.nio.ByteBuffer
 
 class ProtocolAPI(val server: VRServer) {
-	val rpcHandler: RPCHandler = RPCHandler(this)
+	val apiServers: MutableList<ProtocolAPIServer> = ArrayList()
 	val dataFeedHandler: DataFeedHandler = DataFeedHandler(this)
 	val pubSubHandler: PubSubHandler = PubSubHandler(this)
-
-	val apiServers: MutableList<ProtocolAPIServer> = ArrayList()
+	val rpcHandler: RPCHandler = RPCHandler(this)
 
 	fun onMessage(conn: GenericConnection, message: ByteBuffer) {
 		val messageBundle = MessageBundle.getRootAsMessageBundle(message)