31992e418f804b40d8c8983cadd84b5c6e930142
loucass003 <loucass003@gmail.com>
2026-03-10 21:06:55 +0100

    Make postinstall script not give exit 1

--- gui/electron/resources/scripts/postinstall.sh
+++ gui/electron/resources/scripts/postinstall.sh
@@ -3,9 +3,9 @@
 SRC="/opt/SlimeVR/69-slimevr-devices.rules"
 DESTDIRS=("/lib" "/usr/lib")
 
-if [[ ! -f "$SRC" ]; then
+if [[ ! -f "$SRC" ]]; then
     echo "SlimeVR udev rules not found, serial console and dongles may not work" >&2
-    exit 1
+    exit 0
 fi
 
 echo "Configuring SlimeVR udev rules..."
@@ -24,4 +24,3 @@ for DIR in "${DESTDIRS[@]}"; do
 done
 
 echo "Couldn't copy SlimeVR udev rules, serial console and dongles may not work" >&2
-exit 1
