9ca3251eb52c19eb23011d480a9d12b6c4d0d809
HannahPadd <hannah@lindrob.nl>
2026-04-02 15:30:50 +0200

    Remove broken config

---
+++ gui/scripts/notarize.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/* eslint-env node */
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLETEAMID,
+    appBundleId: 'com.yourcompany.yourAppId',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
