287023ef76ca205258e7452f658f16ab4e83aeb1
loucass003 <loucass003@gmail.com>
2026-04-20 18:29:47 +0200

    Fix videos + improve nix flake

--- flake.nix
+++ flake.nix
@@ -15,17 +15,23 @@
           runtimeLibs = pkgs: (with pkgs; [
             jdk17
 
-            alsa-lib at-spi2-atk at-spi2-core cairo cups dbus expat
+            # Audio
+            alsa-lib libpulseaudio
+
+            # Electron / Chromium runtime
+            at-spi2-atk at-spi2-core cairo cups dbus expat
             gdk-pixbuf glib gtk3 libdrm libgbm libglvnd libnotify
             libxkbcommon mesa nspr nss pango systemd vulkan-loader
             wayland xorg.libX11 xorg.libXcomposite xorg.libXdamage
             xorg.libXext xorg.libXfixes xorg.libXrandr xorg.libxcb
             xorg.libxshmfence libusb1 udev libxcrypt-legacy
-            rpm fpm
 
+            # Packaging tools (electron-builder)
+            rpm fpm zlib squashfsTools fakeroot libarchive icu
             wineWow64Packages.stable
-            zlib squashfsTools fakeroot libarchive icu
-            nodejs_22 pnpm pkg-config python3 gcc gnumake binutils git
+
+            # Build tools
+            nodejs_22 pnpm_9 pkg-config python3 gcc gnumake binutils git
             pkgs.nodePackages.node-gyp-build
           ]);
 
@@ -39,6 +45,14 @@
               # Tell electron-builder to use system tools instead of downloading them
               export USE_SYSTEM_FPM=true
               export USE_SYSTEM_MKSQUASHFS=true
+
+              # Prevent electron from re-downloading itself
+              export ELECTRON_SKIP_BINARY_DOWNLOAD=1
+
+              # Wayland support for Electron (mirrors nixpkgs slimevr package)
+              if [ -n "$NIXOS_OZONE_WL" ] && [ -n "$WAYLAND_DISPLAY" ]; then
+                export ELECTRON_OZONE_PLATFORM_HINT=auto
+              fi
             '';
             runScript = "bash";
           };
