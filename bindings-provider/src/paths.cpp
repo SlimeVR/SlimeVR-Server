d1b8b8ffc471b9be45303d74e59d5ed2911cc07d
Sapphire <imsapphire0@gmail.com>
2026-04-23 02:10:13 -0500

    BindingsProvider: Add clang-format configuration and reformat

--- bindings-provider/src/paths.cpp
+++ bindings-provider/src/paths.cpp
@@ -3,31 +3,31 @@
 namespace fs = std::filesystem;
 
 fs::path Paths::getDataPath() {
-  fs::path basePath{};
+    fs::path basePath{};
 
 #if defined(__linux__)
-  if (const char *dataHomeOverride = getenv("XDG_DATA_HOME")) {
-    basePath = dataHomeOverride;
-  } else {
-    const char *homeDir = getenv("HOME");
-    if (homeDir == nullptr)
-      throw std::runtime_error("HOME is unset");
+    if (const char *dataHomeOverride = getenv("XDG_DATA_HOME")) {
+        basePath = dataHomeOverride;
+    } else {
+        const char *homeDir = getenv("HOME");
+        if (homeDir == nullptr)
+            throw std::runtime_error("HOME is unset");
 
-    basePath = fs::path(homeDir) / ".local" / "share";
-  }
+        basePath = fs::path(homeDir) / ".local" / "share";
+    }
 #elif defined(_WIN32)
-  {
-    const char *appData = getenv("APPDATA");
-    if (appData == nullptr)
-      throw std::runtime_error("APPDATA is unset");
+    {
+        const char *appData = getenv("APPDATA");
+        if (appData == nullptr)
+            throw std::runtime_error("APPDATA is unset");
 
-    basePath = appData;
-  }
+        basePath = appData;
+    }
 #else
 #error "Unsupported platform"
 #endif
 
-  return basePath / "dev.slimevr.SlimeVR";
+    return basePath / "dev.slimevr.SlimeVR";
 }
 
 fs::path Paths::getLogPath() { return Paths::getDataPath() / "logs"; }
