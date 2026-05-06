6c7d6add672a82dc3cf7f9883c4358957e8aeb48
loucass003 <loucass003@gmail.com>
2026-05-06 14:26:45 +0200

    Fix flake

--- flake.nix
+++ flake.nix
@@ -3,16 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Pinned to a revision that still ships temurin jdk-24
+    nixpkgs-jdk24.url = "github:NixOS/nixpkgs/d0fc30899600b9b3466ddb260fd83deb486c32f1";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-jdk24, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
-      perSystem = { pkgs, ... }:
+      perSystem = { pkgs, system, ... }:
         let
-          java = pkgs.javaPackages.compiler.temurin-bin.jdk-24;
+          jdkPkgs = import nixpkgs-jdk24 { inherit system; };
+          java = jdkPkgs.javaPackages.compiler.temurin-bin.jdk-24;
 
           runtimeLibs = pkgs: (with pkgs; [
             java
@@ -20,15 +23,15 @@
             alsa-lib at-spi2-atk at-spi2-core cairo cups dbus expat
             gdk-pixbuf glib gtk3 libdrm libgbm libglvnd libnotify
             libxkbcommon mesa nspr nss pango systemd vulkan-loader
-            wayland xorg.libX11 xorg.libXcomposite xorg.libXdamage
-            xorg.libXext xorg.libXfixes xorg.libXrandr xorg.libxcb
-            xorg.libxshmfence libusb1 udev libxcrypt-legacy
+            wayland libx11 libxcomposite libxdamage
+            libxext libxfixes libxrandr libxcb
+            libxshmfence libusb1 udev libxcrypt-legacy
             rpm fpm
 
             wineWow64Packages.stable
             zlib squashfsTools fakeroot libarchive icu
             nodejs_22 pnpm pkg-config python3 gcc gnumake binutils git
-            pkgs.nodePackages.node-gyp-build
+            node-gyp-build
           ]);
 
           slimeShell = pkgs.buildFHSEnv {
