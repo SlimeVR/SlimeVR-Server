075f96553310ac7576376d3786dcc23a41c11e65
Seneral <seneral@seneral.dev>
2026-05-09 13:58:27 +0200

    Integrate IKSolver for proper positional trackers

--- server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/IKConstraint.kt
+++ server/core/src/main/java/dev/slimevr/tracking/processor/skeleton/IKConstraint.kt
@@ -1,9 +1,9 @@
 package dev.slimevr.tracking.processor.skeleton
 
 import dev.slimevr.tracking.trackers.Tracker
+import dev.slimevr.tracking.trackers.TrackerPosition
 import io.github.axisangles.ktmath.Quaternion
 import io.github.axisangles.ktmath.Vector3
-import solarxr_protocol.datatypes.BodyPart
 
 class IKConstraint(val tracker: Tracker) {
 	private var offset = Vector3.NULL
@@ -12,10 +12,20 @@ class IKConstraint(val tracker: Tracker) {
 	fun getPosition(): Vector3 = tracker.position + (tracker.getRotation() * rotationOffset).sandwich(offset)
 
 	fun reset(nodePosition: Vector3) {
-		val bodyPartsToSkip = setOf(BodyPart.LEFT_HAND, BodyPart.RIGHT_HAND)
+		// HMD on Head and Controllers in hands are assumed to be perfectly aligned with the bones they're intended to track
+		// Other generic trackers need to be calibrated from mounting position to actual body part
 
+		// TODO: Make positional mounting calibration configurable with sensible defaults
+		// Generally HMD on head and Controllers in hands don't need to be calibrated
+		// But sometimes controllers may be used on top of the hands, e.g. for tracking gloves
+
+		if ((tracker.isHmd && tracker.trackerPosition == TrackerPosition.HEAD) ||
+			tracker.trackerPosition == TrackerPosition.LEFT_HAND ||
+			tracker.trackerPosition == TrackerPosition.RIGHT_HAND
+		) {
+			return
+		}
 		rotationOffset = tracker.getRotation().inv()
-		if (tracker.trackerPosition?.bodyPart in bodyPartsToSkip) return
 		offset = nodePosition - tracker.position
 	}
 }
