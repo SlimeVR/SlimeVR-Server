869ba273820da6712c49dfcfb69331ed1b5cf88c
Sapphire <imsapphire0@gmail.com>
2026-05-24 18:55:47 -0500

    Skip SteamVR driver installation if it's already in the drivers folder
    The Windows installer copies the driver into the SteamVR drivers folder,
    we should avoid adding the driver ourselves if this is the case since
    having a driver registered twice is undefined behaviour according to
    https://github.com/ValveSoftware/openvr/wiki/Local-Driver-Registration.

--- server/desktop/src/main/java/dev/slimevr/desktop/install/drivers/Windows.kt
+++ server/desktop/src/main/java/dev/slimevr/desktop/install/drivers/Windows.kt
@@ -3,6 +3,8 @@ package dev.slimevr.desktop.install.drivers
 import com.sun.jna.platform.win32.WinReg
 import dev.slimevr.desktop.games.vrchat.RegEditWindows
 import io.eiren.util.logging.LogManager
+import kotlin.io.path.Path
+import kotlin.io.path.exists
 
 class Windows {
 	val path: String = System.getProperty("user.dir")
@@ -14,8 +16,14 @@ class Windows {
 	fun steamVRDriver() {
 		val regEdit = RegEditWindows()
 		val steamVRLocation = regEdit.getKeyByPath(WinReg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 250820")["InstallLocation"]
-		if (steamVRLocation == null || !steamVRLocation.contains("SteamVR")) {
+		if (steamVRLocation == null || !steamVRLocation.endsWith("SteamVR")) {
 			LogManager.warning("SteamVR driver installation failed: couldn't find SteamVR")
+			return
+		}
+
+		if (Path(steamVRLocation, "drivers", "slimevr").exists()) {
+			LogManager.warning("Skipping SteamVR driver installation: driver is in SteamVR drivers folder")
+			LogManager.warning("If you would like the SteamVR driver to automatically update, please uninstall the other version of SlimeVR")
 			return
 		}
 
