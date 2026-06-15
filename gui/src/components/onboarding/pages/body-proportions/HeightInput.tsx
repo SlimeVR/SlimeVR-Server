50df46fb715bc5d18f1f1895b12e0e84188c48ac
Stephen Smith <stephen304@gmail.com>
2026-06-15 17:48:59 -0400

    Add no-user-drag to unit selector buttons

--- gui/src/components/onboarding/pages/body-proportions/HeightInput.tsx
+++ gui/src/components/onboarding/pages/body-proportions/HeightInput.tsx
@@ -74,7 +74,7 @@ function UnitSelector({
           'bg-accent-background-30': active,
           'hover:bg-background-40 bg-background-40': !active,
         },
-        'flex items-center justify-center rounded-md outline-background-10 cursor-pointer'
+        'no-user-drag flex items-center justify-center rounded-md outline-background-10 cursor-pointer'
       )}
       onClick={onClick}
     >
