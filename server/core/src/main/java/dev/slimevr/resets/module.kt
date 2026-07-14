68545e52f7ce251b05c3944ee15a9cd26a877bc7
Erimel <loukalemire@gmail.com>
2026-07-14 16:57:04 -0400

    Fix reset guards

--- server/core/src/main/java/dev/slimevr/resets/module.kt
+++ server/core/src/main/java/dev/slimevr/resets/module.kt
@@ -76,6 +76,8 @@ class ResetsManager(val context: ResetsContext, val server: VRServer, val settin
 
 			executeTrackerResets(resetType, bodyParts, settings.context.state.value.data.resetsConfig)
 
+			// Update state and config
+			context.dispatch(ResetsActions.EndReset(resetType))
 			settings.context.dispatch(SettingsActions.Update { copy(resetsConfig = resetsConfig.copy(lastMountingMethod = MountingMethods.MANUAL)) })
 
 			AppLogger.resets.info("${resetType.name} Reset from $resetSourceName")
