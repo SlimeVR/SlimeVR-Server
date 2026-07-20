400b48c419f21e4e0c20f1250668c70a985e2e5a
Sapphire <imsapphire0@gmail.com>
2026-07-20 11:18:30 -0500

    Fix order of tap detection tracker settings (#1893)

--- gui/src/components/settings/pages/GeneralSettings.tsx
+++ gui/src/components/settings/pages/GeneralSettings.tsx
@@ -1347,7 +1347,7 @@ export function GeneralSettings() {
                 <div>
                   <Typography bold>
                     {l10n.getString(
-                      'settings-general-gesture_control-mountingResetTracker'
+                      'settings-general-gesture_control-fullResetTracker'
                     )}
                   </Typography>
                   <Dropdown
