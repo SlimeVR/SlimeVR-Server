59a76aa898284beeed537c6e07fa89075ad6ff6b
Sapphire <imsapphire0@gmail.com>
2026-07-28 13:36:59 -0500

    fixup! Use JDK 25

--- server/android/build.gradle.kts
+++ server/android/build.gradle.kts
@@ -224,7 +224,7 @@ android {
 	}
 
 	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_24
-		targetCompatibility = JavaVersion.VERSION_24
+		sourceCompatibility = JavaVersion.VERSION_25
+		targetCompatibility = JavaVersion.VERSION_25
 	}
 }
