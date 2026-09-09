d6c45ff72d3d1462492ee25638abf6b461438da5
Erimel <loukalemire@gmail.com>
2026-09-09 13:17:29 -0400

    Go back to tracking polarity on raw rotation

--- server/core/src/main/java/dev/slimevr/hid/behaviours/v3/motion.kt
+++ server/core/src/main/java/dev/slimevr/hid/behaviours/v3/motion.kt
@@ -5,22 +5,23 @@ import dev.slimevr.hid.HIDMagnetometerV3
 import dev.slimevr.hid.HIDReceiver
 import dev.slimevr.hid.HIDReceiverBehaviour
 import dev.slimevr.hid.HIDRotationV3
+import dev.slimevr.tracker.TrackerActions
 
 class HIDMotionV3Behaviour : HIDReceiverBehaviour {
 	override fun observe(receiver: HIDReceiver) {
 		receiver.packetEvents.on<HIDRotationV3> { packet ->
 			if (packet.sensorId != null) return@on
-			receiver.getTracker(packet.hidId)?.setRotation(rotation = packet.rotation)
+			receiver.getTracker(packet.hidId)?.context?.dispatch(TrackerActions.SetRotation(rotation = packet.rotation))
 		}.launchIn(receiver.context.scope)
 
 		receiver.packetEvents.on<HIDAccelerationV3> { packet ->
 			if (packet.sensorId != null) return@on
-			receiver.getTracker(packet.hidId)?.setRotation(acceleration = packet.acceleration)
+			receiver.getTracker(packet.hidId)?.context?.dispatch(TrackerActions.SetRotation(acceleration = packet.acceleration))
 		}.launchIn(receiver.context.scope)
 
 		receiver.packetEvents.on<HIDMagnetometerV3> { packet ->
 			if (packet.sensorId != null) return@on
-			receiver.getTracker(packet.hidId)?.setRotation(magnetometer = packet.magnetometer)
+			receiver.getTracker(packet.hidId)?.context?.dispatch(TrackerActions.SetRotation(magnetometer = packet.magnetometer))
 		}.launchIn(receiver.context.scope)
 	}
 }
