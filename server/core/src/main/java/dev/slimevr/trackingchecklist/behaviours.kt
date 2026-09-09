99de177797714100095cc71079ecf298d3ba3013
loucass003 <loucass003@gmail.com>
2026-09-09 23:19:36 +0200

    Make network profile show when invalid

--- server/core/src/main/java/dev/slimevr/trackingchecklist/behaviours.kt
+++ server/core/src/main/java/dev/slimevr/trackingchecklist/behaviours.kt
@@ -267,6 +267,7 @@ class NetworkProfileCheckBehaviour(
 					valid = state.publicNetworks.isEmpty(),
 					enabled = state.isSupported,
 					ignorable = true,
+					visibility = TrackingChecklistStepVisibility.WHEN_INVALID,
 					extraData = if (state.publicNetworks.isNotEmpty()) {
 						TrackingChecklistPublicNetworks(
 							adapters = state.publicNetworks.map {
