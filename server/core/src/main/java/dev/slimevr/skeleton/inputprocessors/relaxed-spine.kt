6b1ccd3f6fb3f845b491c38284d206b07faad36c
Erimel <loukalemire@gmail.com>
2026-09-10 20:38:01 -0400

    Improve relaxed spine even more!!! ^v^

--- server/core/src/main/java/dev/slimevr/skeleton/inputprocessors/relaxed-spine.kt
+++ server/core/src/main/java/dev/slimevr/skeleton/inputprocessors/relaxed-spine.kt
@@ -2,10 +2,12 @@ package dev.slimevr.skeleton.inputprocessors
 
 import com.jme3.math.FastMath
 import dev.slimevr.config.Settings
+import dev.slimevr.skeleton.BodyPartMap
 import dev.slimevr.skeleton.InputSkeleton
 import dev.slimevr.skeleton.SkeletonInputProcessor
 import io.github.axisangles.ktmath.Quaternion
 import solarxr_protocol.datatypes.BodyPart
+import java.util.EnumMap
 
 // At this default value, the user's spine should behave as we meant it to.
 const val DEFAULT_SPINE_UPPER_LOWER = 0.5f
@@ -19,42 +21,41 @@ private enum class SpineSource(val parts: Array<BodyPart>) {
 	UPPER_LEGS(arrayOf(BodyPart.LEFT_UPPER_LEG, BodyPart.RIGHT_UPPER_LEG)),
 }
 
-private fun interpolateRatio(fromUpperToLower: Float, curvature: Float, fromReliability: Float, toReliability: Float): Float {
-	// Remap the upper-lower ratio with reliability
-	val reliabilityAdjusted = remapRatioWithReliability(fromUpperToLower, fromReliability, toReliability)
+private val SPINE_BONES_TO_RELAX = arrayOf(
+	BodyPart.UPPER_CHEST,
+	BodyPart.LOWER_CHEST,
+	BodyPart.UPPER_WAIST,
+	BodyPart.LOWER_WAIST,
+	BodyPart.HIP,
+)
 
-	// Lerp from upper-lower to reliability-adjusted according to curvature
-	return FastMath.lerp(fromUpperToLower, reliabilityAdjusted, curvature)
-}
-
-/**
- * Remaps a ratio This assumes a default ratio of 50%.
- */
-private fun remapRatioWithReliability(ratio: Float, fromReliability: Float, toReliability: Float): Float {
-	val reliability = (toReliability / fromReliability) * DEFAULT_SPINE_UPPER_LOWER
-
-	return if (ratio <= DEFAULT_SPINE_UPPER_LOWER) {
-		ratio * 2f * reliability
-	} else {
-		reliability + (ratio - DEFAULT_SPINE_UPPER_LOWER) * 2f * (1f - reliability)
-	}
-}
-
-private fun averageRotation(inputSkeleton: InputSkeleton, takeBodyParts: Array<BodyPart> = arrayOf()): Quaternion {
-	val bonesToAverage = inputSkeleton.values.filter { it.bodyPart in takeBodyParts }
-	return bonesToAverage.map { it.rotation }
-		.reduceIndexedOrNull { index, acc, rotation ->
-			acc.lerpQ(rotation, 1f / (index + 1))
-		} ?: Quaternion.IDENTITY
-}
-
+// TODO : fine tuning! :3
+//  (also, refactoring this whole file as it has become a mess!)
 // The higher a value is, the more reliable it is.
 // If a "To" is 2x the "From", it'll use 100% "To".
 // If a "To" is the same as "From", it'll do 50-50.
 private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when (bodyPart) {
+	BodyPart.UPPER_CHEST -> when (source) {
+		// From
+		SpineSource.UPPER_CHEST -> 7f
+
+		// To (negative to relax the spine
+		SpineSource.LOWER_CHEST -> -1f
+
+		SpineSource.UPPER_WAIST -> -1.5f
+
+		SpineSource.LOWER_WAIST -> -2f
+
+		SpineSource.HIP -> -4f
+
+		SpineSource.UPPER_LEGS -> -3f
+	}
+
 	BodyPart.LOWER_CHEST -> when (source) {
 		// From
 		SpineSource.UPPER_CHEST -> 10f
+
+		SpineSource.LOWER_CHEST -> 20f
 
 		// To (negative to relax the spine
 		SpineSource.UPPER_WAIST -> -1.5f
@@ -64,13 +65,13 @@ private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when
 		SpineSource.HIP -> -4f
 
 		SpineSource.UPPER_LEGS -> -3f
-
-		else -> error("Invalid spine combination $bodyPart, $source")
 	}
 
 	BodyPart.UPPER_WAIST -> when (source) {
 		// From
 		SpineSource.UPPER_CHEST, SpineSource.LOWER_CHEST -> 10f
+
+		SpineSource.UPPER_WAIST -> 68f
 
 		// To
 		SpineSource.LOWER_WAIST -> 17f
@@ -78,8 +79,6 @@ private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when
 		SpineSource.HIP -> 5.5f
 
 		SpineSource.UPPER_LEGS -> 4.5f
-
-		else -> error("Invalid spine combination $bodyPart, $source")
 	}
 
 	BodyPart.LOWER_WAIST -> when (source) {
@@ -88,12 +87,12 @@ private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when
 
 		SpineSource.UPPER_WAIST -> 6.25f
 
+		SpineSource.LOWER_WAIST -> 44f
+
 		// To
-		SpineSource.HIP -> 11f
+		SpineSource.HIP -> 10f
 
-		SpineSource.UPPER_LEGS -> 4.5f
-
-		else -> error("Invalid spine combination $bodyPart, $source")
+		SpineSource.UPPER_LEGS -> 4.25f
 	}
 
 	BodyPart.HIP -> when (source) {
@@ -104,13 +103,45 @@ private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when
 
 		SpineSource.LOWER_WAIST -> 13f
 
+		SpineSource.HIP -> 48f
+
 		// To
 		SpineSource.UPPER_LEGS -> 13f
-
-		else -> error("Invalid spine combination $bodyPart, $source")
 	}
 
 	else -> error("Invalid missing spine body part $bodyPart")
+}
+
+private fun interpolateRatio(fromUpperToLower: Float, curvature: Float, fromReliability: Float, toReliability: Float, isActive: Boolean): Float {
+	// Compute reliability ratio
+	val reliability = (toReliability / fromReliability) * DEFAULT_SPINE_UPPER_LOWER
+
+	return if (isActive) {
+		// Use a reliability-adjusted curvature directly if relaxing an active bone
+		reliability * curvature
+	} else {
+		// Remap the upper-lower ratio with reliability
+		val reliabilityAdjusted = remapRatioWithReliability(fromUpperToLower, reliability)
+		// Lerp from upper-lower to reliability-adjusted according to curvature
+		FastMath.lerp(fromUpperToLower, reliabilityAdjusted, curvature)
+	}
+}
+
+/**
+ * Remaps the From-To ratio with From-To reliability.
+ */
+private fun remapRatioWithReliability(ratio: Float, reliability: Float): Float = if (ratio <= DEFAULT_SPINE_UPPER_LOWER) {
+	ratio * (1f / DEFAULT_SPINE_UPPER_LOWER) * reliability
+} else {
+	reliability + (ratio - DEFAULT_SPINE_UPPER_LOWER) * (1f / DEFAULT_SPINE_UPPER_LOWER) * (1f - reliability)
+}
+
+private fun averageRotation(inputSkeleton: InputSkeleton, takeBodyParts: Array<BodyPart> = arrayOf()): Quaternion {
+	val bonesToAverage = inputSkeleton.values.filter { it.bodyPart in takeBodyParts }
+	return bonesToAverage.map { it.rotation }
+		.reduceIndexedOrNull { index, acc, rotation ->
+			acc.lerpQ(rotation, 1f / (index + 1))
+		} ?: Quaternion.IDENTITY
 }
 
 /**
@@ -121,28 +152,40 @@ private fun reliabilityOf(bodyPart: BodyPart, source: SpineSource): Float = when
  */
 class RelaxedSpineInputProcessor(val settings: Settings) : SkeletonInputProcessor {
 	override fun process(mutableInputSkeleton: InputSkeleton, skeletonHeight: Float) {
+		val fixedInputs = BodyPartMap(mutableInputSkeleton)
+
 		val ratios = settings.context.state.value.data.skeletonConfig.ratios
 
-		val hasUpperChest = mutableInputSkeleton[BodyPart.UPPER_CHEST]?.isRotationActive == true
-		val hasLowerChest = mutableInputSkeleton[BodyPart.LOWER_CHEST]?.isRotationActive == true
-		val hasUpperWaist = mutableInputSkeleton[BodyPart.UPPER_WAIST]?.isRotationActive == true
-		val hasLowerWaist = mutableInputSkeleton[BodyPart.LOWER_WAIST]?.isRotationActive == true
-		val hasHip = mutableInputSkeleton[BodyPart.HIP]?.isRotationActive == true
-		val hasUpperLegs = mutableInputSkeleton[BodyPart.LEFT_UPPER_LEG]?.isRotationActive == true && mutableInputSkeleton[BodyPart.RIGHT_UPPER_LEG]?.isRotationActive == true
-		val missingSpineParts = buildList {
-			if (!hasLowerChest) add(BodyPart.LOWER_CHEST)
-			if (!hasUpperWaist) add(BodyPart.UPPER_WAIST)
-			if (!hasLowerWaist) add(BodyPart.LOWER_WAIST)
-			if (!hasHip) add(BodyPart.HIP)
-		}
+		val hasUpperChest = fixedInputs[BodyPart.UPPER_CHEST]?.isRotationActive == true
+		val hasLowerChest = fixedInputs[BodyPart.LOWER_CHEST]?.isRotationActive == true
+		val hasUpperWaist = fixedInputs[BodyPart.UPPER_WAIST]?.isRotationActive == true
+		val hasLowerWaist = fixedInputs[BodyPart.LOWER_WAIST]?.isRotationActive == true
+		val hasHip = fixedInputs[BodyPart.HIP]?.isRotationActive == true
+		val hasUpperLegs = fixedInputs[BodyPart.LEFT_UPPER_LEG]?.isRotationActive == true && fixedInputs[BodyPart.RIGHT_UPPER_LEG]?.isRotationActive == true
 
-		for (bodyPart in missingSpineParts) {
-			val bone = mutableInputSkeleton[bodyPart] ?: continue
+		for (bodyPart in SPINE_BONES_TO_RELAX) {
+			val bone = fixedInputs[bodyPart] ?: continue
 
 			// Get the first active bones above and below this one in the chain
 			val (fromSource, toSource) = when (bodyPart) {
-				BodyPart.LOWER_CHEST -> {
+				BodyPart.UPPER_CHEST -> {
 					val from = SpineSource.UPPER_CHEST.takeIf { hasUpperChest } ?: continue
+					val to = when {
+						hasUpperWaist -> SpineSource.UPPER_WAIST
+						hasLowerWaist -> SpineSource.LOWER_WAIST
+						hasHip -> SpineSource.HIP
+						hasUpperLegs -> SpineSource.UPPER_LEGS
+						else -> continue
+					}
+					from to to
+				}
+
+				BodyPart.LOWER_CHEST -> {
+					val from = when {
+						hasLowerChest -> SpineSource.LOWER_CHEST
+						hasUpperChest -> SpineSource.UPPER_CHEST
+						else -> continue
+					}
 					val to = when {
 						hasUpperWaist -> SpineSource.UPPER_WAIST
 						hasLowerWaist -> SpineSource.LOWER_WAIST
@@ -155,6 +198,7 @@ class RelaxedSpineInputProcessor(val settings: Settings) : SkeletonInputProcesso
 
 				BodyPart.UPPER_WAIST -> {
 					val from = when {
+						hasUpperWaist -> SpineSource.UPPER_WAIST
 						hasLowerChest -> SpineSource.LOWER_CHEST
 						hasUpperChest -> SpineSource.UPPER_CHEST
 						else -> continue
@@ -170,6 +214,7 @@ class RelaxedSpineInputProcessor(val settings: Settings) : SkeletonInputProcesso
 
 				BodyPart.LOWER_WAIST -> {
 					val from = when {
+						hasLowerWaist -> SpineSource.LOWER_WAIST
 						hasUpperWaist -> SpineSource.UPPER_WAIST
 						hasLowerChest -> SpineSource.LOWER_CHEST
 						hasUpperChest -> SpineSource.UPPER_CHEST
@@ -185,6 +230,7 @@ class RelaxedSpineInputProcessor(val settings: Settings) : SkeletonInputProcesso
 
 				BodyPart.HIP -> {
 					val from = when {
+						hasHip -> SpineSource.HIP
 						hasLowerWaist -> SpineSource.LOWER_WAIST
 						hasUpperWaist -> SpineSource.UPPER_WAIST
 						hasLowerChest -> SpineSource.LOWER_CHEST
@@ -203,10 +249,11 @@ class RelaxedSpineInputProcessor(val settings: Settings) : SkeletonInputProcesso
 				ratios.imputeSpineCurvature,
 				reliabilityOf(bodyPart, fromSource),
 				reliabilityOf(bodyPart, toSource),
+				bodyPart == fromSource.parts.first(),
 			)
 
-			val fromRotation = averageRotation(mutableInputSkeleton, fromSource.parts)
-			val toRotation = averageRotation(mutableInputSkeleton, toSource.parts)
+			val fromRotation = averageRotation(fixedInputs, fromSource.parts)
+			val toRotation = averageRotation(fixedInputs, toSource.parts)
 
 			mutableInputSkeleton[bodyPart] = bone.copy(rotation = fromRotation.interpQ(toRotation, interpolateRatio))
 		}
