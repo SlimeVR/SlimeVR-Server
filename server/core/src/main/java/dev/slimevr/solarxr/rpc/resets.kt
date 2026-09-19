d94d41853d2adc8d2267bbdb2b6f7e91f9dbebc1
Sebastina <sebane1@thavea.com>
2026-09-18 20:04:31 -0500

    Remove missing references for now.

--- server/core/src/main/java/dev/slimevr/solarxr/rpc/resets.kt
+++ server/core/src/main/java/dev/slimevr/solarxr/rpc/resets.kt
@@ -22,7 +22,6 @@ class ResetsBehaviour(
 					armsResetMode = config.armsResetMode,
 					yawResetSmoothTime = config.yawResetSmoothTime,
 					saveMountingReset = config.saveMountingReset,
-					resetPositionalHeadAttitude = config.resetPositionalHeadAttitude,
 				),
 			)
 		}.launchIn(receiver.context.scope)
@@ -37,7 +36,6 @@ class ResetsBehaviour(
 							armsResetMode = req.armsResetMode,
 							yawResetSmoothTime = req.yawResetSmoothTime,
 							saveMountingReset = req.saveMountingReset,
-							resetPositionalHeadAttitude = req.resetPositionalHeadAttitude,
 						),
 					)
 				},
