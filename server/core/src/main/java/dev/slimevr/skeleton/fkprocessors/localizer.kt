431c9a584ab3c9b63428e592403d2e9895d33b58
Erimel <loukalemire@gmail.com>
2026-09-23 21:15:00 -0400

    fix CI

--- server/core/src/main/java/dev/slimevr/skeleton/fkprocessors/localizer.kt
+++ server/core/src/main/java/dev/slimevr/skeleton/fkprocessors/localizer.kt
@@ -53,10 +53,11 @@ private fun isUserSitting(fk: ComputedSkeleton): Boolean {
 private fun isFootOnGround(fk: ComputedSkeleton): Boolean {
 	val leftFoot = fk[BodyPart.LEFT_FOOT] ?: return false
 	val rightFoot = fk[BodyPart.RIGHT_FOOT] ?: return false
-	return leftFoot.headPosition.y <= FLOOR_CALIBRATION_OFFSET ||
-		rightFoot.headPosition.y <= FLOOR_CALIBRATION_OFFSET ||
-		leftFoot.tailPosition.y <= FLOOR_CALIBRATION_OFFSET ||
-		rightFoot.tailPosition.y <= FLOOR_CALIBRATION_OFFSET
+	// TODO should use FLOOR_CALIBRATION_OFFSET?
+	return leftFoot.headPosition.y <= 0f ||
+		rightFoot.headPosition.y <= 0f ||
+		leftFoot.tailPosition.y <= 0f ||
+		rightFoot.tailPosition.y <= 0f
 }
 
 private fun getSourceToFollow(fk: ComputedSkeleton): FollowSource = if (isUserSitting(fk)) {
@@ -73,9 +74,9 @@ private fun getSourceToFollow(fk: ComputedSkeleton): FollowSource = if (isUserSi
 // Feet are always considered even if inactive
 private val alwaysActiveBodyParts = arrayOf(BodyPart.LEFT_FOOT, BodyPart.RIGHT_FOOT, BodyPart.LEFT_LOWER_LEG, BodyPart.RIGHT_LOWER_LEG)
 private fun getActiveBodyParts(inputs: InputSkeleton) = inputs.filter { it.value.isRotationActive }.map { it.key } + alwaysActiveBodyParts
+
 /** Returns the active bone closest to or furthest inside the ground */
-fun getLowestBone(inputs: InputSkeleton, fk: ComputedSkeleton) =
-	fk.filter { it.key in getActiveBodyParts(inputs) }.minByOrNull { it.value.tailPosition.y }?.value
+fun getLowestBone(inputs: InputSkeleton, fk: ComputedSkeleton) = fk.filter { it.key in getActiveBodyParts(inputs) }.minByOrNull { it.value.tailPosition.y }?.value
 
 object FootLocalizer {
 	enum class PlantedFoot {
@@ -115,6 +116,7 @@ object FootLocalizer {
 	}
 
 	fun getPlantedFoot(fk: ComputedSkeleton, lastPlantedFoot: PlantedFoot): PlantedFoot {
+		// TODO start with foot, fallback to ankles, else nil
 		val leftLowerLeg = fk[BodyPart.LEFT_LOWER_LEG] ?: return PlantedFoot.NONE
 		val rightLowerLeg = fk[BodyPart.RIGHT_LOWER_LEG] ?: return PlantedFoot.NONE
 
@@ -195,15 +197,15 @@ object COMLocalizer {
 	fun computeTargetCOM(targetCOM: Vector3, comVelocity: Vector3, deltaTime: Duration) = targetCOM + (comVelocity * deltaTime.inFloatingSeconds)
 
 	fun floorAdjustTargetCOM(targetCOM: Vector3, lowestBone: BoneState?) = lowestBone?.let {
-		if (it.tailPosition.y < FLOOR_CALIBRATION_OFFSET) {
-			Vector3(targetCOM.x, targetCOM.y + (FLOOR_CALIBRATION_OFFSET - it.tailPosition.y), targetCOM.z)
+		if (it.tailPosition.y <= FLOOR_CALIBRATION_OFFSET) {
+			Vector3(targetCOM.x, targetCOM.y - it.tailPosition.y, targetCOM.z)
 		} else {
 			targetCOM
 		}
 	} ?: targetCOM
 
 	fun floorAdjustCOMVelocity(comVelocity: Vector3, lowestBone: BoneState?) = lowestBone?.let {
-		if (it.tailPosition.y < FLOOR_CALIBRATION_OFFSET) {
+		if (it.tailPosition.y <= FLOOR_CALIBRATION_OFFSET) {
 			Vector3(comVelocity.x, 0f, comVelocity.z)
 		} else {
 			comVelocity
